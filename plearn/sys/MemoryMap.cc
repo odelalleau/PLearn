bc635839790eb6029353ee24bd95e69a41d8159f
plearner <plearner@15d43a8f-0efa-0310-bc34-a8a306da697e>
2002-07-30 09:58:36 +0000

    *** empty log message ***

---
+++ plearn/sys/MemoryMap.cc
@@ -0,0 +1,142 @@
+
+#ifdef _MSC_VER
+#include <io.h>
+#else
+//#include <sys/io.h>
+#endif
+
+
+#include "MemoryMap.h"
+#include "general.h"
+
+#if !defined(_MSC_VER) && !defined(_MINGW_)
+
+// NOT MICROSOFT: LINUX, SOLARIS, etc.
+#include <sys/mman.h>
+
+#else 
+
+// OUINNEDOZE
+
+//#define _X86_
+#if !defined (_MINGW_)
+#include <varargs.h>
+#else
+#include <windef.h>
+#include <winbase.h>
+#endif
+
+#endif 
+//
+
+#include "general.h"
+
+//#include <sys/stat.h>
+
+namespace PLearn <%
+using namespace std;
+
+#if !(!defined(_MSC_VER) && !defined(_MINGW_))
+
+///////////////////////////////////////////
+static int fileSize(const char *filename)
+ {
+  struct stat t;
+  stat(filename,&t);
+  return t.st_size;
+ }
+
+///////////////////////////////////////////
+void * memoryMap(const char *filename, tFileHandle & file_handle, bool readonly, int offset_, int length)
+ {
+  if ((file_handle = (tFileHandle)CreateFile( filename, 
+                                 readonly ? GENERIC_READ: GENERIC_READ | GENERIC_WRITE, 
+                                 readonly ? FILE_SHARE_READ : 0,
+                                 0,
+                                 OPEN_ALWAYS, 
+                                 FILE_ATTRIBUTE_NORMAL | FILE_FLAG_RANDOM_ACCESS,
+                                 0)))
+       {
+        int file_size = fileSize(filename);
+		HANDLE map_handle = CreateFileMapping( (LPVOID)file_handle,
+                                                   0,
+                                                   readonly ? PAGE_READONLY : PAGE_READWRITE,
+                                                   0, 
+                                                   file_size,
+                                                   0);
+		if (map_handle)
+             return (LPVOID) MapViewOfFile( map_handle,
+                                            readonly ? FILE_MAP_READ : FILE_MAP_WRITE,
+                                            0, 
+                                            offset_, 
+                                            length ? length : file_size-offset_);
+        else {
+              PLERROR("In Storage: Could not open specified memory-mapping file for reading");
+              return 0; // to keep the compiler quiet
+             }
+       } 
+  else {
+        PLERROR("In Storage: Could not open specified memory-mapping file for reading");
+        return 0; // to keep the compiler quiet
+       }
+ }
+
+///////////////////////////////////////////
+bool memoryUnmap(void * p, tFileHandle file_handle)
+ {   
+  // first parameter: start address of byte range to flush
+  // second parameter: number of bytes in range (0==flush all bytes)
+  // flush==(linux)msync(...)
+  if ((bool)FlushViewOfFile(p,0))       
+       {
+        // UnmapViewOfFile==(linux)munmap(...)
+        bool b=UnmapViewOfFile(p); // address where mapped view begins
+        CloseHandle((LPVOID)file_handle);
+        return b;
+       }
+  else return false;
+ }
+
+
+#else // fin de version windows
+
+//////////////////////////////////////////////////
+void * MemoryMap(const char *filename,tFileHandle & handle, bool read_only,  off_t & filesize)
+ {
+  void * addr;
+  if (read_only)
+       {
+        handle = open(filename,O_RDONLY);
+        if (handle<0)
+           PLERROR("In Storage: Could not open specified memory-mapping file for reading");
+        // get the size of the file in bytes
+        filesize = lseek(handle,0,SEEK_END);
+        addr = mmap(0, filesize, PROT_READ, MAP_SHARED, handle, 0);
+       }
+  else {
+        int handle = open(filename,O_RDWR);
+        if (handle<0)
+           PLERROR("In Storage: Could not open specified memory-mapping file for read-write");
+        filesize = lseek(handle,0,SEEK_END); 
+
+        addr = mmap(0, filesize, PROT_READ|PROT_WRITE, MAP_SHARED, handle, 0);
+     }
+  return addr;
+ }
+
+//////////////////////////////////////////////////
+//void FreeMemoryMap(void * data, tFileHandle handle, int length)
+void memoryUnmap(void * data, tFileHandle handle, int length)
+ {
+#if defined(_MINGW_)
+	PLERROR("memoryUnmap(): Not ported for MinGW");
+#else
+  msync((char*)data, length, MS_SYNC);
+  munmap((char *)data, length);
+#endif
+   close(handle);
+ }
+
+#endif  // version linux
+
+%> // end of namespace PLearn
---
+++ plearn/sys/MemoryMap.h
@@ -0,0 +1,61 @@
+
+/*! \file PLearnLibrary/PLearnCore/MemoryMap.h */
+
+#ifndef MODULE_MEMORY_MAP
+#define MODULE_MEMORY_MAP
+
+#include <cstdlib>
+#include <sys/stat.h>
+
+namespace PLearn <%
+using namespace std;
+
+#if !(!defined(_MSC_VER) && !defined(_MINGW_))
+
+//////////version windows NT //////////////////!<  
+typedef void * tFileHandle;
+
+const int STORAGE_UNUSED_HANDLE = 0;
+
+//////////////////////////////////////////////////////!<  
+/*!   
+  maps a file into virtual memory
+  
+  bool readonly: If true, the virtual memory is created in read-only mode
+  offset_: offset_from the start of the file
+  length: length of window to map starting at offset_ (a length of 0 implies to the end of file)
+  
+  returns 0 if fails, and you have to check GetLastError() to see what happened.
+  
+*/
+void * memoryMap(const char *filename, tFileHandle & file_handle, bool readonly=true, int offset_=0, int length=0);
+
+//////////////////////////////////////////////////////!<  
+/*!   
+  unmaps a file from virtual memory.
+  
+  the parameter: the pointer returned by a call to memoryMap(...)
+  
+  return true if succeeds, false if it does not. You will have to 
+  check the value of GetLastError() to see what went wrong.
+*/
+bool memoryUnmap(void * pointer, tFileHandle file_handle);
+
+#else 
+
+/////////////////version linux///////////////////////!<  
+typedef int tFileHandle;
+const int STORAGE_UNUSED_HANDLE = -1;
+
+
+//!  returns a pointer to the memory-mapped file
+//!  or 0 if it fails for some reason.
+void * MemoryMap(const char *filename,tFileHandle & handle, bool read_only, off_t & filesize);
+
+void memoryUnmap(void * mapped_pointer, tFileHandle handle, int length);
+
+#endif
+
+%> // end of namespace PLearn
+
+#endif //!<  MODULE_MEMORY_MAP
