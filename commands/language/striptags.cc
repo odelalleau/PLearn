fdd359e27c9951dd722244391853c028a059cfc5
morinf <morinf@15d43a8f-0efa-0310-bc34-a8a306da697e>
2002-11-17 17:00:32 +0000

    *** empty log message ***

--- commands/language/striptags.cc
+++ commands/language/striptags.cc
@@ -8,19 +8,29 @@ using namespace PLearn;
 
 int main(int argc, char** argv)
 {
-  string line;
-  while (!cin.eof())
-  {
-    getline(cin, line, '\n');
-    if (line == "") continue;
-    vector<string> tokens = split(line, "/");
-    if (tokens.size() == 2)
-    {
-      cout << tokens[0] << endl;
-    } else
-    {
-      cout << "..." << endl;
+#ifdef USE_EXCEPTIONS
+    try {
+#endif
+    // TODO: Should be specifiable via the command line
+    bool skip_dot_info = true;
+
+    string line;
+    while (!cin.eof()) {
+        getline(cin, line, '\n');
+        if (line == "")
+            continue;
+        vector<string> tokens = split(line, "/");
+        if (tokens.size() == 2) {
+            if ((skip_dot_info == false) || (tokens[0][0] != '.'))
+                cout << tokens[0] << endl;
+        } else {
+            cout << "..." << endl;
+        }
     }
-  }
-  return 0;
+#ifdef USE_EXCEPTIONS
+    } catch (const PLearnError &e) {
+        cout << "FATAL ERROR: " << e.message() << endl;
+    }
+#endif
+    return 0;
 }
