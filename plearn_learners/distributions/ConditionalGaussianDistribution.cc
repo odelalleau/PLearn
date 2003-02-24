fd5d66dd324e9b14bea6d5fa272b2d28b0e0c147
paiemeje <paiemeje@15d43a8f-0efa-0310-bc34-a8a306da697e>
2003-02-24 23:03:59 +0000

    *** empty log message ***

---
+++ plearn_learners/distributions/ConditionalGaussianDistribution.cc
@@ -0,0 +1,134 @@
+
+
+// -*- C++ -*-
+
+// ConditionalGaussianDistribution.cc
+// 
+// Copyright (C) *YEAR* *AUTHOR(S)* 
+// ...
+// Copyright (C) *YEAR* *AUTHOR(S)* 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+#include "ConditionalGaussianDistribution.h"
+#include "plapack.h"
+#include "VMat_maths.h"
+
+namespace PLearn <%
+using namespace std;
+
+ConditionalGaussianDistribution::ConditionalGaussianDistribution() 
+  :inherited()
+{
+}
+
+
+IMPLEMENT_NAME_AND_DEEPCOPY(ConditionalGaussianDistribution);
+
+void ConditionalGaussianDistribution::declareOptions(OptionList& ol)
+{
+  // ### Declare all of this object's options here
+  // ### For the "flags" of each option, you should typically specify  
+  // ### one of OptionBase::buildoption, OptionBase::learntoption or 
+  // ### OptionBase::tuningoption. Another possible flag to be combined with
+  // ### is OptionBase::nosave
+  
+  declareOption(ol, "mean", &ConditionalGaussianDistribution::mean, OptionBase::buildoption,
+                "The mean of the gaussian distribution \n"
+                "Could be learned on a training set or specified by calling setInput");
+
+  declareOption(ol, "covariance", &ConditionalGaussianDistribution::covariance, OptionBase::buildoption,
+                "The covariance of the gaussian distribution \n"
+                "Could be learned on a training set or specified by calling setOption");
+  
+  // Now call the parent class' declareOptions
+  inherited::declareOptions(ol);
+}
+
+  string ConditionalGaussianDistribution::help() const
+  {
+    // ### Provide some useful description of what the class is ...
+    return 
+      "ConditionalGaussianDistribution is a gaussian distribution in which the \n."
+      "parameters could be learned or specified manually via setInput and setOption."
+      + optionHelp();
+  }
+
+
+  void ConditionalGaussianDistribution::build()
+  {
+    inherited::build();
+  }
+
+
+  void ConditionalGaussianDistribution::train(VMat training_set)
+{
+  mean.resize(training_set.width());
+  covariance.resize(training_set.width(), training_set.width());
+  computeMeanAndCovar(training_set, mean, covariance);
+}
+
+void ConditionalGaussianDistribution::makeDeepCopyFromShallowCopy(map<const void*, void*>& copies)
+{
+  Learner::makeDeepCopyFromShallowCopy(copies);
+}
+
+double ConditionalGaussianDistribution::log_density(const Vec& x) const
+{ PLERROR("density not implemented yet for ConditionalGaussianDistribution"); return 0; }
+
+double ConditionalGaussianDistribution::density(const Vec& x) const
+{ return exp(log_density(x)); }
+  
+double ConditionalGaussianDistribution::survival_fn(const Vec& x) const
+{ PLERROR("survival_fn not implemented yet for ConditionalGaussianDistribution"); return 0; }
+
+double ConditionalGaussianDistribution::cdf(const Vec& x) const
+{ PLERROR("cdf not implemented yet for ConditionalGaussianDistribution"); return 0; }
+
+Vec ConditionalGaussianDistribution::expectation() const
+{
+  return mean;
+}
+
+Mat ConditionalGaussianDistribution::variance() const
+{
+  return covariance;
+}
+
+void ConditionalGaussianDistribution::generate(Vec& x) const
+{
+  x = multivariate_normal(mean, covariance);
+}
+
+void ConditionalGaussianDistribution::setInput(const Vec& input) const
+{
+  mean = input;
+}
+
+%> // end of namespace PLearn
---
+++ plearn_learners/distributions/ConditionalGaussianDistribution.h
@@ -0,0 +1,113 @@
+
+
+// -*- C++ -*-
+
+// ConditionalGaussianDistribution.h
+// 
+// Copyright (C) *YEAR* *AUTHOR(S)* 
+// ...
+// Copyright (C) *YEAR* *AUTHOR(S)* 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+#ifndef ConditionalGaussianDistribution_INC
+#define ConditionalGaussianDistribution_INC
+
+#include "ConditionalDistribution.h"
+
+namespace PLearn <%
+using namespace std;
+
+class ConditionalGaussianDistribution: public ConditionalDistribution
+{
+protected:
+
+  Vec mean;
+  Mat covariance;
+    
+public:
+
+  typedef ConditionalDistribution inherited;
+
+  ConditionalGaussianDistribution();
+
+
+protected: 
+
+  static void declareOptions(OptionList& ol);
+
+public:
+  // simply calls inherited::build() then build_() 
+  virtual void build();
+
+  //! Provides a help message describing this class
+  virtual string help() const;
+
+  //! Transforms a shallow copy into a deep copy
+  virtual void makeDeepCopyFromShallowCopy(map<const void*, void*>& copies);
+
+  //! Declares name and deepCopy methods
+  DECLARE_NAME_AND_DEEPCOPY(ConditionalGaussianDistribution);
+
+
+  //! trains the model
+  virtual void train(VMat training_set); 
+
+  //! return log of probability density log(p(x))
+  virtual double log_density(const Vec& x) const;
+
+  //! return probability density p(x)
+  //! [ default version returns exp(log_density(x)) ]
+  virtual double density(const Vec& x) const;
+  
+  //! return survival fn = P(X>x)
+  virtual double survival_fn(const Vec& x) const;
+
+  //! return survival fn = P(X<x)
+  virtual double cdf(const Vec& x) const;
+
+  //! return E[X] 
+  virtual Vec expectation() const;
+
+  //! return Var[X]
+  virtual Mat variance() const;
+
+  //! return a pseudo-random sample generated from the distribution.
+  virtual void generate(Vec& x) const;
+
+  virtual void setInput(const Vec& input) const;
+  
+};
+
+// Declares a few other classes and functions related to this class
+  DECLARE_OBJECT_PTR(ConditionalGaussianDistribution);
+  
+%> // end of namespace PLearn
+
+#endif
