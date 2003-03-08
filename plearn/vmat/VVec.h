aa8f8081a48edbe7ad5ef4b06995eb9ea3415e20
yoshua <yoshua@15d43a8f-0efa-0310-bc34-a8a306da697e>
2003-03-08 22:25:56 +0000

    *** empty log message ***

---
+++ plearn/vmat/VVec.h
@@ -0,0 +1,79 @@
+// -*- C++ -*-
+
+// PLearn (A C++ Machine Learning Library)
+// Copyright (C) 1998 Pascal Vincent
+// Copyright (C) 1999-2001 Pascal Vincent, Yoshua Bengio, Rejean Ducharme and University of Montreal
+// Copyright (C) 2002 Pascal Vincent, Julien Keable, Xavier Saint-Mleux
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+
+/* *******************************************************      
+   * $Id: VVec.h,v 1.1 2003/03/08 22:25:56 yoshua Exp $
+   ******************************************************* */
+
+
+/*! \file PLearnLibrary/PLearnCore/VVec.h */
+
+#ifndef VVec_INC
+#define VVec_INC
+
+#include "VMat.h"
+
+namespace PLearn <%
+
+/*! ** VVec ** */
+//! A VVec represents an abstract notion of "sample" or "example"
+//! which will allow us to generalize VMatrices to handle objects
+//! that are not conveniently representable with ordinary vectors.
+class VVector : public Object
+{
+    // We leave the actual representation choice to some
+    // underlying virtual matrix:
+  public:
+    VMat mat;
+    int row_index;
+
+    // to keep compatibility with most current code,
+    // VVec's can be converted to Vec's
+    virtual Vec toVec();
+    virtual void toVec(Vec row_vec);
+    int length() { return mat.width(); }
+};
+
+// CA CAUSE UN PROBLEME DE COMPILATION: POURQUOI?
+//DECLARE_OBJECT_PTR(VVector);
+
+typedef PP<VVector> VVec;
+
+DECLARE_OBJECT_PP(VVec, VVector);
+
+%> // end of namespace PLearn
+
+#endif
