d7fac3fb66538e42c69986e1236989b5fb3f2feb
plearner <plearner@15d43a8f-0efa-0310-bc34-a8a306da697e>
2003-03-22 04:37:00 +0000

    *** empty log message ***

--- plearn/vmat/ForwardVMatrix.cc
+++ plearn/vmat/ForwardVMatrix.cc
@@ -36,7 +36,7 @@
  
 
 /* *******************************************************      
- * $Id: ForwardVMatrix.cc,v 1.3 2003/03/19 23:15:23 jkeable Exp $
+ * $Id: ForwardVMatrix.cc,v 1.4 2003/03/22 04:37:00 plearner Exp $
  * This file is part of the PLearn library.
  ******************************************************* */
 
@@ -87,9 +87,13 @@ real ForwardVMatrix::getStringVal(int col, const string& str) const
 string ForwardVMatrix::getString(int row,int col) const
 { return vm->getString(row,col); }
 
-map<string,real> getStringMapping(int col) const;
+map<string,real> ForwardVMatrix::getStringMapping(int col) const
 {
-  return vm->getStringMapping(col); 
+  PLERROR("ForwardVMatrix::getStringMapping not implemented yet");
+  // making the compiler happy: 
+  return map<string,real>();
+
+  //  return vm->getStringMapping(col); 
 }
 
 void ForwardVMatrix::computeStats()
