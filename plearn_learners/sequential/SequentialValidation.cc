ffb2b8ad8a45cac6c865e305945457ee766b14c9
ducharme <ducharme@15d43a8f-0efa-0310-bc34-a8a306da697e>
2003-05-23 20:28:34 +0000

    *** empty log message ***

--- plearn_learners/sequential/SequentialValidation.cc
+++ plearn_learners/sequential/SequentialValidation.cc
@@ -37,6 +37,8 @@
 
 #include "SequentialValidation.h"
 #include "VecStatsCollector.h"
+#include "AsciiVMatrix.h"
+#include "FileVMatrix.h"
 
 namespace PLearn <%
 using namespace std;
@@ -72,10 +74,10 @@ void SequentialValidation::declareOptions(OptionList& ol)
   declareOption(ol, "init_train_size", &SequentialValidation::init_train_size,
     OptionBase::buildoption, "Size of the first training set. \n");
 
-  declareOption(ol, "save_model", &SequentialValidation::save_model,
+  declareOption(ol, "save_models", &SequentialValidation::save_models,
     OptionBase::buildoption, "If true, the final model will be saved in model.psave \n");
 
-  declareOption(ol, "save_initial_model", &SequentialValidation::save_initial_model,
+  declareOption(ol, "save_initial_models", &SequentialValidation::save_initial_models,
     OptionBase::buildoption, "If true, the initial model will be saved in initial_model.psave. \n");
 
   declareOption(ol, "save_test_outputs", &SequentialValidation::save_test_outputs,
@@ -85,7 +87,7 @@ void SequentialValidation::declareOptions(OptionList& ol)
     OptionBase::buildoption, "If true, the costs of the tests will be saved in test_costs.pmat \n");
 }
 
-void SequentialValidation::help()
+string SequentialValidation::help()
 {
   return
     "The SequentialValidation class allows you to describe a typical sequential \n"
@@ -107,17 +109,96 @@ void SequentialValidation::run()
     PLERROR("Could not create experiment directory %s", expdir.c_str());
 
   // Save this experiment description in the expdir (buildoptions only)
-  /*
   PLearn::save(append_slash(expdir)+"sequential_validation.psave", *this, OptionBase::buildoption);
 
-  for (t=init_train_size;t<=train.length();t++)
-    sub_train = train_set.subMatRows(0,t); // excludes t, last training pair is (t-1-horizon,t-1)
-  sub_test = train_set.subMatRows(t-1,1+horizon)
-     model.train(sub_train)
-     model.test(sub_test)
+  TVec<string> testcostnames = learner->getTestCostNames();
+  TVec<string> traincostnames = learner->getTrainCostNames();
+ 
+  // int traincostsize = traincostnames.size();
+  int testcostsize = testcostnames.size();
+  int outputsize = learner->outputsize();
 
-     (output summary statistics of test performances in model.errors)
-*/
+  // stats for a train on one split
+  VecStatsCollector train_stats;
+
+  // stats for a test on one split
+  VecStatsCollector test_stats;
+
+  // stats over all sequence
+  VecStatsCollector sequence_stats;
+
+  // the vmat in which to save results
+  VMat results;
+
+  // filename
+  string fname = append_slash(expdir)+"results.amat";
+
+  // fieldnames
+  TVec<string> fieldnames(1,string("sequence_num"));
+  fieldnames.append(addprepostfix("train.",traincostnames,".mean"));
+  fieldnames.append(addprepostfix("train.",traincostnames,".stddev"));
+  fieldnames.append(addprepostfix("test.",testcostnames,".mean"));
+  fieldnames.append(addprepostfix("test.",testcostnames,".stddev"));
+
+  int nfields = fieldnames.size();
+
+  // the learner horizon
+  int horizon = learner->horizon;
+ 
+  results = new AsciiVMatrix(fname, nfields, fieldnames, "# Special values for sequence_num are: -1 -> MEAN; -2 -> STDERROR; -3 -> STDDEV");
+
+  string learner_expdir = append_slash(expdir)+"subtrain";
+  learner->setExperimentDirectory(learner_expdir);
+  if (save_initial_models)
+    PLearn::save(learner_expdir+"/initial.psave",learner);
+
+  for (int t=init_train_size; t<=dataset.length()-horizon; t++)
+  {
+    VMat sub_train = dataset.subMatRows(0,t); // excludes t, last training pair is (t-1-horizon,t-1)
+    VMat sub_test = dataset.subMatRows(t-1,1+horizon);
+
+    learner->forget(); // PAS CERTAIN!  Doit-on faire un forget a chaque t?
+    train_stats.forget();
+    learner->setTrainingSet(sub_train);
+    learner->train(train_stats);
+
+    VMat test_outputs;
+    VMat test_costs;
+
+    if (save_test_outputs)
+      test_outputs = new FileVMatrix(learner_expdir+"/test_outputs.pmat",0,outputsize);
+    if (save_test_costs)
+      test_costs = new FileVMatrix(learner_expdir+"/test_costs.pmat",0,testcostsize);
+
+    test_stats.forget();
+    learner->test(sub_test, test_stats, test_outputs, test_costs);
+
+    Vec sequence_res(1,real(t));
+    sequence_res.append(train_stats.getMean());
+    sequence_res.append(train_stats.getStdDev());
+    sequence_res.append(test_stats.getMean());
+    sequence_res.append(test_stats.getStdDev());
+
+    sequence_stats.update(sequence_res);
+    results->appendRow(sequence_res);
+  }
+
+  // MEAN
+  Vec resultrow = sequence_stats.getMean();
+  resultrow[0] = -1;
+  results->appendRow(resultrow);
+
+  // STDERROR
+  resultrow << sequence_stats.getStdError();
+  resultrow[0] = -2;
+  results->appendRow(resultrow);
+
+  // STDDEV
+  resultrow << sequence_stats.getStdDev();
+  resultrow[0] = -3;
+  results->appendRow(resultrow);
+
+  //(output summary statistics of test performances in model.errors)
 }
 
 %> // end of namespace PLearn
