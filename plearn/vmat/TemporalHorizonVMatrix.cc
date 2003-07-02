a2d3994765513381c75af948093480dd875e3d8f
ducharme <ducharme@15d43a8f-0efa-0310-bc34-a8a306da697e>
2003-07-02 16:55:57 +0000

    *** empty log message ***

---
+++ plearn/vmat/TemporalHorizonVMatrix.cc
@@ -0,0 +1,152 @@
+// -*- C++ -*-
+
+// TemporalHorizonVMatrix.cc
+//
+// PLearn (A C++ Machine Learning Library)
+// Copyright (C) 2003 Rejean Ducharme, Yoshua Bengio
+// Copyright (C) 2003 Pascal Vincent
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+
+/* *******************************************************      
+   * $Id: TemporalHorizonVMatrix.cc,v 1.1 2003/07/02 16:55:57 ducharme Exp $
+   ******************************************************* */
+
+#include "TemporalHorizonVMatrix.h"
+
+namespace PLearn <%
+using namespace std;
+
+/** TemporalHorizonVMatrix **/
+
+PLEARN_IMPLEMENT_OBJECT_METHODS(TemporalHorizonVMatrix, "TemporalHorizonVMatrix", VMatrix);
+
+TemporalHorizonVMatrix::TemporalHorizonVMatrix(VMat the_distr, int the_horizon, int target_size)
+  : inherited(the_distr.length()-the_horizon, the_distr->width()),
+    distr(the_distr), horizon(the_horizon), targetsize(target_size)
+{
+  fieldinfos = distr->fieldinfos;
+  row_delay.resize(width());
+  for (int i=0; i<width(); i++)
+    row_delay[i] = i<width()-targetsize ? 0 : horizon;
+
+  defineSizes(distr->inputsize(), distr->targetsize(), distr->weightsize());
+}
+
+real TemporalHorizonVMatrix::get(int i, int j) const
+{ return distr->get(i+row_delay[j], j); }
+
+void TemporalHorizonVMatrix::put(int i, int j, real value)
+{ distr->put(i+row_delay[j], j, value); }
+
+real TemporalHorizonVMatrix::dot(int i1, int i2, int inputsize) const
+{
+  real res = 0.;
+  for(int k=0; k<inputsize; k++)
+    res += distr->get(i1+row_delay[k],k)*distr->get(i2+row_delay[k],k);
+  return res;
+}
+
+real TemporalHorizonVMatrix::dot(int i, const Vec& v) const
+{
+  real res = 0.;
+  for(int k=0; k<v.length(); k++)
+    res += distr->get(i+row_delay[k],k)*v[k];
+  return res;
+}
+
+real TemporalHorizonVMatrix::getStringVal(int col, const string & str) const
+{ return distr->getStringVal(col, str); }
+
+string TemporalHorizonVMatrix::getValString(int col, real val) const
+{ return distr->getValString(col,val); }
+
+string TemporalHorizonVMatrix::getString(int row, int col) const
+{ return distr->getString(row+row_delay[col],col); }
+
+const map<string,real>& TemporalHorizonVMatrix::getStringToRealMapping(int col) const
+{ return distr->getStringToRealMapping(col);}
+
+const map<real,string>& TemporalHorizonVMatrix::getRealToStringMapping(int col) const
+{ return distr->getRealToStringMapping(col);}
+
+void TemporalHorizonVMatrix::declareOptions(OptionList &ol)
+{
+    declareOption(ol, "distr", &TemporalHorizonVMatrix::distr, OptionBase::buildoption,
+        "    The matrix viewed by the TemporalHorizonVMatrix");
+    declareOption(ol, "horizon", &TemporalHorizonVMatrix::horizon, OptionBase::buildoption, 
+        "    The temporal value by which to delay the VMat");
+    declareOption(ol, "targetsize", &TemporalHorizonVMatrix::targetsize, OptionBase::buildoption, 
+        "    The number of last entries to delay");
+    inherited::declareOptions(ol);
+}
+
+void TemporalHorizonVMatrix::makeDeepCopyFromShallowCopy(map<const void*, void*>& copies)
+{
+  inherited::makeDeepCopyFromShallowCopy(copies);
+  deepCopyField(distr, copies);
+}
+
+///////////
+// build //
+///////////
+void TemporalHorizonVMatrix::build()
+{
+  inherited::build();
+  build_();
+}
+
+////////////
+// build_ //
+////////////
+void TemporalHorizonVMatrix::build_()
+{
+  length_ = distr->length()-horizon;
+  width_ = distr->width();
+  fieldinfos = distr->fieldinfos;
+
+  row_delay.resize(width());
+  for (int i=0; i<width(); i++)
+    row_delay[i] = i<width()-targetsize ? 0 : horizon;
+
+  defineSizes(distr->inputsize(), distr->targetsize(), distr->weightsize());
+}
+
+//////////
+// help //
+//////////
+string TemporalHorizonVMatrix::help()
+{
+  return
+    "    VMat class that delay the last entries of an underlying VMat by a certain horizon.\n"
+    + optionHelp();
+}
+
+%> // end of namespace PLearn
---
+++ plearn/vmat/TemporalHorizonVMatrix.h
@@ -0,0 +1,100 @@
+// -*- C++ -*-
+
+// TemporalHorizonVMatrix.h
+//
+// PLearn (A C++ Machine Learning Library)
+// Copyright (C) 2003 Rejean Ducharme, Yoshua Bengio
+// Copyright (C) 2003 Pascal Vincent
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+
+/* *******************************************************      
+   * $Id: TemporalHorizonVMatrix.h,v 1.1 2003/07/02 16:55:57 ducharme Exp $
+   ******************************************************* */
+
+
+
+#ifndef TEMPORAL_HORIZON_VMATRIX
+#define TEMPORAL_HORIZON_VMATRIX
+
+#include "VMat.h"
+
+namespace PLearn <%
+using namespace std;
+ 
+//!  This VMat delay the last targetsize entries of an underlying VMat
+//!  by a certain horizon
+class TemporalHorizonVMatrix: public VMatrix
+{
+  typedef VMatrix inherited;
+
+protected:
+  VMat distr;  // the underlying VMat
+  int horizon;  // the temporal value by which to delay the VMat
+  int targetsize;  // the number of last entries to delay
+  TVec<int> row_delay;  // delay for the row index
+
+public:
+
+  //! Also copies the original fieldinfos upon construction
+  TemporalHorizonVMatrix() {};
+
+  TemporalHorizonVMatrix(VMat the_distr, int the_horizon, int target_size);
+
+  DECLARE_NAME_AND_DEEPCOPY(TemporalHorizonVMatrix);
+
+  static void declareOptions(OptionList &ol);
+
+  static string help();
+  virtual void build();
+  virtual void makeDeepCopyFromShallowCopy(map<const void*, void*>& copies);
+
+  virtual real get(int i, int j) const;
+  virtual void put(int i, int j, real value);
+  virtual real getStringVal(int col, const string & str) const;
+  virtual string getValString(int col, real val) const;
+  virtual string getString(int row,int col) const;
+  //! returns the whole string->value mapping
+  virtual const map<string,real>& getStringToRealMapping(int col) const;
+  virtual const map<real,string>& getRealToStringMapping(int col) const;
+
+  virtual void reset_dimensions() { distr->reset_dimensions(); width_=distr->width(); }
+  virtual real dot(int i1, int i2, int inputsize) const;
+  virtual real dot(int i, const Vec& v) const;
+
+private:
+  void build_();
+};
+
+DECLARE_OBJECT_PTR(TemporalHorizonVMatrix);
+
+
+%> // end of namespcae PLearn
+#endif
