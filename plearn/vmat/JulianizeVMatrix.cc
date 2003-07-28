6f64be67092b501fb37e3720fe0c61f9741c4a20
chapados <chapados@15d43a8f-0efa-0310-bc34-a8a306da697e>
2003-07-28 18:35:43 +0000

    *** empty log message ***

---
+++ plearn/vmat/JulianizeVMatrix.cc
@@ -0,0 +1,161 @@
+
+// -*- C++ -*-
+
+// JulianizeVMatrix.cc
+//
+// Copyright (C) 2003  Nicolas Chapados 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: JulianizeVMatrix.cc,v 1.1 2003/07/28 18:35:43 chapados Exp $ 
+   ******************************************************* */
+
+/*! \file JulianizeVMatrix.cc */
+#include "JulianizeVMatrix.h"
+#include "PDate.h"
+#include "PDateTime.h"
+
+namespace PLearn <%
+using namespace std;
+
+
+JulianizeVMatrix::JulianizeVMatrix()
+  : parentclass()
+  /* all other compiler-supplied defaults are reasonable */
+{ }
+
+JulianizeVMatrix::JulianizeVMatrix(VMat underlying,
+                                   DateCode date_code,
+                                   int starting_column)
+  : parentclass(underlying->length(), newWidth(underlying, date_code)),
+    underlying_(underlying),
+    cols_codes_(1), und_row_(underlying.width())
+{
+  cols_codes_[0] = make_pair(starting_column, date_code);
+}
+
+
+PLEARN_IMPLEMENT_OBJECT_METHODS(JulianizeVMatrix, "JulianizeVMatrix", RowBufferedVMatrix);
+
+void JulianizeVMatrix::getRow(int i, Vec v) const
+{
+  underlying_->getRow(i, und_row_);
+  v.resize(width());
+
+  Vec::iterator
+    src_beg = und_row_.begin(),
+    src_it  = und_row_.begin(),
+    src_end = und_row_.end(),
+    dst_it  = v.begin();
+  vector< pair<int,DateCode> >::const_iterator
+    codes_it  = cols_codes_.begin(),
+    codes_end = cols_codes_.end();
+
+  for ( ; codes_it < codes_end ; ++codes_it ) {
+    // Copy what comes before the current date
+    dst_it = copy(src_it, src_beg + codes_it->first, dst_it);
+    src_it = src_beg + codes_it->first;
+
+    // Now convert the date per se
+    double converted_date;
+    int YYYY,MM,DD,hh,mm,ss;
+    YYYY = int(*src_it++);
+    MM   = int(*src_it++);
+    DD   = int(*src_it++);
+    switch(codes_it->second) {
+    case Date:
+      converted_date = PDate(YYYY,MM,DD).toJulianDay();
+      break;
+    case DateTime:
+      hh = int(*src_it++);
+      mm = int(*src_it++);
+      ss = int(*src_it++);
+      converted_date = PDateTime(YYYY,MM,DD,hh,mm,ss).toJulianDay();
+      break;
+    }
+    *dst_it++ = converted_date;
+  }
+
+  // And now copy what comes after the last date
+  copy(src_it, src_end, dst_it);
+}
+
+void JulianizeVMatrix::declareOptions(OptionList& ol)
+{
+  // ### Declare all of this object's options here
+  // ### For the "flags" of each option, you should typically specify  
+  // ### one of OptionBase::buildoption, OptionBase::learntoption or 
+  // ### OptionBase::tuningoption. Another possible flag to be combined with
+  // ### is OptionBase::nosave
+
+  // ### ex:
+  // declareOption(ol, "myoption", &JulianizeVMatrix::myoption, OptionBase::buildoption,
+  //               "Help text describing this option");
+  // ...
+
+  // Now call the parent class' declareOptions
+  parentclass::declareOptions(ol);
+}
+
+string JulianizeVMatrix::help()
+{
+  // ### Provide some useful description of what the class is ...
+  return 
+    "JulianizeVMatrix provides a conversion from a VMat containing dates\n"
+    "in an explicit 3-column (YYYY,MM,DD) or 6-column (YYYY,MM,DD,HH,MM,SS)\n"
+    "format to a Julian day number format (including fractional part to\n"
+    "represent the hour within the day).  The dates can be at any columns,\n"
+    "not only columns 0-2 (or 0-5).  More than a single date can be\n"
+    "converted.\n"
+    + optionHelp();
+}
+
+void JulianizeVMatrix::build_()
+{
+  // No options to build at this point
+}
+
+// ### Nothing to add here, simply calls build_
+void JulianizeVMatrix::build()
+{
+  parentclass::build();
+  build_();
+}
+
+void JulianizeVMatrix::makeDeepCopyFromShallowCopy(map<const void*, void*>& copies)
+{
+  parentclass::makeDeepCopyFromShallowCopy(copies);
+  deepCopyField(underlying_, copies);
+  deepCopyField(und_row_, copies);
+  // cols_codes_ already deep-copied since it is an STL vector
+}
+
+%> // end of namespace PLearn
+
---
+++ plearn/vmat/JulianizeVMatrix.h
@@ -0,0 +1,151 @@
+
+// -*- C++ -*-
+
+// JulianizeVMatrix.h
+//
+// Copyright (C) 2003  Nicolas Chapados 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: JulianizeVMatrix.h,v 1.1 2003/07/28 18:35:43 chapados Exp $ 
+   ******************************************************* */
+
+/*! \file JulianizeVMatrix.h */
+#ifndef JulianizeVMatrix_INC
+#define JulianizeVMatrix_INC
+
+#include <utilities>
+#include <vector>
+#include <algorithms>
+
+#include "RowBufferedVMatrix.h"
+#include "VMat.h"
+
+namespace PLearn <%
+using namespace std;
+
+/*! JulianizeVMatrix
+ *
+ * The purpose of a JulianizeVMatrix is to convert some columns of an
+ * underlying VMatrix into a JulianDayNumber (represented as a double).
+ * This VMatrix can convert any triplet of (YYYY,MM,DD) or sextuplet
+ * (YYY,MM,DD,HH,MM,SS) into a single double, whose integer part is the JDN
+ * and the fractional part codes the HH:MM:SS as a day fraction.  Columns
+ * anywhere in the VMat can thereby be sepcified.
+ *
+ * A class invariant here is that the member variable cols_codes_ (which
+ * stores pairs of the starting columns for dates to convert along with the
+ * date code for carrying out the conversion) is kept SORTED in order of
+ * increasing column.
+ */
+class JulianizeVMatrix : public RowBufferedVMatrix
+{
+public:
+  //! This specifies the how the dates are coded in the underlying VMatrix;
+  //! for now only two formats are allowed.
+  enum DateCode {
+    Date = 0,                                //!< (YYYY, MM, DD)
+    DateTime = 1                             //!< (YYYY, MM, DD, HH, MM, SS)
+  };
+
+  //! Return the number of columns taken by each date code
+  static int dateCodeWidth(DateCode dc) {
+    switch(dc) {
+    case Date: return 3;
+    case DateTime: return 6;
+    }
+  }
+  
+protected:
+  VMat underlying_;                          //!< underlying vmat
+  vector< pair<int,DateCode> > cols_codes_;  //!< all columns/date codes
+  mutable Vec und_row_;                      //!< buffer for underlying row
+
+public:
+
+  
+  // ************************
+  // * public build options *
+  // ************************
+
+  // ### declare public option fields (such as build options) here
+  // ...
+
+  // ****************
+  // * Constructors *
+  // ****************
+
+  //! Default constructor, the implementation in the .cc
+  //! initializes all fields to reasonable default values.
+  JulianizeVMatrix();
+
+  //! Simple constructor: takes as input only the date code and the starting
+  //! column for a single date.  Starting columns are zero-based.
+  JulianizeVMatrix(VMat underlying,
+                   DateCode date_code = Date,
+                   int starting_column = 0);
+
+  // ******************
+  // * Object methods *
+  // ******************
+
+private: 
+  //! This does the actual building. 
+  void build_();
+
+protected: 
+  //! Declares this class' options.  No options are currently supported
+  static void declareOptions(OptionList& ol);
+
+public:
+  //!  Implement the base class abstract member function
+  virtual void getRow(int i, Vec v) const;
+
+  // simply calls parentclass::build() then build_() 
+  virtual void build();
+
+  //! Provides a help message describing this class
+  static string help();
+
+  //! Transforms a shallow copy into a deep copy
+  virtual void makeDeepCopyFromShallowCopy(map<const void*, void*>& copies);
+
+  //! Declares name and deepCopy methods
+  PLEARN_DECLARE_OBJECT_METHODS(JulianizeVMatrix, "JulianizeVMatrix", RowBufferedVMatrix);
+
+protected:
+  // Return the new number of columns
+  static int newWidth(VMat und, DateCode dc) {
+    return und->width() - dateCodeWidth(dc) + 1;
+  }
+};
+
+%> // end of namespace PLearn
+#endif
