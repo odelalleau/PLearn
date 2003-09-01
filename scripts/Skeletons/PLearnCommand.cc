1c10369211c55aa169ddf05ac200082b1548f643
plearner <plearner@15d43a8f-0efa-0310-bc34-a8a306da697e>
2003-09-01 00:05:31 +0000

    *** empty log message ***

--- scripts/Skeletons/PLearnCommand.cc
+++ scripts/Skeletons/PLearnCommand.cc
@@ -6,6 +6,16 @@ using namespace std;
 //! This allows to register the 'DERIVEDCLASS' command in the command registry
 PLearnCommandRegistry DERIVEDCLASS::reg_(new DERIVEDCLASS);
 
+DERIVEDCLASS::DERIVEDCLASS():
+    PLearnCommand(">>>> INSERT COMMAND NAME HERE",
+
+                  ">>>> INSERT A SHORT ONE LINE DESCRIPTION HERE",
+
+                  ">>>> INSERT SYNTAX AND \n"
+                  "FULL DETAILED HELP HERE \n"
+                  ) 
+  {}
+
 //! The actual implementation of the 'DERIVEDCLASS' command 
 void DERIVEDCLASS::run(const vector<string>& args)
 {
--- scripts/Skeletons/PLearnCommand.h
+++ scripts/Skeletons/PLearnCommand.h
@@ -10,16 +10,7 @@ using namespace std;
 class DERIVEDCLASS: public PLearnCommand
 {
 public:
-  DERIVEDCLASS():
-    PLearnCommand(">>>> INSERT COMMAND NAME HERE",
-
-                  ">>>> INSERT A SHORT ONE LINE DESCRIPTION HERE",
-
-                  ">>>> INSERT SYNTAX AND \n"
-                  "FULL DETAILED HELP HERE \n"
-                  ) 
-  {}
-                    
+  DERIVEDCLASS();                    
   virtual void run(const vector<string>& args);
 
 protected:
