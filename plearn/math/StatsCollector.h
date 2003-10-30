62d7c48ad7ee61bf0df7d90b827efd2a970571ab
jkeable <jkeable@15d43a8f-0efa-0310-bc34-a8a306da697e>
2003-10-30 04:42:49 +0000

    *** empty log message ***

--- plearn/math/StatsCollector.h
+++ plearn/math/StatsCollector.h
@@ -33,7 +33,7 @@
 // library, go to the PLearn Web site at www.plearn.org
  
 /* *******************************************************      
-   * $Id: StatsCollector.h,v 1.20 2003/10/30 04:34:39 jkeable Exp $
+   * $Id: StatsCollector.h,v 1.21 2003/10/30 04:42:49 jkeable Exp $
    * This file is part of the PLearn library.
    ******************************************************* */
 
@@ -140,144 +140,7 @@ inline PStream& operator<<(PStream& out, const StatsCollectorCounts& c)
     real stderror() const { return sqrt(variance()/nnonmissing()); }
     real first_obs() const { return first_; }
     real last_obs() const { return last_; }
-// -*- C++ -*-
-
-// PLearn (A C++ Machine Learning Library)
-// Copyright (C) 2001,2002 Pascal Vincent
-//
-
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-// 
-//  1. Redistributions of source code must retain the above copyright
-//     notice, this list of conditions and the following disclaimer.
-// 
-//  2. Redistributions in binary form must reproduce the above copyright
-//     notice, this list of conditions and the following disclaimer in the
-//     documentation and/or other materials provided with the distribution.
-// 
-//  3. The name of the authors may not be used to endorse or promote
-//     products derived from this software without specific prior written
-//     permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
-// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
-// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
-// This file is part of the PLearn library. For more information on the PLearn
-// library, go to the PLearn Web site at www.plearn.org
- 
-/* *******************************************************      
-   * $Id: StatsCollector.h,v 1.20 2003/10/30 04:34:39 jkeable Exp $
-   * This file is part of the PLearn library.
-   ******************************************************* */
-
-#ifndef StatsCollector_INC
-#define StatsCollector_INC
-
-#include "general.h"
-#include "Object.h"
-#include "TMat.h"
-#include "RealMapping.h"
-
-namespace PLearn <%
-using namespace std;
-
-class StatsCollectorCounts
-{
-public:
-  double n; //!<  counts the number of occurences of the corresponding value
-  double nbelow; //!<  counts the number of occurences of values below this counter's value but above the previous one
-  double sum; //!<  sum of the values below this counter's but above the previous one
-  double sumsquare; //!<  sum of squares of the values below this counter's but above the previous one
-  int id; //!< a unique int identifier corresponding to this value (ids will span from 0 to # of known values) also, take a look at StatsCollector::sortIds()
-  
-  StatsCollectorCounts(): 
-    n(0), nbelow(0),
-    sum(0.), sumsquare(0.),id(0) {}          
-};
-
-typedef pair<real,StatsCollectorCounts*> PairRealSCCType;
-
-//!  this class holds simple statistics about a field
-
-inline PStream& operator>>(PStream& in, StatsCollectorCounts& c)
-{ in >> c.n >> c.nbelow >> c.sum >> c.sumsquare >> c.id; return in; }
-
-inline PStream& operator<<(PStream& out, const StatsCollectorCounts& c)
-{ out << c.n << c.nbelow << c.sum << c.sumsquare << c.id; return out; }
-
-/*!
-    "A StatsCollector allows to compute basic global statistics for a series of numbers,\n"
-    "as well as statistics within automatically determined ranges.\n"
-    "The first maxnvalues encountered values will be used as points to define\n"
-    "the ranges, so to get reasonable results, your sequence should be iid, and NOT sorted!"
-*/
-  class StatsCollector: public Object
-  {
-  public:
-    typedef Object inherited;
-    PLEARN_DECLARE_OBJECT(StatsCollector);
-      
-  public:
-
-    typedef Object inherited;
-
-    // ** Build options **
-
-    //! maximum number of different values to keep track of in counts
-    //! (if 0, we will only keep track of global statistics)
-    int maxnvalues; 
-
-
-    // ** Learnt options **
-
-    double nmissing_;      //!< (weighted) number of missing values
-    double nnonmissing_;   //!< (weighted) number of non missing value 
-    double sum_;           //!< sum of all values
-    double sumsquare_;     //!< sum of square of all values
-    real min_;             //!< the min
-    real max_;             //!< the max
-    real first_;           //!< first encountered nonmissing observation
-    real last_;            //!< last encountered nonmissing observation
-
-    //! will contain up to maxnvalues values and associated Counts
-    //! as well as a last element which maps FLT_MAX, so that we don't miss anything
-    //! (empty if maxnvalues=0)
-    map<real,StatsCollectorCounts> counts; 
-      
-  private:
-  //! This does the actual building.
-  // (Please implement in .cc)
-  void build_();
-
-  protected: 
-    //! Declares this class' options
-    static void declareOptions(OptionList& ol);
-
-  public:
-
-    StatsCollector(int the_maxnvalues=0);
-      
-    real n() const { return nmissing_ + nnonmissing_; } //!< number of samples seen with update (length of VMat for ex.)
-    real nmissing() const { return nmissing_; }
-    real nnonmissing() const { return nnonmissing_; }
-    real sum() const { return real(sum_); }
-    real sumsquare() const { return real(sumsquare_); }
-    real min() const { return min_; }
-    real max() const { return max_; }
-    real mean() const { return real(sum_/nnonmissing_); }
-    real variance() const { return real((sumsquare_ - square(sum_)/nnonmissing_)/(nnonmissing_-1)); }
-    real stddev() const { return sqrt(variance()); }
-    real stderror() const { return sqrt(variance()/nnonmissing()); }
-    real sharperatio() const { return mean()/stddev(); }          
+    real sharperatio() const { return mean()/stddev(); }
 
     //! currently understood statnames are :
     //!   - E (mean)
