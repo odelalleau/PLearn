c89ebfaf3fd80ec772da3071009187f2fe2e35da
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2003-11-17 19:26:03 +0000

    Small bug fix when looking for decimal numbers, code cleanup, and removing of the useless 'weird' type

--- commands/PLearnCommands/FieldConvertCommand.cc
+++ commands/PLearnCommands/FieldConvertCommand.cc
@@ -42,13 +42,14 @@ void FieldConvertCommand::run(const vector<string> & args)
       report_fn=val[1];
     else if(val[0]=="min_fraction")
       UNIQUE_NMISSING_FRACTION_TO_ASSUME_CONTINUOUS=toreal(val[1]);
-    else if(val[0]=="min_pvalue")
+    else if(val[0]=="max_pvalue")
       PVALUE_THRESHOLD=toreal(val[1]);
     else if(val[0]=="frac_missing_to_skip")
       FRAC_MISSING_TO_SKIP=toreal(val[1]);
     else if(val[0]=="onehot_with_correl")
       onehot_with_correl=tobool(val[1]);
     // TODO see what is onehot_with_correl
+    // Apparently it isn't used anyway.
     else PLERROR("unknown argument: %s ",val[0].c_str());
   }
   if(source_fn=="")
@@ -61,7 +62,7 @@ void FieldConvertCommand::run(const vector<string> & args)
  // manual map between field index and types
   map<int,FieldType> force;
 
-  real beta_hat,student;
+  real beta_hat,student=-1;
 
   VMat vm = getDataSet(source_fn);
  
@@ -103,17 +104,17 @@ void FieldConvertCommand::run(const vector<string> & args)
   ofstream report(report_fn.c_str());
   out<<"<SOURCES>\n"+source_fn+"\n</SOURCES>\n<PROCESSING>\n";
 
-  // process each field
+  // Process each field.
   for(int i=0;i<vm->width();i++)
   {
-    type=unknown;
+    type=unknown; // At the beginning we don't know the type.
     beta_hat=0;
     string message;
     int action = 0;
-    int count= sc[i].getCounts()->size()-1;
+    int count = sc[i].getCounts()->size()-1; // Number of unique values.
 
     // is this field's type forced ?
-    if(force.find(i)!=force.end())
+    if(force.find(i) != force.end())
       type = force[i];
     else if(i==target)
       // add target ONLY at the end of the process 
@@ -129,15 +130,22 @@ void FieldConvertCommand::run(const vector<string> & args)
       }
 
       // test for constant values
-      else if(count<=1)
-        if(sc[i].nmissing()>0 && sc[i].nmissing()<vm->length())
-          type=weird; // TODO see what this is exactly
+      else if(count<=1) {
+        if(sc[i].nmissing()>0 && sc[i].nmissing()<vm->length()) {
+          // This case actually never occurs in the Bell database.
+          // That's why we leave it to the user.
+          message = "Constant field, but there are " + tostring(sc[i].nmissing()) +
+            " missing values. Force the type, or modify this program !";
+        }
         else 
+          // Either there is no missing value, or they are all missing.
           type=constant;
+      }
       else if(sc[i].max()>-1000 && vm->getStringToRealMapping(i).size()>0)
         message="Field uses both string map & numerical values";
-      else if(sc[i].min()>19700000 && sc[i].max()<20040000)
-        message="Looks like a date. edit the source file to change the 'TextFilesVMatrix' field type (use jdate). otherwise, edit force.txt to confirm so other type.";
+      else if(sc[i].min()>19700000 && sc[i].max()<20080000)
+        // Could be a date between 1970 and 2008.
+        message="Looks like a date. Edit the source file to change the 'TextFilesVMatrix' field type (use jdate). Otherwise, edit force.txt to force the type.";
       
       // Test whether there are enough unique values to assume continuous data (having a string map implies discrete data)
       else if((count >= MIN( UNIQUE_NMISSING_FRACTION_TO_ASSUME_CONTINUOUS * sc[i].nnonmissing(), 2000)) 
@@ -148,84 +156,76 @@ void FieldConvertCommand::run(const vector<string> & args)
         for(int j=0;j<vm->length();j++)
         {
           real val = vm->get(j,i);
-          if(!is_missing(val) && ((val-(int)val)>0))
+          if(!is_missing(val) && ((val-(int)val) != 0))
           {
             type=continuous;
             break;
           }
         }
       }
+
       // if the data doesn't look continuous (small numb. of unique 
       // values and no fractional parts), 'type' still equals unknown.
       if(type==unknown && message=="")
-
-        // nevermind
-
-//          if(vm->getStringToRealMapping(i).size()>0)
-//            // if there is a string mapping, let the user find out by himself
-//            // (i.e. : maybe there exists an ordering for the strings
-//            // but the computer cannot find out by itself)
-//            message = "has string mapping. (maybe strings have a relevant sorting, but the mappings are unordered). Count:"+tostring(count);
-//        else
-
+      {
         // perform correlation test
-        {
-          real sigma_hat=0,sigma_beta_hat=0;
-          real xmean = sc[i].mean();
-          real ymean = sc[target].mean();
-          real x_minus_xmean_square=0;
-          
-          int len_nm = (int)sc[i].nnonmissing();
-          int len = vm->length();
-          
-          Vec x(vm->length());
-          Vec y(vm->length());
-          vm->getColumn(i, x);
-          vm->getColumn(target, y);
-          
-          // compute beta-hat
-          for(int j=0;j<len;j++)
-            if(!is_missing(x[j]))
-            {
-              beta_hat += (x[j] - xmean) * (y[j] - ymean);
-              x_minus_xmean_square += (x[j] - xmean)*(x[j]-xmean);
-            }
-          
-          beta_hat /= x_minus_xmean_square;
-          
-          // compute sigma-hat
-          for(int j=0;j<len;j++)
-            if(!is_missing(x[j]))
-               sigma_hat += square(y[j]-ymean - beta_hat*(x[j]-xmean));
-          sigma_hat /= len_nm-2;
+        real sigma_hat=0,sigma_beta_hat=0;
+        real xmean = sc[i].mean();
+        real ymean = sc[target].mean();
+        real x_minus_xmean_square=0;
 
-          sigma_beta_hat = sigma_hat / x_minus_xmean_square;
+        int len_nm = (int)sc[i].nnonmissing();
+        int len = vm->length();
+
+        Vec x(len);
+        Vec y(len);
+        vm->getColumn(i, x);
+        vm->getColumn(target, y);
           
-          real t = beta_hat / sqrt(sigma_beta_hat);
-          
-          student = 2 * student_t_cdf(-fabs(t), len_nm-2);
-          if(student < PVALUE_THRESHOLD)
+        // compute beta-hat
+        for(int j=0;j<len;j++)
+          if(!is_missing(x[j]))
           {
-            // then assume data is discrete but correlated
-            type=discrete_corr; // TODO Note that this was 5
-            cout<<"##"<<i<<": nmiss:"<<sc[i].nnonmissing()<<" b:"<<beta_hat<<" sigma_beta_hat:"<<sigma_beta_hat<<" T:"<<student<<endl;
+            real xdiff = x[j] - xmean;
+            beta_hat += xdiff * (y[j] - ymean);
+            x_minus_xmean_square += xdiff * xdiff;
           }
+          
+        beta_hat /= x_minus_xmean_square;
+
+        // compute sigma-hat
+        for(int j=0;j<len;j++)
+          if(!is_missing(x[j]))
+            sigma_hat += square(y[j]-ymean - beta_hat*(x[j]-xmean));
+        sigma_hat /= len_nm-2;
+
+        sigma_beta_hat = sigma_hat / x_minus_xmean_square;
+
+        real t = beta_hat / sqrt(sigma_beta_hat);
+
+        student = 2 * student_t_cdf(-fabs(t), len_nm-2);
+        if(student < PVALUE_THRESHOLD)
+        {
+          // then assume data is discrete but correlated
+          type = discrete_corr;
+          // cout<<"##"<<i<<": nonmiss:"<<sc[i].nnonmissing()<<" b:"<<beta_hat<<" sigma_beta_hat:"<<sigma_beta_hat<<" T:"<<student<<endl;
         }
-      
-      // if we're still not sure (that is type==unknown && message=="")
+      }
+
+      // If we're still not sure (that is to say, type==unknown && message=="").
       if(type==unknown && message=="")
         // is data 'uncorrelated + discrete + sparse'? Yes : Flag 
         if((float)(sc[i].max()-sc[i].min()+1) > (float)(count)*2 ) {
           type=continuous;
-          cout << "Hmm, not sure what that means exactly" << endl;
+          // cout << "Uncorrelated + discrete + sparse: " << i << " (max = " << sc[i].max() << ", min = " << sc[i].min() << ", count = " << count << ")" << endl;
         }
         else if((float)(sc[i].max()-sc[i].min()+1) != (float)(count) )
           message = "(edit force.txt): Data is made of a semi-sparse (density<50%) distribution of integers (uncorrelated with target). max: "+tostring(sc[i].max())+" min:"+tostring(sc[i].min())+" count:"+tostring(count);
         else {
           // data is discrete, not sparse, and not correlated to target,
           // then simply make it as onehot
-          type = discrete_uncorr; // TODO Note that this was 2
-          cout << "Hmm, not sure either here" << endl;
+          type = discrete_uncorr;
+          // cout << "Discrete uncorrelated: " << i << endl;
         }
     }
 
@@ -233,7 +233,7 @@ void FieldConvertCommand::run(const vector<string> & args)
 
     if(type==unknown)
       cout<<tostring(i)+" ("+vm->fieldName(i)+") "<<message<<endl;
-    else if(type==continuous || type==weird)
+    else if(type==continuous)
     {
       action |= NORMALIZE;
       if(sc[i].nmissing()>0)
@@ -256,9 +256,6 @@ void FieldConvertCommand::run(const vector<string> & args)
       if(sc[i].nmissing()>0)
         action |= MISSING_BIT;
     }
-    else if(type==weird)
-      // TODO Note that this line is probably never executed
-      action=MISSING_BIT;
     
     // perform actions
     
@@ -269,7 +266,7 @@ void FieldConvertCommand::run(const vector<string> & args)
       {
         // find out 'mode' of the distribution, if any
         double maxi=-1;
-        real missingval;
+        real missingval = -1;
         for(map<real,StatsCollectorCounts>::iterator it = sc[i].getCounts()->begin(); it!=sc[i].getCounts()->end(); ++it)
           if(it->second.n > maxi)
           {
@@ -277,7 +274,12 @@ void FieldConvertCommand::run(const vector<string> & args)
             missingval=it->first;
           }
         if(maxi<10)
+          // The most frequent value appears less than 10 times: a missing value is replaced by the mean.
           missingval=sc[i].mean();
+        else {
+          // We replace a missing value by the most frequent value.
+          // cout << i << ": maxi >= 10, and missingval = " << missingval << endl;
+        }
         
         out<<"@"<<vm->fieldName(i)<<" isnan "<<missingval<<" @"<<vm->fieldName(i)<<" ifelse "<<sc[i].mean()<<" - "<<sc[i].stddev()<<" / :"<<vm->fieldName(i)<<"\n";      
       }
@@ -323,8 +325,6 @@ PLearn::FieldConvertCommand::FieldType FieldConvertCommand::stringToFieldType(st
     return constant;
   else if (s == "skip")
     return skip;
-  else if (s == "weird")
-    return weird;
   else {
     PLERROR("In FieldConvertCommand::stringToFieldType Unknown field type: %s",s.c_str());
     return skip;
