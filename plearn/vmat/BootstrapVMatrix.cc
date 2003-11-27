343f3cb288aa2a37082e16aa19039d6762a88e4e
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2003-11-27 14:27:58 +0000

    Performs a kind of bootstrap sampling on its parent VMatrix

---
+++ plearn/vmat/BootstrapVMatrix.cc
@@ -0,0 +1,86 @@
+// -*- C++ -*-
+
+// PLearn (A C++ Machine Learning Library)
+// Copyright (C) 2003 Olivier Delalleau
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+
+/* *******************************************************      
+   * $Id: BootstrapVMatrix.cc,v 1.1 2003/11/27 14:27:58 tihocan Exp $
+   ******************************************************* */
+
+#include "BootstrapVMatrix.h"
+#include "random.h"
+
+namespace PLearn <%
+using namespace std;
+
+/** BootstrapVMatrix **/
+
+PLEARN_IMPLEMENT_OBJECT(BootstrapVMatrix,
+    "A VMatrix that sees a bootstrap subset of its parent VMatrix.\n"
+    "This is not a real bootstrap since a sample can only appear once."
+    , 
+    "The only option to specify is \"distr\"(and possibly \"frac\")."
+);
+
+BootstrapVMatrix::BootstrapVMatrix() : frac(0.6667)
+{}
+
+void BootstrapVMatrix::declareOptions(OptionList &ol)
+{
+    declareOption(ol, "frac", &BootstrapVMatrix::frac, OptionBase::buildoption,
+        "    The fraction of elements we keep (default = 0.6667)");
+    inherited::declareOptions(ol);
+}
+
+///////////
+// build //
+///////////
+void BootstrapVMatrix::build()
+{
+  inherited::build();
+  build_();
+}
+
+////////////
+// build_ //
+////////////
+void BootstrapVMatrix::build_()
+{
+  indices = TVec<int>(0, distr.length()-1, 1); // Range-vector
+  shuffleElements(indices);
+  indices = indices.subVec(0,int(frac * distr.length()));
+  sortElements(indices);
+  // Because we changed the indices, a rebuild may be needed.
+  inherited::build();
+}
+
+%> // end of namespcae PLearn
