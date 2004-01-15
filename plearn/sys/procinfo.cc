15dfc47d9260182c35457aad0dae1e008e73f9ee
chapados <chapados@15d43a8f-0efa-0310-bc34-a8a306da697e>
2004-01-15 18:41:53 +0000

    *** empty log message ***

---
+++ plearn/sys/procinfo.cc
@@ -0,0 +1,23 @@
+
+#include <cstdio>
+#include <cstdlib>
+#include "procinfo.h"
+#include "plerror.h"
+
+namespace PLearn <%
+
+int getSystemTotalMemory()
+{
+  int memory_size=0;
+  char units[1000];
+  FILE* p=popen("grep MemTotal /proc/meminfo","r");
+  fscanf(p,"%*s %d %s",&memory_size,units);
+  if (strcmp(units,"kB")==0)
+    memory_size*=1024;
+  else 
+    PLERROR("getSystemTotalMemory: unknown memory units %s",units);
+  pclose(p);
+  return memory_size;
+}
+
+%> // end of namespace PLearn
---
+++ plearn/sys/procinfo.h
@@ -0,0 +1,11 @@
+
+#ifndef MODULE_PROCINFO
+#define MODULE_PROCINFO
+
+namespace PLearn <%
+
+int getSystemTotalMemory();
+
+%> // end of namespace PLearn
+
+#endif
