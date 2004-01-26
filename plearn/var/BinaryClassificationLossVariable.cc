d31b1ac1b9650405b3f87768f090b4c22903a4fb
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2004-01-26 14:15:52 +0000

    A class error function for 1-dimensional output binary classifier

---
+++ plearn/var/BinaryClassificationLossVariable.cc
@@ -0,0 +1,78 @@
+// -*- C++ -*-
+
+// PLearn (A C++ Machine Learning Library)
+// Copyright (C) 1998 Pascal Vincent
+// Copyright (C) 1999-2002 Pascal Vincent, Yoshua Bengio, Rejean Ducharme and University of Montreal
+// Copyright (C) 2001-2002 Nicolas Chapados, Ichiro Takeuchi, Jean-Sebastien Senecal
+// Copyright (C) 2002 Xiangdong Wang, Christian Dorion
+// Copyright (C) 2004 Olivier Delalleau
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+
+/* *******************************************************      
+   * $Id: BinaryClassificationLossVariable.cc,v 1.1 2004/01/26 14:15:52 tihocan Exp $
+   * This file is part of the PLearn library.
+   ******************************************************* */
+
+#include "BinaryClassificationLossVariable.h"
+namespace PLearn <%
+using namespace std;
+
+/** BinaryClassificationLossVariable **/
+
+PLEARN_IMPLEMENT_OBJECT(BinaryClassificationLossVariable, "ONE LINE DESCR", "NO HELP");
+
+BinaryClassificationLossVariable::BinaryClassificationLossVariable(Variable* netout, Variable* classnum)
+  :BinaryVariable(netout,classnum,1,1)
+{
+  if(!classnum->isScalar())
+    PLERROR("In BinaryClassificationLossVariable: classnum must be a scalar variable representing an index of netout (typically a class number)");
+}
+
+
+void BinaryClassificationLossVariable::recomputeSize(int& l, int& w) const
+{ l=1, w=1; }
+
+
+void BinaryClassificationLossVariable::fprop()
+{
+  int classnum = int(input2->valuedata[0]);
+  int outputclass;
+  if (input1->valuedata[0] <= 0.5) {
+    outputclass = 0;
+  } else {
+    outputclass = 1;
+  }
+  
+  valuedata[0] = (outputclass == classnum ?0 :1);
+}
+
+%> // end of namespace PLearn
+
---
+++ plearn/var/BinaryClassificationLossVariable.h
@@ -0,0 +1,83 @@
+// -*- C++ -*-
+
+// PLearn (A C++ Machine Learning Library)
+// Copyright (C) 1998 Pascal Vincent
+// Copyright (C) 1999-2002 Pascal Vincent, Yoshua Bengio, Rejean Ducharme and University of Montreal
+// Copyright (C) 2001-2002 Nicolas Chapados, Ichiro Takeuchi, Jean-Sebastien Senecal
+// Copyright (C) 2002 Xiangdong Wang, Christian Dorion
+// Copyright (C) 2004 Olivier Delalleau
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+
+/* *******************************************************      
+   * $Id: BinaryClassificationLossVariable.h,v 1.1 2004/01/26 14:15:52 tihocan Exp $
+   * This file is part of the PLearn library.
+   ******************************************************* */
+
+#ifndef BinaryClassificationLossVariable_INC
+#define BinaryClassificationLossVariable_INC
+
+#include "BinaryVariable.h"
+
+namespace PLearn <%
+using namespace std;
+
+
+//! For one-dimensional output: class is 0 if output < 0.5, and 1 if >= 0.5.
+class BinaryClassificationLossVariable: public BinaryVariable
+{
+protected:
+  typedef BinaryVariable inherited;
+  //!  Default constructor for persistence
+  BinaryClassificationLossVariable() {}
+
+public:
+  BinaryClassificationLossVariable(Variable* netout, Variable* classnum);
+  PLEARN_DECLARE_OBJECT(BinaryClassificationLossVariable);
+  virtual void recomputeSize(int& l, int& w) const;
+  virtual void fprop();
+  //! can't bprop through a hard classification error...
+  virtual void bprop() {}
+};
+
+inline Var binary_classification_loss(Var network_output, Var classnum)
+{ 
+  if (classnum->isScalar()) {
+    return new BinaryClassificationLossVariable(network_output, classnum); 
+  } else {
+    PLERROR("In binary_classification_loss : Can only be used with a scalar output");
+    // The line below is just for the compiler to stop complaining.
+    return new BinaryClassificationLossVariable(network_output, classnum);
+  }
+}
+
+%> // end of namespace PLearn
+
+#endif 
