fbecffbe78a129dd1355f066555668374f200f49
nova77 <nova77@15d43a8f-0efa-0310-bc34-a8a306da697e>
2004-02-26 05:17:59 +0000

    Added for WIN32 compatibility

---
+++ plearn/base/ms_hash_wrapper.cpp
@@ -0,0 +1,15 @@
+
+// dummy file: just as a test..
+#include <string>
+#include "ms_hash_wrapper.h"
+
+//template<class T>
+//struct hash<PLearn::TVec<T> >
+//{
+//	size_t operator()(PLearn::TVec<T> v) const { return hash<T>()(sumsquare(v));} 
+//};
+
+//stdext::HashFunction<double>(10.0){};
+
+//SET_HASH_FUNCTION(std::string, const char*, a, a.c_str())
+//SET_HASH_FUNCTION(double, T, val, sqrt(val))
---
+++ plearn/base/ms_hash_wrapper.h
@@ -0,0 +1,300 @@
+// -*- C++ -*-
+
+// ms_hash_wrapper Works as a wrapper between MS (VS .NET 2003) hashing functions
+//                 and GCC plearn (gcc inspired) hashing specializations.
+// Copyright (C) 2004 Norman Casagrande
+//
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+#ifndef ms_hash_wrapper_H
+#define ms_hash_wrapper_H
+
+#ifdef __GNUC__
+
+#include <ext/hash_set> //to get stl_hash_fun.h ... (template<> class hash)
+#include <ext/hash_map> //to get stl_hash_fun.h ... (template<> class hash)
+
+#if __GNUC__==3 && __GNUC_MINOR__>0
+namespace __gnu_cxx {
+#else
+namespace std {
+#endif
+
+// Three original cases for hash specialization and declaration:
+
+// In file pl_hash_fun:
+// template<>
+// struct hash<float>
+// {
+//   size_t operator()(float x) const { return PLearn::hashval(x); }
+// };
+// NOW IS: SET_HASH_WITH_FUNCTION
+
+// -------------------------------------------------------------------
+
+// In file pl_hash_fun:
+// template<>
+// struct hash<string>
+// {
+//   size_t operator()(const string& __s) const { return hash<const char*>()(__s.c_str()); }
+// };
+// NOW IS: SET_HASH_WITH_INHERITANCE
+
+// -------------------------------------------------------------------
+
+// In file TMat_maths_impl.h:
+// template<class T>
+// struct hash<PLearn::TVec<T> >
+// {
+//   size_t operator()(PLearn::TVec<T> v) const { return hash<T>()(sumsquare(v));} 
+// };
+// NOW IS: SET_HASH_FUNCTION
+
+// -------------------------------------------------------------------
+// -------------------------------------------------------------------
+// -------------------------------------------------------------------
+
+//////////////////////////////////////////////////////////////////////////////
+//////// GCC Version /////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+// WARNING: paramName should be included in hashFunc!	
+// Example: 
+//     SET_HASH_WITH_FUNCTION(float, val, PLearn::hashval(val))
+// --> Will return (see the function operator() below): 
+//     size_t operator()(const float& val) const
+//     { 
+//        return (size_t)(PLearn::hashval(val));
+//     } 
+#define SET_HASH_WITH_FUNCTION(type, paramName, hashFunc)						\
+	template<>																	\
+	struct hash< type >															\
+	{																			\
+		size_t operator()(type& paramName) const								\
+		{																		\
+			return (size_t)(hashFunc);											\
+		}																		\
+	};										
+
+// Example: 
+//     SET_HASH_WITH_INHERITANCE(std::string, const char*, val, val.c_str())
+// --> Will return (see the function operator() below): 
+//   size_t operator()(const std::string& val) const 
+//   { 
+//     return hash<const char*>()val.c_str()); 
+//   } 
+#define SET_HASH_WITH_INHERITANCE(type, originaltype, paramName, hashFunc)		\
+	template<>																	\
+	struct hash< type >															\
+	{																			\
+		size_t operator()(type& paramName) const								\
+		{																		\
+			return hash< originalType >()(hashFunc);							\
+		}																		\
+	};										
+
+
+// WARNING: paramName should be included in hashFunc!	
+// Example: 
+//     SET_HASH_FUNCTION(PLearn::TVec<T>, T, val, sqrt(val))
+// --> Will return:
+//   template <class T >
+//   struct hash< PLearn::TVec<T> >
+//   {
+//      size_t operator()(PLearn::TVec<T>& val) const 
+//      {								
+//         return hash< T >()(sqrt(val));											
+//      }	
+//   }
+// See below: size_t operator()(const type& paramName) const 
+#define SET_HASH_FUNCTION(type, templateClass, paramName, hashFunc)				\
+	template<class templateClass >												\
+	struct hash< type >															\
+	{																			\
+		size_t operator()(type& paramName) const							\
+		{																		\
+			return hash< templateClass >()(hashFunc);							\
+		}																		\
+	};										
+
+#endif // __GNUC__
+
+//////////////////////////////////////////////////////////////////////////////
+//////// WIN32 Version ///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+#ifdef WIN32
+
+#include <hash_set>
+#include <hash_map>
+
+namespace std {
+
+// WARNING: paramName should be included in hashFunc!	
+// Example: 
+//     SET_HASH_WITH_FUNCTION(float, val, PLearn::hashval(val))
+// --> Will return (see the function operator() below): 
+//     size_t operator()(const float& val) const
+//     { 
+//        return (size_t)(PLearn::hashval(val));
+//     } 
+#define SET_HASH_WITH_FUNCTION(type, paramName, hashFunc)						\
+	template<class _Kty>														\
+	class stdext::hash_compare<type, std::less<_Kty> >							\
+	{																			\
+	public:																		\
+		enum																	\
+		{	/* parameters for hash table */										\
+			bucket_size = 4,	/* 0 < bucket_size */							\
+			min_buckets = 8};	/* min_buckets = 2 ^^ N, 0 < N */				\
+																				\
+			hash_compare()														\
+				: comp()														\
+			{	/* construct with default comparator */							\
+			}																	\
+																				\
+			hash_compare(std::less<_Kty> _Pred)									\
+				: comp(_Pred)													\
+			{	/* construct with _Pred comparator */							\
+			}																	\
+																				\
+			size_t operator()(const type& paramName) const						\
+			{	/* hash _Keyval to size_t value */								\
+				return (size_t)(hashFunc);										\
+			}																	\
+																				\
+			bool operator()(const _Kty& _Keyval1, const _Kty& _Keyval2) const	\
+			{	/* test if _Keyval1 ordered before _Keyval2 */					\
+				return (comp(_Keyval1, _Keyval2));								\
+			}																	\
+																				\
+	protected:																	\
+		std::less<_Kty> comp;	/* the comparator object */						\
+																				\
+	}; 
+
+// WARNING: paramName should be included in hashFunc!	
+//     SET_HASH_WITH_INHERITANCE(std::string, const char*, val, val.c_str())
+// --> Will return (see the function operator() below): 
+//   size_t operator()(const std::string& val) const 
+//   { 
+//     return stdext::hash_compare<const char*>()val.c_str()); 
+//   } 
+#define SET_HASH_WITH_INHERITANCE(type, originalType, paramName, hashFunc)		\
+	template<class _Kty>														\
+	class stdext::hash_compare<type, std::less<_Kty> >							\
+	{																			\
+	public:																		\
+		enum																	\
+		{	/* parameters for hash table */										\
+			bucket_size = 4,	/* 0 < bucket_size */							\
+			min_buckets = 8};	/* min_buckets = 2 ^^ N, 0 < N */				\
+																				\
+			hash_compare()														\
+				: comp()														\
+			{	/* construct with default comparator */							\
+			}																	\
+																				\
+			hash_compare(std::less<_Kty> _Pred)									\
+				: comp(_Pred)													\
+			{	/* construct with _Pred comparator */							\
+			}																	\
+																				\
+			size_t operator()(const type& paramName) const						\
+			{	/* hash _Keyval to size_t value */								\
+				return stdext::hash_compare< originalType >()(hashFunc);		\
+			}																	\
+																				\
+			bool operator()(const _Kty& _Keyval1, const _Kty& _Keyval2) const	\
+			{	/* test if _Keyval1 ordered before _Keyval2 */					\
+				return (comp(_Keyval1, _Keyval2));								\
+			}																	\
+																				\
+	protected:																	\
+		std::less<_Kty> comp;	/* the comparator object */						\
+																				\
+	}; 
+
+// WARNING: paramName should be included in hashFunc!	
+// Example: 
+//     SET_HASH_FUNCTION(PLearn::TVec<T>, T, val, sqrt(val))
+// --> Will return:
+//   template <class T >
+//   class stdext::hash_compare<PLearn::TVec<T>, std::less< T > >				
+//   {
+//   //// Some other stuffs...
+//      size_t operator()(PLearn::TVec<T>& val) const 
+//      {								
+//          return stdext::hash_compare< T >()(sqrt(val)); 
+//      }	
+//   //// Some other stuffs...
+//   }
+// See below: size_t operator()(const type& paramName) const 
+#define SET_HASH_FUNCTION(type, templateClass, paramName, hashFunc)				\
+	template<class templateClass >												\
+	class stdext::hash_compare<type, std::less< templateClass > >				\
+	{																			\
+	public:																		\
+		enum																	\
+		{	/* parameters for hash table */										\
+			bucket_size = 4,	/* 0 < bucket_size */							\
+			min_buckets = 8};	/* min_buckets = 2 ^^ N, 0 < N */				\
+																				\
+			hash_compare()														\
+				: comp()														\
+			{	/* construct with default comparator */							\
+			}																	\
+																				\
+			hash_compare(std::less< templateClass > _Pred)						\
+				: comp(_Pred)													\
+			{	/* construct with _Pred comparator */							\
+			}																	\
+																				\
+			size_t operator()(const type& paramName) const						\
+			{	/* hash _Keyval to size_t value */								\
+				return stdext::hash_compare< templateClass >()(hashFunc);		\
+			}																	\
+																				\
+			bool operator()(const templateClass& _Keyval1, const templateClass& _Keyval2) const	\
+			{	/* test if _Keyval1 ordered before _Keyval2 */					\
+				return (comp(_Keyval1, _Keyval2));								\
+			}																	\
+																				\
+	protected:																	\
+		std::less< templateClass > comp;	/* the comparator object */			\
+																				\
+	}; 
+
+
+#endif // WIN32 
+
+}
+
+#endif // ms_hash_wrapper_H
