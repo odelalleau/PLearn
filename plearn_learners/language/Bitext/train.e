66d9c5d1822196d39a6d82232eca49f24935238b
kermorvc <kermorvc@15d43a8f-0efa-0310-bc34-a8a306da697e>
2004-03-23 19:21:39 +0000

    Add bitext directory. It contains tools to prepare data for learning senses from bitexts

---
+++ plearn_learners/language/Bitext/train.e
@@ -0,0 +1,20 @@
+tabling of document
+house of commons
+thursday , april 17 , 1986
+a house meet at 11 a.m.
+statement by minister
+routine proceeding
+petition
+government response
+canadian charter of right and freedom
+fourth anniversary of proclamation
+mr. speaker , my government have demonstrate my support for that important principle by take step to enforce a provision of a charter more vigourously .
+I take a initiative in assess and amend current legislation and policy to ensure that I reflect a broad interpretation of a charter .
+in do so , I have try to rise above a conventional political option and take a new and broad view .
+for example , last december new initiative and increased funding be announce to improve a status of disabled person .
+mr. speaker , i be proud of a role play by canadians in carry out that difficult task , which consist in define a mechanism for bring about equality and social justice in canada .
+I be commit to a continuation of that process of consultation because I believe canadians have a right to participate in a development of policy that fundamentally affect my life .
+there be no doubt that a charter be affect all of my life .
+I require court and government to be ever aware of a need to balance individual and societal right in a fashion which facilitate society 's interest with a least amount of restriction upon individual right .
+a charter be give individual a new opportunity to seek redress when my right be infringe upon .
+I also impose on all a obligation to be tolerant of a right of others .
---
+++ plearn_learners/language/Bitext/train.f
@@ -0,0 +1,20 @@
+pétition
+chambre du commune
+le jeudi 17 avril 1986
+déclaration du ministre
+le séance être ouvrir à 11 heure .
+affaire courant
+pétition
+réponse du gouvernement
+le charte canadien du droit et liberté
+quatrième anniversaire de le proclamation
+monsieur le président , notre gouvernement avoir prouver mon adhésion à ce important principe en prendre du mesure pour appliquer plus systématiquement le précepte de le charte .
+je avoir prendre le initiative de évaluer et de modifier un loi et du politique en vigueur afin que je correspondre à un interprétation généreux de le charte .
+ce faire , je avoir chercher à dépasser le option politique traditionnel pour me ouvrir à de nouveau perspective .
+ainsi , en décembre dernier , je annoncer de nouveau initiative et le déblocage de crédit en vue de améliorer le sort du handicapé .
+monsieur le président , je être fier du rôle que le canadien avoir jouer dans ce tâche difficile qui consister à définir le moyen à mettre en oeuvre pour réaliser le égalité et le justice social au canada .
+je tenir ferme au processus de consultation , car je croire que le canadien avoir le droit de prendre part à le mise au point du politique qui me toucher de près .
+je être indéniable que le charte toucher personnellement chacun de moi .
+je obliger le tribunal et le gouvernement à rechercher constamment le équilibre entre le droit individuel et collectif de manière à favoriser le intérêt de le société sans pour autant brimer le droit du particulier .
+le charte accorder au particulier de nouveau moyen de faire valoir notre droit si je estimer que je y avoir porter atteindre .
+je imposer également à tout le tolérance à le égard du droit du autre .
