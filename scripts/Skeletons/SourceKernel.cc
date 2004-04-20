68eb57d12b93d2fb61a7e768f90960123efa295a
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2004-04-20 20:38:54 +0000

    Skeleton for a SourceKernel

---
+++ scripts/Skeletons/SourceKernel.cc
@@ -0,0 +1,153 @@
+#include "DERIVEDCLASS.h"
+
+namespace PLearn {
+using namespace std;
+
+//////////////////
+// DERIVEDCLASS //
+//////////////////
+DERIVEDCLASS::DERIVEDCLASS() 
+/* ### Initialize all fields to their default value here */
+{
+  // ...
+
+  // ### You may or may not want to call build_() to finish building the object
+  // build_();
+}
+
+PLEARN_IMPLEMENT_OBJECT(DERIVEDCLASS,
+    "ONE LINE DESCRIPTION",
+    "MULTI-LINE \nHELP"
+);
+
+////////////////////
+// declareOptions //
+////////////////////
+void DERIVEDCLASS::declareOptions(OptionList& ol)
+{
+  // ### Declare all of this object's options here
+  // ### For the "flags" of each option, you should typically specify  
+  // ### one of OptionBase::buildoption, OptionBase::learntoption or 
+  // ### OptionBase::tuningoption. Another possible flag to be combined with
+  // ### is OptionBase::nosave
+
+  // ### ex:
+  // declareOption(ol, "myoption", &DERIVEDCLASS::myoption, OptionBase::buildoption,
+  //               "Help text describing this option");
+  // ...
+
+  // Now call the parent class' declareOptions
+  inherited::declareOptions(ol);
+}
+
+///////////
+// build //
+///////////
+void DERIVEDCLASS::build()
+{
+  // ### Nothing to add here, simply calls build_
+  inherited::build();
+  build_();
+}
+
+////////////
+// build_ //
+////////////
+void DERIVEDCLASS::build_()
+{
+  // ### This method should do the real building of the object,
+  // ### according to set 'options', in *any* situation. 
+  // ### Typical situations include:
+  // ###  - Initial building of an object from a few user-specified options
+  // ###  - Building of a "reloaded" object: i.e. from the complete set of all serialised options.
+  // ###  - Updating or "re-building" of an object after a few "tuning" options have been modified.
+  // ### You should assume that the parent class' build_() has already been called.
+
+  // Since a SourceKernel takes its 'is_symmetric' option from the underlying kernel,
+  // you should modify it here if you want it to be different.
+}
+
+///////////////////////
+// computeGramMatrix //
+///////////////////////
+void DERIVEDCLASS::computeGramMatrix(Mat K) const {
+  // Default = uses the Kernel implementation.
+  Kernel::computeGramMatrix(K);
+}
+
+//////////////
+// evaluate //
+//////////////
+real DERIVEDCLASS::evaluate(const Vec& x1, const Vec& x2) const {
+  // ### Evaluate the kernel on a pair of points.
+  // TODO: Implement.
+}
+
+//////////////////
+// evaluate_i_j //
+//////////////////
+real DERIVEDCLASS::evaluate_i_j(int i, int j) const {
+  // Default = uses the Kernel implementation.
+  return Kernel::evaluate_i_j(i,j);
+}
+
+//////////////////
+// evaluate_i_x //
+//////////////////
+real DERIVEDCLASS::evaluate_i_x(int i, const Vec& x, real squared_norm_of_x) const {
+  // Default = uses the Kernel implementation.
+  return Kernel::evaluate_i_x(i, x, squared_norm_of_x);
+}
+
+////////////////////////
+// evaluate_i_x_again //
+////////////////////////
+real DERIVEDCLASS::evaluate_i_x_again(int i, const Vec& x, real squared_norm_of_x, bool first_time) {
+  // Default = uses the Kernel implementation.
+  return Kernel::evaluate_i_x_again(i, x, squared_norm_of_x, first_time);
+}
+
+//////////////////
+// evaluate_x_i //
+//////////////////
+real DERIVEDCLASS::evaluate_x_i(const Vec& x, int i, real squared_norm_of_x) const {
+  // Default = uses the Kernel implementation.
+  return Kernel::evaluate_x_i(x, i, squared_norm_of_x);
+}
+
+////////////////////////
+// evaluate_x_i_again //
+////////////////////////
+real DERIVEDCLASS::evaluate_x_i_again(const Vec& x, int i, real squared_norm_of_x, bool first_time) {
+  // Default = uses the Kernel implementation.
+  return Kernel::evaluate_x_i_again(x, i, squared_norm_of_x, first_time);
+}
+
+/////////////////////////////////
+// makeDeepCopyFromShallowCopy //
+/////////////////////////////////
+void DERIVEDCLASS::makeDeepCopyFromShallowCopy(map<const void*, void*>& copies)
+{
+  inherited::makeDeepCopyFromShallowCopy(copies);
+
+  // ### Call deepCopyField on all "pointer-like" fields 
+  // ### that you wish to be deepCopied rather than 
+  // ### shallow-copied.
+  // ### ex:
+  // deepCopyField(trainvec, copies);
+
+  // ### Remove this line when you have fully implemented this method.
+  PLERROR("DERIVEDCLASS::makeDeepCopyFromShallowCopy not fully (correctly) implemented yet!");
+}
+
+/* This method may often need to be overridden in subclasses.
+////////////////////////////
+// setDataForKernelMatrix //
+////////////////////////////
+void DERIVEDCLASS::setDataForKernelMatrix(VMat the_data) {
+  // Do some preprocessing here, for instance.
+}
+*/
+
+} // end of namespace PLearn
+
---
+++ scripts/Skeletons/SourceKernel.h
@@ -0,0 +1,109 @@
+#ifndef DERIVEDCLASS_INC
+#define DERIVEDCLASS_INC
+
+#include "SourceKernel.h"
+
+namespace PLearn {
+using namespace std;
+
+class DERIVEDCLASS: public SourceKernel
+{
+
+private:
+
+  typedef SourceKernel inherited;
+  
+protected:
+
+  // *********************
+  // * Protected options *
+  // *********************
+
+  // ### declare protected option fields (such as learnt parameters) here
+  // ...
+    
+public:
+
+  // ************************
+  // * Public build options *
+  // ************************
+
+  // ### declare public option fields (such as build options) here
+  // ...
+
+  // ****************
+  // * Constructors *
+  // ****************
+
+  //! Default constructor.
+  // Make sure the implementation in the .cc initializes all fields to
+  // reasonable default values.
+  DERIVEDCLASS();
+
+  // ************************
+  // * SourceKernel methods *
+  // ************************
+
+private: 
+
+  //! This does the actual building. 
+  // (Please implement in .cc)
+  void build_();
+
+protected: 
+  
+  //! Declares this class' options.
+  // (Please implement in .cc)
+  static void declareOptions(OptionList& ol);
+
+public:
+
+  // ************************
+  // **** Object methods ****
+  // ************************
+
+  //! Simply calls inherited::build() then build_().
+  virtual void build();
+
+  //! Transforms a shallow copy into a deep copy.
+  virtual void makeDeepCopyFromShallowCopy(map<const void*, void*>& copies);
+
+  // Declares other standard object methods.
+  // If your class is not instantiatable (it has pure virtual methods)
+  // you should replace this by PLEARN_DECLARE_ABSTRACT_OBJECT_METHODS.
+  PLEARN_DECLARE_OBJECT(DERIVEDCLASS);
+
+  // ******************************
+  // **** SourceKernel methods ****
+  // ******************************
+
+  //! Overridden.
+  // The default behavior of a SourceKernel is to forward all calls to the
+  // underlying kernel. When writing a subclass, you will probably want to
+  // override the following methods. The only method you really need to implement
+  // is evaluate().
+  virtual real evaluate(const Vec& x1, const Vec& x2) const;
+  virtual real evaluate_i_j(int i, int j) const;
+  virtual real evaluate_i_x(int i, const Vec& x, real squared_norm_of_x=-1) const;
+  virtual real evaluate_x_i(const Vec& x, int i, real squared_norm_of_x=-1) const;
+  virtual real evaluate_i_x_again(int i, const Vec& x, real squared_norm_of_x=-1, bool first_time = false);
+  virtual real evaluate_x_i_again(const Vec& x, int i, real squared_norm_of_x=-1, bool first_time = false);
+  virtual void computeGramMatrix(Mat K) const;
+  
+  // You may also want to override these methods if you don't want them
+  // to be directly forwarded to the underlying kernel.
+  // virtual void setDataForKernelMatrix(VMat the_data);
+  // virtual void addDataForKernelMatrix(const Vec& newRow);
+  // virtual void setParameters(Vec paramvec);
+  // virtual Vec getParameters() const;
+
+};
+
+// Declares a few other classes and functions related to this class.
+DECLARE_OBJECT_PTR(DERIVEDCLASS);
+  
+} // end of namespace PLearn
+
+#endif
+
+
