ebedb7c7fde97cd8cf745b647ef39c9eb175416e
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2004-05-07 19:02:31 +0000

    Skeleton for KernelProjection subclasses

---
+++ scripts/Skeletons/KernelProjection.h
@@ -0,0 +1,93 @@
+#ifndef DERIVEDCLASS_INC
+#define DERIVEDCLASS_INC
+
+#include "KernelProjection.h"
+
+namespace PLearn {
+using namespace std;
+
+class DERIVEDCLASS: public KernelProjection
+{
+
+private:
+
+  typedef KernelProjection inherited;
+  
+protected:
+
+  // *********************
+  // * protected options *
+  // *********************
+
+  // ### declare protected option fields (such as learnt parameters) here
+  // ...
+    
+public:
+
+  // ************************
+  // * public build options *
+  // ************************
+
+  // ### declare public option fields (such as build options) here
+  // ...
+
+  // ****************
+  // * Constructors *
+  // ****************
+
+  //! Default constructor.
+  // (Make sure the implementation in the .cc
+  // initializes all fields to reasonable default values)
+  DERIVEDCLASS();
+
+
+  // ****************************
+  // * KernelProjection methods *
+  // ****************************
+
+private: 
+
+  //! This does the actual building. 
+  // (Please implement in .cc)
+  void build_();
+
+protected: 
+  
+  //! Declares this class' options.
+  // (Please implement in .cc)
+  static void declareOptions(OptionList& ol);
+
+public:
+
+  // ************************
+  // **** Object methods ****
+  // ************************
+
+  //! Simply calls inherited::build() then build_().
+  virtual void build();
+
+  //! Transforms a shallow copy into a deep copy.
+  virtual void makeDeepCopyFromShallowCopy(map<const void*, void*>& copies);
+
+  // Declares other standard object methods.
+  // If your class is not instantiatable (it has pure virtual methods)
+  // you should replace this by PLEARN_DECLARE_ABSTRACT_OBJECT_METHODS.
+  PLEARN_DECLARE_OBJECT(DERIVEDCLASS);
+
+
+  // **********************************
+  // **** KernelProjection methods ****
+  // **********************************
+
+  //! (Re-)initializes the PLearner in its fresh state.
+  // (Default implementation just calls inherited::forget()).
+  virtual void forget();
+
+};
+
+// Declares a few other classes and functions related to this class.
+DECLARE_OBJECT_PTR(DERIVEDCLASS);
+  
+} // end of namespace PLearn
+
+#endif
