c340624253ba9391be19ae6efe10f615430006a2
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2004-05-13 13:14:08 +0000

    Cosmetic changes, and removed now useless help() method

--- scripts/Skeletons/VMatrix.cc
+++ scripts/Skeletons/VMatrix.cc
@@ -3,6 +3,9 @@
 namespace PLearn {
 using namespace std;
 
+//////////////////
+// DERIVEDCLASS //
+//////////////////
 DERIVEDCLASS::DERIVEDCLASS() 
 /* ### Initialise all fields to their default value here */
   {
@@ -12,72 +15,66 @@ DERIVEDCLASS::DERIVEDCLASS()
     // build_();
   }
 
-  PLEARN_IMPLEMENT_OBJECT(DERIVEDCLASS, "ONE LINE DESCR", "NO HELP");
+PLEARN_IMPLEMENT_OBJECT(DERIVEDCLASS,
+    "ONE LINE DESCR",
+    "NO HELP"
+);
 
-  void DERIVEDCLASS::declareOptions(OptionList& ol)
-  {
-    // ### Declare all of this object's options here
-    // ### For the "flags" of each option, you should typically specify  
-    // ### one of OptionBase::buildoption, OptionBase::learntoption or 
-    // ### OptionBase::tuningoption. Another possible flag to be combined with
-    // ### is OptionBase::nosave
+////////////////////
+// declareOptions //
+////////////////////
+void DERIVEDCLASS::declareOptions(OptionList& ol)
+{
+  // ### Declare all of this object's options here
+  // ### For the "flags" of each option, you should typically specify  
+  // ### one of OptionBase::buildoption, OptionBase::learntoption or 
+  // ### OptionBase::tuningoption. Another possible flag to be combined with
+  // ### is OptionBase::nosave
 
-    // ### ex:
-    // declareOption(ol, "myoption", &DERIVEDCLASS::myoption, OptionBase::buildoption,
-    //               "Help text describing this option");
-    // ...
+  // ### ex:
+  // declareOption(ol, "myoption", &DERIVEDCLASS::myoption, OptionBase::buildoption,
+  //               "Help text describing this option");
+  // ...
 
-    // Now call the parent class' declareOptions
-    inherited::declareOptions(ol);
-  }
+  // Now call the parent class' declareOptions
+  inherited::declareOptions(ol);
+}
 
-  string DERIVEDCLASS::help()
-  {
-    // ### Provide some useful description of what the class is ...
-    return 
-      "DERIVEDCLASS implements a ...\n";
-  }
+///////////
+// build //
+///////////
+void DERIVEDCLASS::build()
+{
+  inherited::build();
+  build_();
+}
 
-  void DERIVEDCLASS::build_()
-  {
-    // ### This method should do the real building of the object,
-    // ### according to set 'options', in *any* situation. 
-    // ### Typical situations include:
-    // ###  - Initial building of an object from a few user-specified options
-    // ###  - Building of a "reloaded" object: i.e. from the complete set of all serialised options.
-    // ###  - Updating or "re-building" of an object after a few "tuning" options have been modified.
-    // ### You should assume that the parent class' build_() has already been called.
-  }
+////////////
+// build_ //
+////////////
+void DERIVEDCLASS::build_()
+{
+  // ### This method should do the real building of the object,
+  // ### according to set 'options', in *any* situation. 
+  // ### Typical situations include:
+  // ###  - Initial building of an object from a few user-specified options
+  // ###  - Building of a "reloaded" object: i.e. from the complete set of all serialised options.
+  // ###  - Updating or "re-building" of an object after a few "tuning" options have been modified.
+  // ### You should assume that the parent class' build_() has already been called.
+}
 
-  // ### Nothing to add here, simply calls build_
-  void DERIVEDCLASS::build()
-  {
-    inherited::build();
-    build_();
-  }
-
-
-  void DERIVEDCLASS::makeDeepCopyFromShallowCopy(map<const void*, void*>& copies)
-  {
-    inherited::makeDeepCopyFromShallowCopy(copies);
-
-    // ### Call deepCopyField on all "pointer-like" fields 
-    // ### that you wish to be deepCopied rather than 
-    // ### shallow-copied.
-    // ### ex:
-    // deepCopyField(trainvec, copies);
-
-    // ### Remove this line when you have fully implemented this method.
-    PLERROR("DERIVEDCLASS::makeDeepCopyFromShallowCopy not fully (correctly) implemented yet!");
-  }
-
-
+/////////
+// get //
+/////////
 real DERIVEDCLASS::get(int i, int j) const
 {
   // get element at i-th row, j-th column
   // ...
 }
 
+///////////////
+// getSubRow //
+///////////////
 void DERIVEDCLASS::getSubRow(int i, int j, Vec v) const
 {
   // get part or all of the i-th, starting at the j-th column,
@@ -85,5 +82,21 @@ void DERIVEDCLASS::getSubRow(int i, int j, Vec v) const
   // ...
 }
 
+/////////////////////////////////
+// makeDeepCopyFromShallowCopy //
+/////////////////////////////////
+void DERIVEDCLASS::makeDeepCopyFromShallowCopy(map<const void*, void*>& copies)
+{
+  inherited::makeDeepCopyFromShallowCopy(copies);
+
+  // ### Call deepCopyField on all "pointer-like" fields 
+  // ### that you wish to be deepCopied rather than 
+  // ### shallow-copied.
+  // ### ex:
+  // deepCopyField(trainvec, copies);
+
+  // ### Remove this line when you have fully implemented this method.
+  PLERROR("DERIVEDCLASS::makeDeepCopyFromShallowCopy not fully (correctly) implemented yet!");
+}
 
 } // end of namespace PLearn
