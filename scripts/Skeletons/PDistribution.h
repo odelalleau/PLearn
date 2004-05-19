4656342d30c99a7657302be5b589c0b18b98bc62
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2004-05-19 14:11:48 +0000

    Cosmetic reformatting

--- scripts/Skeletons/PDistribution.h
+++ scripts/Skeletons/PDistribution.h
@@ -14,11 +14,12 @@ private:
   typedef PDistribution inherited;  
 
 protected:
+
   // *********************
   // * protected options *
   // *********************
 
-  // ### declare protected option fields (such as learnt parameters) here
+  // ### Declare protected option fields (such as learnt parameters) here.
   // ...
     
 public:
@@ -27,30 +28,32 @@ public:
   // * public build options *
   // ************************
 
-  // ### declare public option fields (such as build options) here
+  // ### Declare public option fields (such as build options) here.
   // ...
 
   // ****************
   // * Constructors *
   // ****************
 
-  // Default constructor, make sure the implementation in the .cc
-  // initializes all fields to reasonable default values.
+  //! Default constructor.
+  // ### Make sure the implementation in the .cc
+  // ### initializes all fields to reasonable default values.
   DERIVEDCLASS();
 
-
-  // ******************
+  // *************************
   // * PDistribution methods *
-  // ******************
+  // *************************
 
 private: 
+
   //! This does the actual building. 
-  // (Please implement in .cc)
+  // ### Please implement in .cc.
   void build_();
 
 protected: 
-  //! Declares this class' options
-  // (Please implement in .cc)
+
+  //! Declare this class' options.
+  // ### Please implement in .cc.
   static void declareOptions(OptionList& ol);
 
 public:
@@ -59,72 +62,64 @@ public:
   // **** Object methods ****
   // ************************
 
-  //! simply calls inherited::build() then build_() 
+  //! Simply call inherited::build() then build_().
   virtual void build();
 
-  //! Provides a help message describing this class
-  static string help();
-
-  //! Transforms a shallow copy into a deep copy
+  //! Transform a shallow copy into a deep copy.
   virtual void makeDeepCopyFromShallowCopy(map<const void*, void*>& copies);
 
-  // Declares other standard object methods
-  //  If your class is not instantiatable (it has pure virtual methods)
-  // you should replace this by PLEARN_DECLARE_ABSTRACT_OBJECT_METHODS 
+  // Declare other standard object methods.
+  // ### If your class is not instantiatable (it has pure virtual methods)
+  // ### you should replace this by PLEARN_DECLARE_ABSTRACT_OBJECT_METHODS.
   PLEARN_DECLARE_OBJECT(DERIVEDCLASS);
 
-
-  // **************************
+  // *******************************
   // **** PDistribution methods ****
-  // **************************
+  // *******************************
 
-  //! return log of probability density log(p(x))
+  //! Return log of probability density log(p(x)).
   virtual double log_density(const Vec& x) const;
 
-  //! return survival fn = P(X>x)
+  //! Return survival fn = P(X>x).
   virtual double survival_fn(const Vec& x) const;
 
-  //! return survival fn = P(X<x)
+  //! Return survival fn = P(X<x).
   virtual double cdf(const Vec& x) const;
 
-  //! return E[X] 
+  //! Return E[X].
   virtual void expectation(Vec& mu) const;
 
-  //! return Var[X]
+  //! Return Var[X].
   virtual void variance(Mat& cov) const;
 
-  //! Resets the random number generator used by generate using the given seed
+  //! Reset the random number generator used by generate() using the given seed.
   virtual void resetGenerator(long g_seed) const;
 
-  //! return a pseudo-random sample generated from the distribution.
+  //! Return a pseudo-random sample generated from the distribution.
   virtual void generate(Vec& x) const;
 
-  
   // **************************
   // **** Learner methods ****
   // **************************
 
-  // Default version of inputsize returns learner->inputsize()
-  // If this is not appropriate, you should uncomment this and define
-  // it properly in the .cc
+  // ### Default version of inputsize returns learner->inputsize()
+  // ### If this is not appropriate, you should uncomment this and define
+  // ### it properly in the .cc
   // virtual int inputsize() const;
 
-  //! (Re-)initializes the PDistribution in its fresh state (that state may depend on the 'seed' option)
-  //! And sets 'stage' back to 0   (this is the stage of a fresh learner!)
-  //! You may remove this method if your distribution does not implement it
+  //! (Re-)initializes the PDistribution in its fresh state (that state may depend on the 'seed' option).
+  //! And sets 'stage' back to 0 (this is the stage of a fresh learner!).
+  // ### You may remove this method if your distribution does not implement it.
   virtual void forget();
-
     
-  //! The role of the train method is to bring the learner up to stage==nstages,
+  //! The role of the train method is to bring the learner up to stage == nstages,
   //! updating the train_stats collector with training costs measured on-line in the process.
-  //! You may remove this method if your distribution does not implement it
+  // ### You may remove this method if your distribution does not implement it.
   virtual void train();
 
 };
 
-// Declares a few other classes and functions related to this class
-  DECLARE_OBJECT_PTR(DERIVEDCLASS);
+// Declare a few other classes and functions related to this class.
+DECLARE_OBJECT_PTR(DERIVEDCLASS);
   
 } // end of namespace PLearn
-
-#endif
