8ccaa021fb2ffed6426a033daaa2c4a894a3084b
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2004-06-04 16:08:21 +0000

    A splitter to add in the training set points in the test set given by another splitter

---
+++ plearn/vmat/TestInTrainSplitter.cc
@@ -0,0 +1,189 @@
+// -*- C++ -*-
+
+// TestInTrainSplitter.cc
+//
+// Copyright (C) 2004 Olivier Delalleau 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: TestInTrainSplitter.cc,v 1.1 2004/06/04 16:08:21 tihocan Exp $ 
+   ******************************************************* */
+
+// Authors: Olivier Delalleau
+
+/*! \file TestInTrainSplitter.cc */
+
+
+#include "ConcatRowsVMatrix.h"      //!< For vconcat.
+#include "SubVMatrix.h"
+#include "TestInTrainSplitter.h"
+
+namespace PLearn {
+using namespace std;
+
+TestInTrainSplitter::TestInTrainSplitter() 
+: percentage_added(0.1)
+  /* ### Initialize all fields to their default value */
+{
+  // ...
+
+  // ### You may or may not want to call build_() to finish building the object
+  // build_();
+}
+
+PLEARN_IMPLEMENT_OBJECT(TestInTrainSplitter,
+    "A splitter that adds the test points given by another splitter into the training set.",
+    "The underlying splitter should return train / test sets of constant size."
+);
+
+void TestInTrainSplitter::declareOptions(OptionList& ol)
+{
+  // ### Declare all of this object's options here
+  // ### For the "flags" of each option, you should typically specify  
+  // ### one of OptionBase::buildoption, OptionBase::learntoption or 
+  // ### OptionBase::tuningoption. Another possible flag to be combined with
+  // ### is OptionBase::nosave
+
+  declareOption(ol, "percentage_added", &TestInTrainSplitter::percentage_added, OptionBase::buildoption,
+      "The ratio between the number of examples in the test set added to the train set and the\n"
+      "number of examples in the train set.");
+
+  declareOption(ol, "source_splitter", &TestInTrainSplitter::source_splitter, OptionBase::buildoption,
+      "The underlying splitter.");
+
+  // Now call the parent class' declareOptions
+  inherited::declareOptions(ol);
+}
+
+void TestInTrainSplitter::build_()
+{
+  // ### This method should do the real building of the object,
+  // ### according to set 'options', in *any* situation. 
+  // ### Typical situations include:
+  // ###  - Initial building of an object from a few user-specified options
+  // ###  - Building of a "reloaded" object: i.e. from the complete set of all serialised options.
+  // ###  - Updating or "re-building" of an object after a few "tuning" options have been modified.
+  // ### You should assume that the parent class' build_() has already been called.
+}
+
+void TestInTrainSplitter::build()
+{
+  inherited::build();
+  build_();
+}
+
+void TestInTrainSplitter::makeDeepCopyFromShallowCopy(map<const void*, void*>& copies)
+{
+  Splitter::makeDeepCopyFromShallowCopy(copies);
+
+  // ### Call deepCopyField on all "pointer-like" fields 
+  // ### that you wish to be deepCopied rather than 
+  // ### shallow-copied.
+  // ### ex:
+  // deepCopyField(trainvec, copies);
+
+  // ### Remove this line when you have fully implemented this method.
+  PLERROR("TestInTrainSplitter::makeDeepCopyFromShallowCopy not fully (correctly) implemented yet!");
+}
+
+/////////////
+// nsplits //
+/////////////
+int TestInTrainSplitter::nsplits() const
+{
+  if (first_source_split.isEmpty()) {
+    getFirstSplit(); // This is just to initialize n_train and n_test.
+  }
+  n_to_add = int(n_train * percentage_added + 0.5);
+  n_splits_per_source_split = n_test / n_to_add;
+  if (n_splits_per_source_split == 0) {
+    PLERROR("In TestInTrainSplitter::nsplits - Asked to add more test samples than available");
+  }
+  return n_splits_per_source_split * source_splitter->nsplits();
+}
+
+///////////////////
+// nSetsPerSplit //
+///////////////////
+int TestInTrainSplitter::nSetsPerSplit() const
+{
+  return source_splitter->nSetsPerSplit();
+}
+
+///////////////////
+// getFirstSplit //
+///////////////////
+void TestInTrainSplitter::getFirstSplit() const {
+  first_source_split = source_splitter->getSplit(0);
+  n_train = first_source_split[0]->length();
+  n_test = first_source_split[1]->length();
+}
+
+//////////////
+// getSplit //
+//////////////
+TVec<VMat> TestInTrainSplitter::getSplit(int k)
+{
+  TVec<VMat> source_split;
+  if (first_source_split.isEmpty()) {
+    getFirstSplit();
+  }
+  if (k == 0) {
+    source_split = first_source_split;
+  } else {
+    source_split = source_splitter->getSplit(k / n_splits_per_source_split);
+  }
+  int n_test_part = k % n_splits_per_source_split;
+  int i_test_start = n_test_part * n_to_add;
+  VMat train_set = source_split[0];
+  VMat test_set = source_split[1];
+  if (train_set->length() != n_train || test_set->length() != n_test) {
+    PLERROR("In TestInTrainSplitter::getSplit - The train / test sizes have changed!");
+  }
+  TVec<VMat> result(source_split.length());
+  VMat added_to_train = new SubVMatrix(test_set, i_test_start, 0, n_to_add, test_set->width());
+  result[0] = vconcat(train_set, added_to_train);
+  result[1] = added_to_train;
+  for (int i = 2; i < result.length(); i++) {
+    result[i] = source_split[i];
+  }
+  return result;
+}
+
+////////////////
+// setDataSet //
+////////////////
+void TestInTrainSplitter::setDataSet(VMat the_dataset) {
+  first_source_split.resize(0);
+  inherited::setDataSet(the_dataset);
+  source_splitter->setDataSet(the_dataset);
+}
+
+} // end of namespace PLearn
---
+++ plearn/vmat/TestInTrainSplitter.h
@@ -0,0 +1,153 @@
+// -*- C++ -*-
+
+// TestInTrainSplitter.h
+//
+// Copyright (C) 2004 Olivier Delalleau 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: TestInTrainSplitter.h,v 1.1 2004/06/04 16:08:21 tihocan Exp $ 
+   ******************************************************* */
+
+// Authors: Olivier Delalleau
+
+/*! \file TestInTrainSplitter.h */
+
+
+#ifndef TestInTrainSplitter_INC
+#define TestInTrainSplitter_INC
+
+#include "Splitter.h"
+
+namespace PLearn {
+using namespace std;
+
+class TestInTrainSplitter: public Splitter
+{
+
+private:
+
+  typedef Splitter inherited;
+
+protected:
+  // *********************
+  // * protected options *
+  // *********************
+
+  // ### declare protected option fields (such as learnt parameters) here
+  // ...
+  
+  // Fields below are not options.
+
+  //! Store the first split provided by the source_splitter.
+  //! This is because we need it the number of points in the train and test
+  //! parts in the nsplits() method.
+  mutable TVec<VMat> first_source_split;
+
+  //! The number of splits to make for each split given by the source_splitter.
+  mutable int n_splits_per_source_split;
+    
+  //! The number of samples in the train and test parts, as given by
+  //! the source_splitter.
+  mutable int n_train, n_test;
+
+  //! The number of sample to add each time in the train set.
+  mutable int n_to_add;
+
+public:
+
+  // ************************
+  // * public build options *
+  // ************************
+
+  real percentage_added;
+  PP<Splitter> source_splitter;
+
+  // ****************
+  // * Constructors *
+  // ****************
+
+  // Default constructor, make sure the implementation in the .cc
+  // initializes all fields to reasonable default values.
+  TestInTrainSplitter();
+
+
+  // ******************
+  // * Object methods *
+  // ******************
+
+private: 
+  //! This does the actual building. 
+  // (Please implement in .cc)
+  void build_();
+
+protected: 
+  //! Declares this class' options
+  // (Please implement in .cc)
+  static void declareOptions(OptionList& ol);
+
+public:
+  // simply calls inherited::build() then build_() 
+  virtual void build();
+
+  //! Transforms a shallow copy into a deep copy
+  virtual void makeDeepCopyFromShallowCopy(map<const void*, void*>& copies);
+
+  //! Declares name and deepCopy methods
+  PLEARN_DECLARE_OBJECT(TestInTrainSplitter);
+
+
+  // ********************************
+  // *        Splitter methods      *
+  // ********************************
+  
+  //! Overridden to forward to underlying splitter and reset 'first_source_split'.
+  virtual void setDataSet(VMat the_dataset);
+
+  //! Returns the number of available different "splits"
+  virtual int nsplits() const;
+
+  //! Returns the number of sets per split
+  virtual int nSetsPerSplit() const;
+
+  //! Compute the first split of source_splitter and store it in 'first_source_split'.
+  virtual void getFirstSplit() const;
+
+  //! Returns split number i
+  virtual TVec<VMat> getSplit(int i=0);
+
+};
+
+// Declares a few other classes and functions related to this class
+DECLARE_OBJECT_PTR(TestInTrainSplitter);
+  
+} // end of namespace PLearn
+
+#endif
