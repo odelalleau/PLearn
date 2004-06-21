b6f70a1794af1364c183722c9f39e8fc288c6c4b
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2004-06-21 14:48:32 +0000

    A class for unconditional distributions, that simply hides the conditional stuff in PDistribution

---
+++ plearn_learners/distributions/UnconditionalDistribution.cc
@@ -0,0 +1,158 @@
+// -*- C++ -*-
+
+// UnconditionalDistribution.cc
+//
+// Copyright (C) 2004 Olivier Delalleau 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: UnconditionalDistribution.cc,v 1.1 2004/06/21 14:48:32 tihocan Exp $ 
+   ******************************************************* */
+
+// Authors: Olivier Delalleau
+
+/*! \file UnconditionalDistribution.cc */
+
+
+#include "UnconditionalDistribution.h"
+
+namespace PLearn {
+using namespace std;
+
+///////////////////////////////
+// UnconditionalDistribution //
+///////////////////////////////
+UnconditionalDistribution::UnconditionalDistribution() 
+/* ### Initialize all fields to their default value here */
+{
+  // ...
+
+  // ### You may or may not want to call build_() to finish building the object
+  // build_();
+}
+
+PLEARN_IMPLEMENT_OBJECT(UnconditionalDistribution,
+    "This class is a simplified version of PDistribution for unconditional distributions.",
+    "Its only goal is to hide the conditional side of PDistributions to make it simpler."
+);
+
+////////////////////
+// declareOptions //
+////////////////////
+void UnconditionalDistribution::declareOptions(OptionList& ol)
+{
+  // ### Declare all of this object's options here
+  // ### For the "flags" of each option, you should typically specify  
+  // ### one of OptionBase::buildoption, OptionBase::learntoption or 
+  // ### OptionBase::tuningoption. Another possible flag to be combined with
+  // ### is OptionBase::nosave
+
+  // ### ex:
+  // declareOption(ol, "myoption", &UnconditionalDistribution::myoption, OptionBase::buildoption,
+  //               "Help text describing this option");
+  // ...
+
+  // First call the parent class' declareOptions().
+  inherited::declareOptions(ol);
+
+  // And modify some options for unconditional distributions.
+
+  redeclareOption(ol, "outputs_def", &PDistribution::outputs_def, OptionBase::buildoption,
+      "Defines what will be given in output. This is a string where the characters\n"
+      "have the following meaning:\n"
+      "'l'-> log_density, 'd' -> density, 'c' -> cdf, 's' -> survival_fn,\n"
+      "'e' -> expectation, 'v' -> variance.\n"
+      "In lower case they give the value associated with a given observation.\n"
+      "In upper case, a curve is evaluated at regular intervals and produced in\n"
+      "output (as a histogram), only for 'L', 'D', 'C' and 'S'.\n"
+      "The number of curve points is determined by the 'n_curve_points' option.\n"
+      "Note that the upper case letters only work for SCALAR variables."
+      );
+
+  redeclareOption(ol, "conditional_flags", &PDistribution::conditional_flags, OptionBase::nosave,
+      "Unused in unconditional distributions.");
+
+  redeclareOption(ol, "provide_input", &PDistribution::provide_input, OptionBase::nosave,
+      "Unused in unconditional distributions.");
+
+  redeclareOption(ol, "cond_sort",  &PDistribution::cond_sort, OptionBase::nosave,
+      "Unused in unconditional distributions.");
+
+  redeclareOption(ol, "n_input",  &PDistribution::n_input, OptionBase::nosave,
+      "Unused in unconditional distributions.");
+
+  redeclareOption(ol, "n_target",  &PDistribution::n_target, OptionBase::nosave,
+      "Unused in unconditional distributions.");
+
+  redeclareOption(ol, "n_margin",  &PDistribution::n_margin, OptionBase::nosave,
+      "Unused in unconditional distributions.");
+
+}
+
+///////////
+// build //
+///////////
+void UnconditionalDistribution::build()
+{
+  inherited::build();
+  build_();
+}
+
+////////////
+// build_ //
+////////////
+void UnconditionalDistribution::build_()
+{
+}
+
+/////////////////////////////////
+// makeDeepCopyFromShallowCopy //
+/////////////////////////////////
+void UnconditionalDistribution::makeDeepCopyFromShallowCopy(map<const void*, void*>& copies)
+{
+  inherited::makeDeepCopyFromShallowCopy(copies);
+}
+
+//////////////
+// setInput //
+//////////////
+void UnconditionalDistribution::setInput(const Vec& input) const {
+  PLERROR("setInput not implemented for UnconditionalDistribution");
+}
+
+//////////////////////////////////
+// updateFromConditionalSorting //
+//////////////////////////////////
+void UnconditionalDistribution::updateFromConditionalSorting() {
+    PLERROR("updateFromConditionalSorting not implemented for UnconditionalDistribution");
+}
+
+} // end of namespace PLearn
+
