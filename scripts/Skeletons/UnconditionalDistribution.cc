b82ccd2cb6e39b753e5de5f8806b9659bed3886a
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2004-06-21 14:48:53 +0000

    Skeleton for UnconditionalDistribution

---
+++ scripts/Skeletons/UnconditionalDistribution.cc
@@ -0,0 +1,201 @@
+#include "DERIVEDCLASS.h"
+
+namespace PLearn {
+using namespace std;
+
+//////////////////
+// DERIVEDCLASS //
+//////////////////
+DERIVEDCLASS::DERIVEDCLASS() 
+/* ### Initialize all fields to their default value here */
+{
+  // ...
+
+  // ### You may or may not want to call build_() to finish building the object
+  // build_();
+}
+
+PLEARN_IMPLEMENT_OBJECT(DERIVEDCLASS,
+    "ONE LINE DESCR",
+    "NO HELP"
+);
+
+////////////////////
+// declareOptions //
+////////////////////
+void DERIVEDCLASS::declareOptions(OptionList& ol)
+{
+  // ### Declare all of this object's options here
+  // ### For the "flags" of each option, you should typically specify  
+  // ### one of OptionBase::buildoption, OptionBase::learntoption or 
+  // ### OptionBase::tuningoption. Another possible flag to be combined with
+  // ### is OptionBase::nosave
+
+  // ### ex:
+  // declareOption(ol, "myoption", &DERIVEDCLASS::myoption, OptionBase::buildoption,
+  //               "Help text describing this option");
+  // ...
+
+  // Now call the parent class' declareOptions().
+  inherited::declareOptions(ol);
+}
+
+///////////
+// build //
+///////////
+void DERIVEDCLASS::build()
+{
+  // ### Nothing to add here, simply calls build_().
+  inherited::build();
+  build_();
+}
+
+////////////
+// build_ //
+////////////
+void DERIVEDCLASS::build_()
+{
+  // ### This method should do the real building of the object,
+  // ### according to set 'options', in *any* situation. 
+  // ### Typical situations include:
+  // ###  - Initial building of an object from a few user-specified options
+  // ###  - Building of a "reloaded" object: i.e. from the complete set of all serialised options.
+  // ###  - Updating or "re-building" of an object after a few "tuning" options have been modified.
+  // ### You should assume that the parent class' build_() has already been called.
+}
+
+/////////
+// cdf //
+/////////
+real DERIVEDCLASS::cdf(const Vec& y) const
+{
+  PLERROR("cdf not implemented for DERIVEDCLASS"); return 0;
+}
+
+/////////////////
+// expectation //
+/////////////////
+void DERIVEDCLASS::expectation(Vec& mu) const
+{
+  PLERROR("expectation not implemented for DERIVEDCLASS");
+}
+
+// ### Remove this method if your distribution does not implement it.
+////////////
+// forget //
+////////////
+void DERIVEDCLASS::forget()
+{
+  /*!
+    A typical forget() method should do the following:
+    - initialize a random number generator with the seed option
+    - initialize the learner's parameters, using this random generator
+    - stage = 0
+   */
+  PLERROR("forget method not implemented for DERIVEDCLASS");
+}
+
+//////////////
+// generate //
+//////////////
+void DERIVEDCLASS::generate(Vec& y) const
+{
+  PLERROR("generate not implemented for DERIVEDCLASS");
+}
+
+// ### Default version of inputsize returns learner->inputsize()
+// ### If this is not appropriate, you should uncomment this and define
+// ### it properly here:
+// int DERIVEDCLASS::inputsize() const {}
+
+/////////////////
+// log_density //
+/////////////////
+real DERIVEDCLASS::log_density(const Vec& y) const
+{
+  PLERROR("density not implemented for DERIVEDCLASS"); return 0; 
+}
+
+/////////////////////////////////
+// makeDeepCopyFromShallowCopy //
+/////////////////////////////////
+void DERIVEDCLASS::makeDeepCopyFromShallowCopy(map<const void*, void*>& copies)
+{
+  inherited::makeDeepCopyFromShallowCopy(copies);
+
+  // ### Call deepCopyField on all "pointer-like" fields 
+  // ### that you wish to be deepCopied rather than 
+  // ### shallow-copied.
+  // ### ex:
+  // deepCopyField(trainvec, copies);
+
+  // ### Remove this line when you have fully implemented this method.
+  PLERROR("DERIVEDCLASS::makeDeepCopyFromShallowCopy not fully (correctly) implemented yet!");
+}
+
+////////////////////
+// resetGenerator //
+////////////////////
+void DERIVEDCLASS::resetGenerator(long g_seed) const
+{
+  PLERROR("resetGenerator not implemented for DERIVEDCLASS");
+}
+
+/////////////////
+// survival_fn //
+/////////////////
+real DERIVEDCLASS::survival_fn(const Vec& y) const
+{
+  PLERROR("survival_fn not implemented for DERIVEDCLASS"); return 0;
+}
+
+// ### Remove this method, if your distribution does not implement it.
+///////////
+// train //
+///////////
+void DERIVEDCLASS::train()
+{
+  PLERROR("train method not implemented for DERIVEDCLASS");
+
+  // The role of the train method is to bring the learner up to stage==nstages,
+  // updating train_stats with training costs measured on-line in the process.
+
+  /* TYPICAL CODE:
+
+     static Vec input  // static so we don't reallocate/deallocate memory each time...
+     static Vec target
+     input.resize(inputsize())    // the train_set's inputsize()
+     target.resize(targetsize())  // the train_set's targetsize()
+     real weight
+
+     if(!train_stats)  // make a default stats collector, in case there's none
+     train_stats = new VecStatsCollector()
+
+     if(nstages<stage) // asking to revert to a previous stage!
+     forget()  // reset the learner to stage=0
+
+     while(stage<nstages)
+     {
+  // clear statistics of previous epoch
+  train_stats->forget() 
+
+  //... train for 1 stage, and update train_stats,
+  // using train_set->getSample(input, target, weight)
+  // and train_stats->update(train_costs)
+
+  ++stage
+  train_stats->finalize() // finalize statistics for this epoch
+  }
+   */
+}
+
+//////////////
+// variance //
+//////////////
+void DERIVEDCLASS::variance(Mat& covar) const
+{
+  PLERROR("variance not implemented for DERIVEDCLASS");
+}
+
+} // end of namespace PLearn
+
---
+++ scripts/Skeletons/UnconditionalDistribution.h
@@ -0,0 +1,131 @@
+#ifndef DERIVEDCLASS_INC
+#define DERIVEDCLASS_INC
+
+#include "PDistribution.h"
+
+namespace PLearn {
+using namespace std;
+
+class DERIVEDCLASS: public UnconditionalDistribution
+{
+
+private:
+
+  typedef UnconditionalDistribution inherited;  
+
+protected:
+
+  // *********************
+  // * protected options *
+  // *********************
+
+  // ### Declare protected option fields (such as learnt parameters) here.
+  // ...
+    
+public:
+
+  // ************************
+  // * public build options *
+  // ************************
+
+  // ### Declare public option fields (such as build options) here.
+  // ...
+
+  // ****************
+  // * Constructors *
+  // ****************
+
+  //! Default constructor.
+  // ### Make sure the implementation in the .cc
+  // ### initializes all fields to reasonable default values.
+  DERIVEDCLASS();
+
+  // *************************
+  // * PDistribution methods *
+  // *************************
+
+private: 
+
+  //! This does the actual building. 
+  // ### Please implement in .cc.
+  void build_();
+
+protected: 
+
+  //! Declare this class' options.
+  // ### Please implement in .cc.
+  static void declareOptions(OptionList& ol);
+
+public:
+
+  // ************************
+  // **** Object methods ****
+  // ************************
+
+  //! Simply call inherited::build() then build_().
+  virtual void build();
+
+  //! Transform a shallow copy into a deep copy.
+  virtual void makeDeepCopyFromShallowCopy(map<const void*, void*>& copies);
+
+  // Declare other standard object methods.
+  // ### If your class is not instantiatable (it has pure virtual methods)
+  // ### you should replace this by PLEARN_DECLARE_ABSTRACT_OBJECT_METHODS.
+  PLEARN_DECLARE_OBJECT(DERIVEDCLASS);
+
+  // *******************************
+  // **** PDistribution methods ****
+  // *******************************
+
+  //! Return log of probability density log(p(y)).
+  virtual real log_density(const Vec& x) const;
+
+  //! Return survival function: P(Y>y).
+  virtual real survival_fn(const Vec& y) const;
+
+  //! Return cdf: P(Y<y).
+  virtual real cdf(const Vec& y) const;
+
+  //! Return E[Y].
+  virtual void expectation(Vec& mu) const;
+
+  //! Return Var[Y].
+  virtual void variance(Mat& cov) const;
+
+  //! Return a pseudo-random sample generated from the distribution.
+  virtual void generate(Vec& y) const;
+
+  //! Reset the random number generator used by generate() using the given seed.
+  virtual void resetGenerator(long g_seed) const;
+
+  // ### These methods may be overridden for efficiency purpose:
+  /*
+  //! Return probability density p(y)
+  virtual real density(const Vec& y) const;
+  */
+
+  // **************************
+  // **** PLearner methods ****
+  // **************************
+
+  // ### Default version of inputsize returns learner->inputsize()
+  // ### If this is not appropriate, you should uncomment this and define
+  // ### it properly in the .cc
+  // virtual int inputsize() const;
+
+  //! (Re-)initializes the PDistribution in its fresh state (that state may depend on the 'seed' option).
+  //! And sets 'stage' back to 0 (this is the stage of a fresh learner!).
+  // ### You may remove this method if your distribution does not implement it.
+  virtual void forget();
+    
+  //! The role of the train method is to bring the learner up to stage == nstages,
+  //! updating the train_stats collector with training costs measured on-line in the process.
+  // ### You may remove this method if your distribution does not implement it.
+  virtual void train();
+
+};
+
+// Declare a few other classes and functions related to this class.
+DECLARE_OBJECT_PTR(DERIVEDCLASS);
+  
+} // end of namespace PLearn
