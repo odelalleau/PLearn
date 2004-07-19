ff247f1f440afb4e0fd7db48459a67a711816a25
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2004-07-19 14:52:59 +0000

    An object to run shell commands within a script

---
+++ plearn/misc/ShellScript.cc
@@ -0,0 +1,111 @@
+// -*- C++ -*-
+
+// ShellScript.cc
+//
+// Copyright (C) 2004 Olivier Delalleau 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: ShellScript.cc,v 1.1 2004/07/19 14:52:59 tihocan Exp $ 
+   ******************************************************* */
+
+// Authors: Olivier Delalleau
+
+/*! \file ShellScript.cc */
+
+
+#include "ShellScript.h"
+
+namespace PLearn {
+using namespace std;
+
+ShellScript::ShellScript() 
+  /* ### Initialize all fields to their default value */
+{
+  // ...
+
+  // ### You may or may not want to call build_() to finish building the object
+  // build_();
+}
+
+PLEARN_IMPLEMENT_OBJECT(ShellScript,
+    "Allows one to run shell commands (especially within a script).",
+    "This runnable object will execute the given shell commands when run.");
+
+void ShellScript::declareOptions(OptionList& ol)
+{
+  // ### Declare all of this object's options here
+  // ### For the "flags" of each option, you should typically specify  
+  // ### one of OptionBase::buildoption, OptionBase::learntoption or 
+  // ### OptionBase::tuningoption. Another possible flag to be combined with
+  // ### is OptionBase::nosave
+
+  declareOption(ol, "shell_commands", &ShellScript::shell_commands, OptionBase::buildoption,
+      "The commands to be executed at run time.");
+
+  // Now call the parent class' declareOptions
+  inherited::declareOptions(ol);
+}
+
+void ShellScript::build_()
+{
+  // ### This method should do the real building of the object,
+  // ### according to set 'options', in *any* situation. 
+  // ### Typical situations include:
+  // ###  - Initial building of an object from a few user-specified options
+  // ###  - Building of a "reloaded" object: i.e. from the complete set of all serialised options.
+  // ###  - Updating or "re-building" of an object after a few "tuning" options have been modified.
+  // ### You should assume that the parent class' build_() has already been called.
+}
+
+// ### Nothing to add here, simply calls build_
+void ShellScript::build()
+{
+  inherited::build();
+  build_();
+}
+
+void ShellScript::makeDeepCopyFromShallowCopy(map<const void*, void*>& copies)
+{
+  inherited::makeDeepCopyFromShallowCopy(copies);
+  deepCopyField(shell_commands, copies);
+}
+
+/////////
+// run //
+/////////
+void ShellScript::run() {
+  // Perform the commands provided in shell_commands.
+  for (int i = 0; i < shell_commands.length(); i++) {
+    system(shell_commands[i].c_str());
+  }
+}
+
+} // end of namespace PLearn
---
+++ plearn/misc/ShellScript.h
@@ -0,0 +1,121 @@
+// -*- C++ -*-
+
+// ShellScript.h
+//
+// Copyright (C) 2004 Olivier Delalleau 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: ShellScript.h,v 1.1 2004/07/19 14:52:59 tihocan Exp $ 
+   ******************************************************* */
+
+// Authors: Olivier Delalleau
+
+/*! \file ShellScript.h */
+
+
+#ifndef ShellScript_INC
+#define ShellScript_INC
+
+#include "Object.h"
+
+namespace PLearn {
+using namespace std;
+
+class ShellScript: public Object
+{
+
+private:
+  
+  typedef Object inherited;
+
+protected:
+  // *********************
+  // * protected options *
+  // *********************
+
+  // ### declare protected option fields (such as learnt parameters) here
+  // ...
+    
+public:
+
+  // ************************
+  // * public build options *
+  // ************************
+
+  TVec<string> shell_commands;
+
+  // ****************
+  // * Constructors *
+  // ****************
+
+  //! Default constructor.
+  // ### Make sure the implementation in the .cc
+  // ### initializes all fields to reasonable default values.
+  ShellScript();
+
+
+  // ******************
+  // * Object methods *
+  // ******************
+
+private: 
+  
+  //! This does the actual building. 
+  void build_();
+
+protected: 
+
+  //! Declares this class' options.
+  static void declareOptions(OptionList& ol);
+
+public:
+
+  // Declares other standard object methods.
+  PLEARN_DECLARE_OBJECT(ShellScript);
+
+  // simply calls inherited::build() then build_() 
+  virtual void build();
+
+  //! Transforms a shallow copy into a deep copy
+  // (PLEASE IMPLEMENT IN .cc)
+  virtual void makeDeepCopyFromShallowCopy(map<const void*, void*>& copies);
+
+  //! Execute the shell commands.
+  virtual void run();
+
+};
+
+// Declares a few other classes and functions related to this class
+DECLARE_OBJECT_PTR(ShellScript);
+  
+} // end of namespace PLearn
+
+#endif
