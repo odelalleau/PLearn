50b95a1658dfda101ec0cef607a5e55e4203fc6a
monperrm <monperrm@15d43a8f-0efa-0310-bc34-a8a306da697e>
2004-08-05 13:52:18 +0000

    An easy PTester

---
+++ plearn_learners/testers/Train.h
@@ -0,0 +1,126 @@
+// -*- C++ -*-
+
+// Train.h
+//
+// Copyright (C) 2004 Martin Monperrus 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: Train.h,v 1.1 2004/08/05 13:52:18 monperrm Exp $ 
+   ******************************************************* */
+
+// Authors: Martin Monperrus
+
+/*! \file Train.h */
+
+
+#ifndef Train_INC
+#define Train_INC
+
+#include "plearn/base/Object.h"
+#include "plearn_learners/generic/PLearner.h"
+
+namespace PLearn {
+using namespace std;
+
+class Train: public Object
+{
+
+private:
+  
+  typedef Object inherited;
+
+protected:
+  // *********************
+  // * protected options *
+  // *********************
+
+  // ### declare protected option fields (such as learnt parameters) here
+  // ...
+    
+public:
+
+  // ************************
+  // * public build options *
+  // ************************
+  PP<PLearner> learner;
+  //string dataset;
+  string psave;
+  VMat trainset;
+ 
+  void run();
+  // ### declare public option fields (such as build options) here
+  // ...
+
+  // ****************
+  // * Constructors *
+  // ****************
+
+  //! Default constructor.
+  // ### Make sure the implementation in the .cc
+  // ### initializes all fields to reasonable default values.
+  Train();
+
+
+  // ******************
+  // * Object methods *
+  // ******************
+
+private: 
+  //! This does the actual building. 
+  // (PLEASE IMPLEMENT IN .cc)
+  void build_();
+
+protected: 
+  //! Declares this class' options.
+  // (PLEASE IMPLEMENT IN .cc)
+  static void declareOptions(OptionList& ol);
+
+public:
+  // Declares other standard object methods.
+  // ### If your class is not instantiatable (it has pure virtual methods)
+  // ### you should replace this by PLEARN_DECLARE_ABSTRACT_OBJECT_METHODS 
+  PLEARN_DECLARE_OBJECT(Train);
+
+  // simply calls inherited::build() then build_() 
+  virtual void build();
+
+  //! Transforms a shallow copy into a deep copy
+  // (PLEASE IMPLEMENT IN .cc)
+  virtual void makeDeepCopyFromShallowCopy(map<const void*, void*>& copies);
+
+};
+
+// Declares a few other classes and functions related to this class
+  DECLARE_OBJECT_PTR(Train);
+  
+} // end of namespace PLearn
+
+#endif
