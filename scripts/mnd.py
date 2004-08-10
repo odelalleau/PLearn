ad431b449b805c5b8738409c42c6cd750a38b0a8
plearner <plearner@15d43a8f-0efa-0310-bc34-a8a306da697e>
2004-08-10 21:22:16 +0000

    Multivariate normal density

---
+++ scripts/mnd.py
@@ -0,0 +1,23 @@
+from Numeric import *
+from LinearAlgebra import *
+from math import pi
+from math import log
+
+def mnd(x, mean, cov):
+    cov_inv = inverse(cov)
+    cov_det = determinant(cov)
+    exponent = -0.5 * matrixmultiply(matrixmultiply(transpose(x - mean),
+                                                    cov_inv),
+                                     x - mean)
+    return (1 / (2 * pi)) * cov_det ** -0.5 * exp(exponent)
+
+def log_mnd(x, mean, cov):
+    cov_inv = inverse(cov)
+    cov_det = determinant(cov)
+    exponent = -0.5 * matrixmultiply(matrixmultiply(transpose(x - mean),
+                                                    cov_inv),
+                                     x - mean)
+    return log(1 / (2 * pi) * cov_det ** -0.5) + exponent
+
+__all__ = [ "mnd", "log_mnd" ]
+
