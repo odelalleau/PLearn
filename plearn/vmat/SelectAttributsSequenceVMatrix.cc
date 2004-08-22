050fc8dd78a90ae0b1f599ab09220acaf2cda1b0
larocheh <larocheh@15d43a8f-0efa-0310-bc34-a8a306da697e>
2004-08-22 17:51:31 +0000

    I added a VMatrix that constructs contexts from a source VMatrix which represents a sequence of elements (symbol and attributs).

---
+++ plearn/vmat/SelectAttributsSequenceVMatrix.cc
@@ -0,0 +1,433 @@
+// -*- C++ -*-
+
+// SelectAttributsSequenceVMatrix.cc
+//
+// Copyright (C) 2004 Hugo Larochelle 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: SelectAttributsSequenceVMatrix.cc,v 1.1 2004/08/22 17:51:31 larocheh Exp $ 
+   ******************************************************* */
+
+// Authors: Hugo Larochelle
+
+/*! \file SelectAttributsSequenceVMatrix.cc */
+
+
+#include "SelectAttributsSequenceVMatrix.h"
+
+namespace PLearn {
+using namespace std;
+
+
+SelectAttributsSequenceVMatrix::SelectAttributsSequenceVMatrix()
+  :inherited(), n_left_context(2), n_right_context(2), conditions_offset(0), full_context(true),
+   target_is_last_element(true), use_last_context(true)
+  /* ### Initialise all fields to their default value */
+{
+}
+  
+  PLEARN_IMPLEMENT_OBJECT(SelectAttributsSequenceVMatrix,
+                          "This VMatrix takes a VMat of a sequence of elements (symbol and attributs) and constructs context rows.",
+                          "The elements' symbol and attributs have to be coded using integers, and the source VMat string mapping\n"
+                          "is also used with the functions getStringVal(...) and getValString(...).\n"
+                          "The context rows can be of fixed length, or constrained by delimiter symbols.\n"
+                          "Certain rows can be selected/excluded, and certain elements can be excluded of\n"
+                          "a context according to some conditions on the symbol or its attributs.\n"
+                          "The conditions are expressed as disjunctions of conjunctions. For example: \n\n"
+                          "[ [ 0 : \"person\", 1 : \"NN\" ] , [ 0 : \"kind\", 2 : \"plural\" ] ] \n\n"
+                          "is equivalent in C++ logic form to : \n\n"
+                          "(fields[0]==\"person\" && fields[1]==\"NN\") || (fields[0]==\"kind\" && fields[2]==\"plural\").\n\n"
+                          "Conditions can be expressed in string or int format. The string mapping is used to make the correspondance.\n"
+    );
+  
+void SelectAttributsSequenceVMatrix::getNewRow(int i, const Vec& v) const
+{
+  if(i<0 || i>=length_) PLERROR("In SelectAttributSequenceVMatrix::getNewRow() : invalid row acces i=%d for VMatrix(%d,%d)",i,length_,width_);
+  
+  int target = indices[i];
+
+  // If the target element is a delimiter, do nothing
+  source->getRow(target,target_element);
+  if(is_true(delimiters,target_element))
+  {
+    v.fill(MISSING_VALUE);
+    if(use_last_context)
+    {
+      current_context_pos.clear();
+      current_row_i.fill(MISSING_VALUE);
+      current_target_pos = -1;
+      lower_bound = -1;
+      upper_bound = -1;
+    }
+    return;
+  }
+
+  int left_added_elements = 0;
+  int right_added_elements = 0;
+
+  int left_pos = 1;
+  int right_pos = 1;
+  int p = 0;
+
+  bool to_add = false;
+  bool not_delimiter = false;
+
+  // Left context construction
+
+  while(n_left_context < 0 || left_added_elements < n_left_context)
+  {
+    to_add = false;
+    not_delimiter = false;
+
+    p = target-left_pos;
+
+    if(p < 0 || p >= source->length())
+      break;
+
+    // Verifying if context can be found in last context
+    if(use_last_context && lower_bound <= p && upper_bound >= p)
+    {
+      if(current_context_pos.find(p) != current_context_pos.end())
+      {
+        int position = current_context_pos[p];
+        Vec element = current_row_i.subVec(n_attributs*position,n_attributs);
+        if(position != current_target_pos || !is_true(ignored_context,element))
+        {
+          to_add = true;
+          not_delimiter = true;
+        }
+      }
+      else
+      {
+        left_pos++;
+        continue;
+      }
+    }  // If not, fetch element in source VMat
+    else
+    {
+      source->getRow(p,element);
+      if(!is_true(ignored_context,element))
+        to_add = true;  
+    }
+    
+    if(!not_delimiter && is_true(delimiters,element)) break;
+
+    if(to_add)
+    {
+      left_context.subVec(n_attributs*left_added_elements,n_attributs) << element;
+      if(use_last_context) left_positions[left_added_elements] = p;
+      left_added_elements++;
+    }
+    else
+      if(!full_context)
+      {
+        left_context.subVec(n_attributs*left_added_elements,n_attributs).fill(MISSING_VALUE);
+        if(use_last_context) left_positions[left_added_elements] = p;
+        left_added_elements++;
+      }
+
+    left_pos++;
+  }
+
+  lower_bound = p;
+
+  // Right context construction
+
+  while(n_right_context < 0 || right_added_elements < n_right_context)
+  {
+    to_add = false;
+    not_delimiter = false;
+
+    p = target+right_pos;
+
+    if(p < 0 || p >= source->length())
+      break;
+
+    // Verifying if context can be found in last context
+    if(use_last_context && lower_bound <= p && upper_bound >= p)
+    {
+      if(current_context_pos.find(p) != current_context_pos.end())
+      {
+        int position = current_context_pos[p];
+        Vec element = current_row_i.subVec(n_attributs*position,n_attributs);
+        if(position != current_target_pos || !is_true(ignored_context,element))
+        {
+          to_add = true;
+          not_delimiter = true;
+        }
+      }
+      else
+      {
+        right_pos++;
+        continue;
+      }
+    }  // If not, fetch element in source VMat
+    else
+    {
+      source->getRow(p,element);
+      if(!is_true(ignored_context,element))
+        to_add = true;  
+    }
+    
+    if(!not_delimiter && is_true(delimiters,element)) break;
+
+    if(to_add)
+    {
+      right_context.subVec(n_attributs*right_added_elements,n_attributs) << element;
+      if(use_last_context) right_positions[right_added_elements] = p;
+      right_added_elements++;
+    }
+    else
+      if(!full_context)
+      {
+        right_context.subVec(n_attributs*right_added_elements,n_attributs).fill(MISSING_VALUE);
+        if(use_last_context) right_positions[right_added_elements] = p;
+        right_added_elements++;
+      }
+
+    right_pos++;
+  }
+
+  upper_bound = p;
+
+  // Constructing complete row
+
+  int cp = 0;
+
+  if(fixed_context)  // Adding missing value, for the case where the context is of fixed length
+    for(cp=0; cp<n_left_context-left_added_elements; cp++)
+      current_row_i.subVec(cp*n_attributs,n_attributs).fill(MISSING_VALUE);
+
+  current_context_pos.clear();
+
+  // adding left context
+
+  int temp = left_added_elements;
+
+  while(temp > 0)
+  {
+    temp--;
+    current_row_i.subVec(cp*n_attributs,n_attributs) << left_context.subVec(temp*n_attributs,n_attributs);
+    if(use_last_context) current_context_pos[(int)left_positions[temp]] = cp;
+    cp++;
+  }
+
+  // adding target element
+
+  current_row_i.subVec(cp*n_attributs,n_attributs) << target_element;
+  if(use_last_context) 
+  {
+    current_context_pos[target] = cp;
+    current_target_pos = cp;
+  }
+  cp++;
+
+  // adding right context
+
+  int r=0;
+  while(r<right_added_elements)
+  {
+    current_row_i.subVec(cp*n_attributs,n_attributs) << right_context.subVec(r*n_attributs,n_attributs);
+    if(use_last_context) current_context_pos[(int)right_positions[r]] = cp;
+    r++;
+    cp++;
+  }
+
+  if(target_is_last_element)
+  {
+    if(fixed_context) 
+    {
+      left_added_elements = n_left_context;
+      right_added_elements = n_right_context;
+    }
+    v.subVec(0,n_attributs*left_added_elements) << current_row_i.subVec(0,n_attributs*left_added_elements);
+    v.subVec(n_attributs*left_added_elements,n_attributs*right_added_elements) << current_row_i.subVec(n_attributs*(1+left_added_elements),n_attributs*right_added_elements);
+    v.subVec(n_attributs*(left_added_elements+right_added_elements),n_attributs) << current_row_i.subVec(n_attributs*left_added_elements,n_attributs);
+  }
+  else
+    v << current_row_i;
+  
+  return;
+}
+
+void SelectAttributsSequenceVMatrix::declareOptions(OptionList& ol)
+{
+  // ### Declare all of this object's options here
+  // ### For the "flags" of each option, you should typically specify  
+  // ### one of OptionBase::buildoption, OptionBase::learntoption or 
+  // ### OptionBase::tuningoption. Another possible flag to be combined with
+  // ### is OptionBase::nosave
+
+  declareOption(ol, "n_left_context", &SelectAttributsSequenceVMatrix::n_left_context, OptionBase::buildoption,
+                 "Number of elements at the left of the target element (if < 0, all elements to the left are included until a delimiter is met)");
+  declareOption(ol, "n_right_context", &SelectAttributsSequenceVMatrix::n_right_context, OptionBase::buildoption,
+                 "Number of elements at the right of the target element (if < 0, all elements to the right are included until a delimiter is met)");
+  declareOption(ol, "conditions_offset", &SelectAttributsSequenceVMatrix::conditions_offset, OptionBase::buildoption,
+                 "Offset for the position of the element on which conditions are tested (default = 0)");
+  declareOption(ol, "conditions_for_exclusion", &SelectAttributsSequenceVMatrix::conditions_for_exclusion, OptionBase::buildoption,
+                 "Indication that the specified conditions are for the exclusion (true) or inclusion (false) of elements in the VMatrix");
+  declareOption(ol, "full_context", &SelectAttributsSequenceVMatrix::full_context, OptionBase::buildoption,
+                 "Indication that ignored elements of context should be replaced by the next nearest valid element");
+  declareOption(ol, "target_is_last_element", &SelectAttributsSequenceVMatrix::target_is_last_element, OptionBase::buildoption,
+                 "Indication that the last element of the context should be the target element");
+  declareOption(ol, "use_last_context", &SelectAttributsSequenceVMatrix::use_last_context, OptionBase::buildoption,
+                 "Indication that the last accessed context should be put in a buffer.");
+  declareOption(ol, "conditions", &SelectAttributsSequenceVMatrix::conditions, OptionBase::buildoption,
+                 "Conditions to be satisfied for the exclusion or inclusion (see conditions_for_exclusion) of elements in the VMatrix");
+  declareOption(ol, "string_conditions", &SelectAttributsSequenceVMatrix::string_conditions, OptionBase::nosave,
+                 "Conditions, in string format, to be satisfied for the exclusion or inclusion (see conditions_for_exclusion) of elements in the VMatrix");
+  declareOption(ol, "delimiters", &SelectAttributsSequenceVMatrix::delimiters, OptionBase::buildoption,
+                 "Delimiters of context");
+  declareOption(ol, "string_delimiters", &SelectAttributsSequenceVMatrix::string_delimiters, OptionBase::nosave,
+                 "Delimiters, in string format, of context");
+  declareOption(ol, "ignored_context", &SelectAttributsSequenceVMatrix::ignored_context, OptionBase::buildoption,
+                 "Elements to be ignored in context");
+  declareOption(ol, "string_ignored_context", &SelectAttributsSequenceVMatrix::string_ignored_context, OptionBase::nosave,
+                 "Elements, in string format, to be ignored in context");
+  declareOption(ol, "source", &SelectAttributsSequenceVMatrix::source, OptionBase::buildoption,
+                 "Source VMat, from which contexts are extracted");
+  
+
+  // Now call the parent class' declareOptions
+  inherited::declareOptions(ol);
+}
+
+void SelectAttributsSequenceVMatrix::build_()
+{
+  
+  if(!source) PLERROR("In SelectAttributSequenceVMatrix::build_() : no source defined");
+
+  n_attributs = source->width();
+  row.resize(n_attributs);
+  element.resize(n_attributs);
+  target_element.resize(n_attributs);
+
+  fixed_context = n_left_context >=0 && n_right_context >= 0;
+
+  // from string to int format on ...
+
+  // conditions
+  from_string_to_int_format(string_conditions, conditions);
+
+  // delimiters
+  from_string_to_int_format(string_delimiters, delimiters);
+  
+  // ignored_context
+  from_string_to_int_format(string_ignored_context, ignored_context);
+
+  // gathering information from source VMat
+
+  int current_context_length = 0;
+  ProgressBar *pb = new ProgressBar("Gathering information from source VMat of length " + tostring(source->length()), source->length());
+  for(int i=0; i<source->length(); i++)
+  {
+    source->getRow(i,row);
+
+    if(is_true(delimiters,row))
+    {
+      max_context_length = max_context_length < current_context_length ? current_context_length : max_context_length;
+      current_context_length = 0;
+    }
+    else
+    {
+      if(!is_true(ignored_context,row)) current_context_length++;
+    }
+
+    // Testing conditions for inclusion/exclusion
+
+    if(i + conditions_offset >= 0 && i + conditions_offset < source->length())
+    {
+      source->getRow(i+conditions_offset,row);
+      if(is_true(conditions,row))
+      {
+        if(!conditions_for_exclusion) indices.append(i);
+      }
+      else
+      {
+        if(conditions_for_exclusion) indices.append(i);
+      }
+    }
+    pb->update(i+1);
+  }
+
+  if(n_left_context < 0 || n_right_context < 0)
+    max_context_length = 1 + n_left_context + n_right_context;
+
+  length_ = indices.length();
+  width_ = n_attributs*(max_context_length);
+
+  current_row_i.resize(width_);
+  current_target_pos = -1;
+  current_context_pos.clear();
+  lower_bound = -1;
+  upper_bound = -1;
+
+  if(n_left_context < 0) 
+  {
+    left_context.resize(width_);
+    left_positions.resize(max_context_length);
+  }
+  else 
+  {
+    left_context.resize(n_attributs*n_left_context);
+    left_positions.resize(n_left_context);
+  }
+  
+  if(n_right_context < 0) 
+  {
+    right_context.resize(width_);
+    right_positions.resize(max_context_length);
+  }
+  else 
+  {
+    right_context.resize(n_attributs*n_right_context);
+    right_positions.resize(n_right_context);
+  }
+}
+
+void SelectAttributsSequenceVMatrix::build()
+{
+  inherited::build();
+  build_();
+}
+
+void SelectAttributsSequenceVMatrix::makeDeepCopyFromShallowCopy(map<const void*, void*>& copies)
+{
+  inherited::makeDeepCopyFromShallowCopy(copies);
+
+  deepCopyField(conditions, copies);
+  deepCopyField(delimiters, copies);
+  deepCopyField(ignored_context, copies);
+  deepCopyField(source, copies);
+}
+
+} // end of namespace PLearn
+
---
+++ plearn/vmat/SelectAttributsSequenceVMatrix.h
@@ -0,0 +1,264 @@
+// -*- C++ -*-
+
+// SelectAttributsSequenceVMatrix.h
+//
+// Copyright (C) 2004 Hugo Larochelle 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: SelectAttributsSequenceVMatrix.h,v 1.1 2004/08/22 17:51:31 larocheh Exp $ 
+   ******************************************************* */
+
+// Authors: Hugo Larochelle
+
+/*! \file SelectAttributsSequenceVMatrix.h */
+
+
+#ifndef SelectAttributsSequenceVMatrix_INC
+#define SelectAttributsSequenceVMatrix_INC
+
+#include <plearn/vmat/RowBufferedVMatrix.h>
+#include <plearn/vmat/VMat.h>
+#include <plearn/math/pl_math.h>
+#include <plearn/base/ProgressBar.h>
+#include <hash_map>
+
+namespace PLearn {
+using namespace std;
+
+//! This class creates context rows from a sequence of elements (symbol and attributs) in a VMat
+class SelectAttributsSequenceVMatrix: public RowBufferedVMatrix
+{
+
+private:
+
+  typedef RowBufferedVMatrix inherited;
+
+  typedef hash_map<int,int> this_hash_map;
+
+  //! Indication that the context is of fixed length
+  bool fixed_context;
+  //! Maximum length of a context
+  int max_context_length;
+  //! Number of attributs
+  int n_attributs;
+
+  // fields for usage of last row buffer
+  
+  //! Position in the last context of the target element (symbol and attributs)
+  mutable int current_target_pos;
+  //! Position in the source sequence of the first element in the last context
+  mutable int lower_bound;
+  //! Position in the source sequence of the last element in the last context
+  mutable int upper_bound;
+  //! Last context
+  mutable Vec current_row_i;
+  //! Mapping from the position in the source sequence to the position in the last context
+  mutable this_hash_map current_context_pos;
+
+  //! Indices of the selected contexts
+  TVec<int> indices;
+
+  //! Temporary fields
+  mutable TVec<int> left_positions, right_positions;
+  
+  //! Temporary fields
+  mutable Vec left_context, right_context, 
+    row, element, target_element;
+
+protected:
+
+  // *********************
+  // * protected options *
+  // *********************
+
+public:
+
+  // ************************
+  // * public build options *
+  // ************************
+
+  //! Number of elements at the left of the target element. 
+  //! If < 0, all elements to the left are included until a delimiter is met.
+  int n_left_context;
+ 
+  //! Number of elements at the right of the target element. 
+  //! If < 0, all elements to the right are included until a delimiter is met.
+  int n_right_context;
+
+  //! Offset for the position of the element on which conditions are tested (default = 0).
+  int conditions_offset;
+
+  //! Indication that the specified conditions are for the exclusion (true) or inclusion (false) of elements in the VMatrix.
+  bool conditions_for_exclusion;
+
+  //! Indication that ignored elements of context should be replaced by the next nearest valid element.
+  bool full_context;
+
+  //! Indication that the last element of the context should be the target element.
+  bool target_is_last_element;
+
+  //! Indication that the last accessed context should be put in a buffer.
+  bool use_last_context;
+
+  //! Conditions to be satisfied for the exclusion or inclusion (see conditions_for_exclusion) of elements in the VMatrix.
+  TVec< TVec< pair<int,int> > > conditions;
+  
+  //! Conditions, in string format, to be satisfied for the exclusion or inclusion (see conditions_for_exclusion) of elements in the VMatrix.
+  TVec< TVec< pair<int,string> > > string_conditions;
+  
+  //! Delimiters of context.
+  TVec< TVec< pair<int,int> > > delimiters;
+
+  //! Delimiters, in string format, of context.
+  TVec< TVec< pair<int,string> > > string_delimiters;
+
+  //! Elements to be ignored in context.
+  TVec< TVec< pair<int,int> > > ignored_context;
+
+  //! Elements, in string format, to be ignored in context.
+  TVec< TVec< pair<int,string> > > string_ignored_context;
+
+  //! Source VMat, from which contexts are extracted.
+  VMat source;
+
+  // ****************
+  // * Constructors *
+  // ****************
+
+  // Default constructor, make sure the implementation in the .cc
+  // initializes all fields to reasonable default values.
+  SelectAttributsSequenceVMatrix();
+
+  // ******************
+  // * Object methods *
+  // ******************
+
+private: 
+
+  //! This does the actual building. 
+  // (Please implement in .cc)
+  void build_();
+
+  void from_string_to_int_format(TVec< TVec< pair<int,string> > >& str_format, TVec< TVec< pair<int,int> > >& int_format)
+    {
+      int int_format_length = int_format.length();
+      int_format.resize(int_format_length + str_format.length());
+      for(int i=0; i<str_format.length(); i++)
+        from_string_to_int_format(str_format[i], int_format[i+int_format_length]);
+    }
+
+  void from_string_to_int_format(TVec< pair<int,string> >& str_format, TVec< pair<int,int> >& int_format)
+    {
+      int int_format_length = int_format.length();
+      int_format.resize(int_format_length+str_format.length());
+      for(int i=0; i<str_format.length(); i++)
+      {
+        int_format[int_format_length+i].first = str_format[i].first;
+        real value = source->getStringVal(str_format[i].first,str_format[i].second);
+        if(is_missing(value)) PLERROR("In SelectAttributsSequenceVMatrix::from_string_to_int_format : %s not a valid string symbol for column %d", 
+                                           str_format[i].second.c_str(), str_format[i].first);
+        int_format[int_format_length+i].second = (int)value;
+      }
+    }
+
+  bool is_true(const TVec< TVec< pair<int,int> > >& conditions, const Vec row) const
+    {
+      bool condition_satisfied;
+      for(int i=0; i<conditions.length(); i++)
+      {
+        condition_satisfied = true;
+        if(conditions[i].length() == 0) PLERROR("SelectAttributsSequenceVMatrix::is_true : conditions[%d] should not be empty", i);
+        for(int j=0; j<conditions[i].length(); j++)
+        {
+          if(row[conditions[i][j].first] != conditions[i][j].second)
+          {
+            condition_satisfied = false;
+            break;
+          }
+        }
+        if(condition_satisfied) return true;
+      }
+      
+      return false;
+    }
+  
+protected: 
+
+  //! Declares this class' options
+  // (Please implement in .cc)
+  static void declareOptions(OptionList& ol);
+
+  //! Fill the vector 'v' with the content of the i-th row.
+  //! v is assumed to be the right size.
+  virtual void getNewRow(int i, const Vec& v) const;
+
+public:
+
+  // Simply call inherited::build() then build_().
+  virtual void build();
+
+  //! Transform a shallow copy into a deep copy.
+  virtual void makeDeepCopyFromShallowCopy(map<const void*, void*>& copies);
+
+  //! returns value associated with a string (or MISSING_VALUE if there's no association for this string)
+  virtual real getStringVal(int col, const string & str) const
+    {
+      if(source)
+      {
+        int src_col = col%n_attributs;
+        return source->getStringVal(src_col,str);
+      }
+      
+      return MISSING_VALUE;
+    }
+
+  //! returns the string associated with value val 
+  //! for field# col. Or returns "" if no string is associated.
+  virtual string getValString(int col, real val) const
+    {
+      if(source)
+      {
+        int src_col = col%n_attributs;
+        return source->getValString(src_col,val);
+      }
+      
+      return "";
+    }
+
+  //! Declare name and deepCopy methods.
+  PLEARN_DECLARE_OBJECT(SelectAttributsSequenceVMatrix);
+
+};
+
+DECLARE_OBJECT_PTR(SelectAttributsSequenceVMatrix);
+
+} // end of namespace PLearn
+#endif
