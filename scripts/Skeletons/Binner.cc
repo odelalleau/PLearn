5fc2ac735bfb0e56e782cdd71dd5590efc8ae4cc
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2004-10-29 21:19:30 +0000

    Revamped with more methods and better readability

--- scripts/Skeletons/Binner.cc
+++ scripts/Skeletons/Binner.cc
@@ -3,75 +3,88 @@
 namespace PLearn {
 using namespace std;
 
+//////////////////
+// DERIVEDCLASS //
+//////////////////
 DERIVEDCLASS::DERIVEDCLASS() 
-  :Binner() 
-/* ### Initialise all fields to their default value */
-  {
-    // ...
+/* ### Initialize all fields to their default value */
+{
+  // ...
+  // ### You may or may not want to call build_() to finish building the object
+  // build_();
+}
 
-    // ### You may or may not want to call build_() to finish building the object
-    // build_();
-  }
+PLEARN_IMPLEMENT_OBJECT(DERIVEDCLASS,
+    "ONE LINE DESCR",
+    "NO HELP"
+);
 
+////////////////////
+// declareOptions //
+////////////////////
+void DERIVEDCLASS::declareOptions(OptionList& ol)
+{
+  // ### Declare all of this object's options here
+  // ### For the "flags" of each option, you should typically specify  
+  // ### one of OptionBase::buildoption, OptionBase::learntoption or 
+  // ### OptionBase::tuningoption. Another possible flag to be combined with
+  // ### is OptionBase::nosave
 
-PLEARN_IMPLEMENT_OBJECT(DERIVEDCLASS, "ONE LINE DESCR", "NO HELP");
+  // ### ex:
+  // declareOption(ol, "myoption", &DERIVEDCLASS::myoption, OptionBase::buildoption,
+  //               "Help text describing this option");
+  // ...
 
-  void DERIVEDCLASS::declareOptions(OptionList& ol)
-  {
-    // ### Declare all of this object's options here
-    // ### For the "flags" of each option, you should typically specify  
-    // ### one of OptionBase::buildoption, OptionBase::learntoption or 
-    // ### OptionBase::tuningoption. Another possible flag to be combined with
-    // ### is OptionBase::nosave
+  // Now call the parent class' declareOptions
+  inherited::declareOptions(ol);
+}
 
-    // ### ex:
-    // declareOption(ol, "myoption", &DERIVEDCLASS::myoption, OptionBase::buildoption,
-    //               "Help text describing this option");
-    // ...
+////////////
+// build //
+////////////
+void DERIVEDCLASS::build()
+{
+  inherited::build();
+  build_();
+}
 
-    // Now call the parent class' declareOptions
-    inherited::declareOptions(ol);
-  }
+////////////
+// build_ //
+////////////
+void DERIVEDCLASS::build_()
+{
+  // ### This method should do the real building of the object,
+  // ### according to set 'options', in *any* situation. 
+  // ### Typical situations include:
+  // ###  - Initial building of an object from a few user-specified options
+  // ###  - Building of a "reloaded" object: i.e. from the complete set of all serialised options.
+  // ###  - Updating or "re-building" of an object after a few "tuning" options have been modified.
+  // ### You should assume that the parent class' build_() has already been called.
+}
 
-  void DERIVEDCLASS::build_()
-  {
-    // ### This method should do the real building of the object,
-    // ### according to set 'options', in *any* situation. 
-    // ### Typical situations include:
-    // ###  - Initial building of an object from a few user-specified options
-    // ###  - Building of a "reloaded" object: i.e. from the complete set of all serialised options.
-    // ###  - Updating or "re-building" of an object after a few "tuning" options have been modified.
-    // ### You should assume that the parent class' build_() has already been called.
-  }
-
-  // ### Nothing to add here, simply calls build_
-  void DERIVEDCLASS::build()
-  {
-    inherited::build();
-    build_();
-  }
-
-
-  void DERIVEDCLASS::makeDeepCopyFromShallowCopy(CopiesMap& copies)
-  {
-    Object::makeDeepCopyFromShallowCopy(copies);
-
-    // ### Call deepCopyField on all "pointer-like" fields 
-    // ### that you wish to be deepCopied rather than 
-    // ### shallow-copied.
-    // ### ex:
-    // deepCopyField(trainvec, copies);
-
-    // ### Remove this line when you have fully implemented this method.
-    PLERROR("DERIVEDCLASS::makeDeepCopyFromShallowCopy not fully (correctly) implemented yet!");
-  }
-
-
-//! Returns a binning for a single column vmatrix v 
+////////////////
+// getBinning //
+////////////////
 RealMapping DERIVEDCLASS::getBinning(VMat v) const
 {
   // ### Construct the RealMapping from v and buildoptions.
 }
 
+/////////////////////////////////
+// makeDeepCopyFromShallowCopy //
+/////////////////////////////////
+void DERIVEDCLASS::makeDeepCopyFromShallowCopy(CopiesMap& copies)
+{
+  Object::makeDeepCopyFromShallowCopy(copies);
+
+  // ### Call deepCopyField on all "pointer-like" fields 
+  // ### that you wish to be deepCopied rather than 
+  // ### shallow-copied.
+  // ### ex:
+  // deepCopyField(trainvec, copies);
+
+  // ### Remove this line when you have fully implemented this method.
+  PLERROR("DERIVEDCLASS::makeDeepCopyFromShallowCopy not fully (correctly) implemented yet!");
+}
 
 } // end of namespace PLearn
--- scripts/Skeletons/Binner.h
+++ scripts/Skeletons/Binner.h
@@ -13,6 +13,7 @@ private:
   typedef Binner inherited;  
 
 protected:
+
   // *********************
   // * protected options *
   // *********************
@@ -33,30 +34,33 @@ public:
   // * Constructors *
   // ****************
 
-  // Default constructor, make sure the implementation in the .cc
-  // initializes all fields to reasonable default values.
+  //! Default constructor.
+  // ### make sure the implementation in the .cc
+  // ### initializes all fields to reasonable default values.
   DERIVEDCLASS();
-
 
   // ******************
   // * Object methods *
   // ******************
 
 private: 
+
   //! This does the actual building. 
   // (Please implement in .cc)
   void build_();
 
 protected: 
-  //! Declares this class' options
+
+  //! Declares this class' options.
   // (Please implement in .cc)
   static void declareOptions(OptionList& ol);
 
 public:
-  // simply calls inherited::build() then build_() 
+
+  // Simply calls inherited::build() then build_().
   virtual void build();
 
-  //! Transforms a shallow copy into a deep copy
+  //! Transforms a shallow copy into a deep copy.
   virtual void makeDeepCopyFromShallowCopy(CopiesMap& copies);
 
   //! Declares name and deepCopy methods
@@ -67,13 +71,24 @@ public:
   // ******************
 
 public:
+
   //! Returns a binning for a single column vmatrix v 
-  virtual RealMapping getBinning(VMat v) const;
+  virtual PP<RealMapping> getBinning(VMat v) const;
+
+  // ### You may want to override these methods.
+  /*
+  //! Return a vector whose i-th element is the list of the indices in 'v' that
+  //! belong to the i-th bin.
+  virtual TVec< TVec<int> > getBins(const Vec& v) const;
+
+  //! Return the number of bins computed by this binner.
+  virtual int nBins() const;
+  */
 
 };
 
 // Declares a few other classes and functions related to this class
-  DECLARE_OBJECT_PTR(DERIVEDCLASS);
+DECLARE_OBJECT_PTR(DERIVEDCLASS);
   
 } // end of namespace PLearn
 
