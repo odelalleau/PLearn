8be3f3eff723960d030ce3f6f58f529a9f4e9c89
chapados <chapados@15d43a8f-0efa-0310-bc34-a8a306da697e>
2004-12-25 07:59:45 +0000

    Initial commit

---
+++ plearn_learners/regressors/KNNRegressor.cc
@@ -0,0 +1,296 @@
+// -*- C++ -*-
+
+// KNNRegressor.cc
+//
+// Copyright (C) 2004 Nicolas Chapados 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: KNNRegressor.cc,v 1.1 2004/12/25 07:59:45 chapados Exp $ 
+   ******************************************************* */
+
+// Authors: Nicolas Chapados
+
+/*! \file KNNRegressor.cc */
+
+#include <math.h>
+
+#include <plearn/math/TMat_maths.h>
+#include <plearn_learners/nearest_neighbors/ExhaustiveNearestNeighbors.h>
+#include <plearn/ker/EpanechnikovKernel.h>
+#include "KNNRegressor.h"
+
+namespace PLearn {
+using namespace std;
+
+PLEARN_IMPLEMENT_OBJECT(
+  KNNRegressor,
+  "Implementation of the Nadaraya-Watson kernel estimator for regression",
+  "This class provides a simple multivariate regressor based upon an\n"
+  "enclosed K-nearest-neighbors finder (derived from\n"
+  "GenericNearestNeighbors; specified with the 'knn' option).\n"
+  "\n"
+  "The class contains several options to determine the number of neighbors\n"
+  "to use (K).  This number always overrides the option 'num_neighbors'\n"
+  "that may have been specified in the GenericNearestNeighbors utility\n"
+  "object.  Basically, the generic formula for the number of neighbors is\n"
+  "\n"
+  "    K = max(kmin, kmult*(n^kpow)),\n"
+  "\n"
+  "where 'kmin', 'kmult', and 'kpow' are options, and 'n' is the number of\n"
+  "examples in the training set.\n"
+  "\n"
+  "The cost output from this class is:\n"
+  "\n"
+  "- 'mse', the mean-squared error, i.e. given an output o and target t,\n"
+  "      mse(o,t) = sum_i (o[i]-t[i])^2,\n"
+  "\n"
+  "If the option 'use_knn_costs_as_weights' is true (by default), it is\n"
+  "assumed that the costs coming from the 'knn' object are kernel\n"
+  "evaluations for each nearest neighbor.  These are used as weights to\n"
+  "determine the final class probabilities.  (NOTE: it is important to use\n"
+  "a kernel that computes a SIMILARITY MEASURE, and not a DISTANCE MEASURE;\n"
+  "the default EpanechnikovKernel has the proper behavior.)  If the option\n"
+  "is false, an equal weighting is used (equivalent to square window).  In\n"
+  "addition, a different weighting kernel may be specified with the\n"
+  "'kernel' option.\n"
+  "\n"
+  "A local weighted regression model may be trained at each test point\n"
+  "by specifying a 'local_model'.  For instance, to perform local linear\n"
+  "regression, you may use a LinearRegressor for this purpose.\n"
+  );
+
+KNNRegressor::KNNRegressor()
+  : knn(new ExhaustiveNearestNeighbors(new EpanechnikovKernel(), false)),
+    kmin(5),
+    kmult(0.0),
+    kpow(0.5),
+    use_knn_costs_as_weights(true),
+    kernel(),
+    local_model()
+{ }
+
+void KNNRegressor::declareOptions(OptionList& ol)
+{
+  declareOption(
+    ol, "knn", &KNNRegressor::knn, OptionBase::buildoption,
+    "The K-nearest-neighbors finder to use (default is an\n"
+    "ExhaustiveNearestNeighbors with a EpanechnikovKernel, lambda=1)");
+
+  declareOption(
+    ol, "kmin", &KNNRegressor::kmin, OptionBase::buildoption,
+    "Minimum number of neighbors to use (default=5)");
+
+  declareOption(
+    ol, "kmult", &KNNRegressor::kmult, OptionBase::buildoption,
+    "Multiplicative factor on n^kpow to determine number of neighbors to\n"
+    "use (default=0)");
+
+  declareOption(
+    ol, "kpow", &KNNRegressor::kpow, OptionBase::buildoption,
+    "Power of the number of training examples to determine number of\n"
+    "neighbors (default=0.5)");
+
+  declareOption(
+    ol, "use_knn_costs_as_weights", &KNNRegressor::use_knn_costs_as_weights,
+    OptionBase::buildoption,
+    "Whether to weigh each of the K neighbors by the kernel evaluations,\n"
+    "obtained from the costs coming out of the 'knn' object (default=true)");
+
+  declareOption(
+    ol, "kernel", &KNNRegressor::kernel, OptionBase::buildoption,
+    "Disregard the 'use_knn_costs_as_weights' option, and use this kernel\n"
+    "to weight the observations.  If this object is not specified\n"
+    "(default), and the 'use_knn_costs_as_weights' is false, the\n"
+    "rectangular kernel is used.");
+
+  declareOption(
+    ol, "local_model", &KNNRegressor::local_model, OptionBase::buildoption,
+    "Train a local regression model from the K neighbors, weighted by\n"
+    "the kernel evaluations.  This is carried out at each test point.");
+  
+  // Now call the parent class' declareOptions
+  inherited::declareOptions(ol);
+}
+
+void KNNRegressor::build_()
+{
+  if (!knn)
+    PLERROR("KNNRegressor::build_: the 'knn' option must be specified");
+
+  if (kmin <= 0)
+    PLERROR("KNNRegressor::build_: the 'kmin' option must be strictly positive");
+}
+
+// ### Nothing to add here, simply calls build_
+void KNNRegressor::build()
+{
+  inherited::build();
+  build_();
+}
+
+
+void KNNRegressor::makeDeepCopyFromShallowCopy(CopiesMap& copies)
+{
+  deepCopyField(knn_output,           copies);
+  deepCopyField(knn_costs,            copies);
+  deepCopyField(knn,                  copies);
+  deepCopyField(kernel,               copies);
+  deepCopyField(local_model,          copies);
+  inherited::makeDeepCopyFromShallowCopy(copies);
+}
+
+
+int KNNRegressor::outputsize() const
+{
+  return train_set->targetsize();
+}
+
+
+void KNNRegressor::setTrainingSet(VMat training_set, bool call_forget)
+{
+  assert( knn );
+  inherited::setTrainingSet(training_set,call_forget);
+
+  // Now we carry out a little bit of tweaking on the embedded knn:
+  // - ask to report input+target+weight
+  // - set number of neighbors
+  // - set training set (which performs a build if necessary)
+  int n = training_set.length();
+  int num_neighbors = max(kmin, int(kmult*pow(n,kpow)));
+  knn->num_neighbors = num_neighbors;
+  knn->copy_input  = true;
+  knn->copy_target = true;
+  knn->copy_weight = true;
+  knn->copy_index  = false;
+  knn->setTrainingSet(training_set,call_forget);
+  knn_costs.resize(knn->nTestCosts());
+  knn_output.resize(knn->outputsize());
+}
+
+void KNNRegressor::forget()
+{
+  assert( knn );
+  knn->forget();
+}
+    
+void KNNRegressor::train()
+{
+  assert( knn );
+  knn->train();
+}
+
+void KNNRegressor::computeOutput(const Vec& input, Vec& output) const
+{
+  assert( output.size() == outputsize() );
+
+  // Start by computing the nearest neighbors
+  Vec knn_targets;                           //!< not used by knn
+  knn->computeOutputAndCosts(input, knn_targets, knn_output, knn_costs);
+
+  // For each neighbor, the KNN object outputs the following:
+  //     1) input vector
+  //     2) output vector
+  //     3) the weight (in all cases)
+  // We shall patch the weight of each neighbor (observation) to reflect
+  // the effect of the kernel weighting
+  const int inputsize    = input.size();
+  const int outputsize   = output.size();
+  const int weightoffset = inputsize+outputsize;
+  const int rowwidth     = weightoffset+1;
+  real* knn_output_data  = knn_output.data();
+  real total_weight      = 0.0;
+  for (int i=0, n=knn->num_neighbors; i<n; ++i, knn_output_data += rowwidth) {
+    real w;
+    if (kernel) {
+      Vec cur_input(inputsize, knn_output_data);
+      w = kernel(cur_input, input);
+    }
+    else if (use_knn_costs_as_weights)
+      w = knn_costs[i];
+    else
+      w = 1.0;
+
+    // Patch the existing weight
+    knn_output_data[weightoffset] *= w;
+    total_weight += knn_output_data[weightoffset];
+  }
+
+  // If total weight is too small, make the output all zeros
+  if (total_weight < 1e-6) {
+    output.fill(0.0);
+    return;
+  }
+  
+  // Now compute the output per se
+  if (! local_model) {
+    // If no local model was requested, simply perform a weighted
+    // average of the nearest-neighbors
+    output.fill(0.0);
+    knn_output_data = knn_output.data();
+    for (int i=0, n=knn->num_neighbors; i<n; ++i, knn_output_data+=rowwidth) {
+      Vec cur_output(outputsize, knn_output_data+inputsize);
+      multiplyAcc(output, cur_output,
+                  knn_output_data[weightoffset] / total_weight);
+    }
+  }
+  else {
+    // Reinterpret knn_output as a training set and use local model
+    Mat training_data = knn_output.toMat(knn->num_neighbors, rowwidth);
+    VMat training_set(training_data);
+    training_set->defineSizes(inputsize, outputsize, 1 /* weightsize */);
+    local_model->setTrainingSet(training_set, true /* forget */);
+    local_model->setTrainStatsCollector(new VecStatsCollector());
+    local_model->train();
+    local_model->computeOutput(input,output);
+  }
+}
+
+void KNNRegressor::computeCostsFromOutputs(const Vec& input, const Vec& output, 
+                                           const Vec& target, Vec& costs) const
+{
+  assert( costs.size() == 1 );
+  costs[0] = powdistance(output,target,2);
+}
+
+TVec<string> KNNRegressor::getTestCostNames() const
+{
+  static TVec<string> costs(1);
+  costs[0] = "mse";
+  return costs;
+}
+
+TVec<string> KNNRegressor::getTrainCostNames() const
+{
+  return TVec<string>();
+}
+
+
+} // end of namespace PLearn
---
+++ plearn_learners/regressors/KNNRegressor.h
@@ -0,0 +1,191 @@
+// -*- C++ -*-
+
+// KNNRegressor.h
+//
+// Copyright (C) 2004 Nicolas Chapados 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: KNNRegressor.h,v 1.1 2004/12/25 07:59:45 chapados Exp $ 
+   ******************************************************* */
+
+// Authors: Nicolas Chapados
+
+/*! \file KNNRegressor.h */
+
+
+#ifndef KNNRegressor_INC
+#define KNNRegressor_INC
+
+#include <plearn_learners/generic/PLearner.h>
+#include <plearn_learners/nearest_neighbors/GenericNearestNeighbors.h>
+
+namespace PLearn {
+
+/**
+ * This class provides a simple multivariate regressor based upon an
+ * enclosed K-nearest-neighbors finder (derived from
+ * GenericNearestNeighbors; specified with the 'knn' option).
+ *
+ * The class contains several options to determine the number of neighbors
+ * to use (K).  This number always overrides the option 'num_neighbors'
+ * that may have been specified in the GenericNearestNeighbors utility
+ * object.  Basically, the generic formula for the number of neighbors is
+ *
+ *     K = max(kmin, kmult*(n^kpow)),
+ *
+ * where 'kmin', 'kmult', and 'kpow' are options, and 'n' is the number of
+ * examples in the training set.
+ *
+ * The cost output from this class is:
+ *
+ * - 'mse', the mean-squared error, i.e. given an output o and target t,
+ *       mse(o,t) = \sum_i (o[i]-t[i])^2,
+ *
+ * If the option 'use_knn_costs_as_weights' is true (by default), it is
+ * assumed that the costs coming from the 'knn' object are kernel
+ * evaluations for each nearest neighbor.  These are used as weights to
+ * determine the final class probabilities.  (NOTE: it is important to use
+ * a kernel that computes a SIMILARITY MEASURE, and not a DISTANCE MEASURE;
+ * the default EpanechnikovKernel has the proper behavior.)  If the option
+ * is false, an equal weighting is used (equivalent to square window).  In
+ * addition, a different weighting kernel may be specified with the
+ * 'kernel' option.
+ *
+ * A local weighted regression model may be trained at each test point
+ * by specifying a 'local_model'.  For instance, to perform local linear
+ * regression, you may use a LinearRegressor for this purpose.
+ */
+class KNNRegressor: public PLearner
+{
+  typedef PLearner inherited;
+
+protected:
+  //! Internal use: temporary buffer for knn output
+  mutable Vec knn_output;
+
+  //! Internal use: temporary buffer for knn costs
+  mutable Vec knn_costs;
+  
+public:
+  //#####  Public Build Options  ############################################
+
+  //! The K-nearest-neighbors finder to use (default is an
+  //! ExhaustiveNearestNeighbors with an EpanechnikovKernel, lambda=1)
+  PP<GenericNearestNeighbors> knn;
+
+  //! Minimum number of neighbors to use (default=5)
+  int kmin;
+
+  //! Multiplicative factor on n^kpow to determine number of neighbors to
+  //! use (default=0)
+  real kmult;
+
+  //! Power of the number of training examples to determine number of
+  //! neighbors (default=0.5)
+  real kpow;
+
+  //! Whether to weigh each of the K neighbors by the kernel evaluations,
+  //! obtained from the costs coming out of the 'knn' object (default=true)
+  bool use_knn_costs_as_weights;
+
+  //! Disregard the 'use_knn_costs_as_weights' option, and use this kernel
+  //! to weight the observations.  If this object is not specified
+  //! (default), and the 'use_knn_costs_as_weights' is false, the
+  //! rectangular kernel is used.
+  Ker kernel;
+
+  //! Train a local regression model from the K neighbors, weighted by
+  //! the kernel evaluations.  This is carried out at each test point.
+  PP<PLearner> local_model;
+
+public:
+  //#####  Object Methods  ##################################################
+
+  //! Default constructor.
+  KNNRegressor();
+
+  //! Simply calls inherited::build() then build_().
+  virtual void build();
+
+  //! Transforms a shallow copy into a deep copy.
+  virtual void makeDeepCopyFromShallowCopy(CopiesMap& copies);
+
+  // Declares other standard object methods.
+  // If your class is not instantiatable (it has pure virtual methods)
+  // you should replace this by PLEARN_DECLARE_ABSTRACT_OBJECT.
+  PLEARN_DECLARE_OBJECT(KNNRegressor);
+
+public:
+  //#####  PLearner Methods  ####################################################
+
+  //! Overridden to call knn->setTrainingSet
+  virtual void setTrainingSet(VMat training_set, bool call_forget=true);
+
+  //! Returns the size of this learner's output, (which typically
+  //! may depend on its inputsize(), targetsize() and set options).
+  virtual int outputsize() const;
+
+  //! Forwarded to knn
+  virtual void forget();
+    
+  //! Forwarded to knn
+  virtual void train();
+
+  //! Computes the output from the input.
+  virtual void computeOutput(const Vec& input, Vec& output) const;
+
+  //! Computes the costs from already computed output. 
+  virtual void computeCostsFromOutputs(const Vec& input, const Vec& output, 
+                                       const Vec& target, Vec& costs) const;
+  
+  //! Returns the names of the costs computed by computeCostsFromOutpus
+  //! (and thus the test method).
+  virtual TVec<std::string> getTestCostNames() const;
+
+  //! Returns the names of the objective costs that the train method computes and 
+  //! for which it updates the VecStatsCollector train_stats.
+  virtual TVec<std::string> getTrainCostNames() const;
+
+private: 
+  //! This does the actual building. 
+  void build_();
+
+protected: 
+  //! Declares this class' options.
+  static void declareOptions(OptionList& ol);
+};
+
+// Declares a few other classes and functions related to this class.
+DECLARE_OBJECT_PTR(KNNRegressor);
+  
+} // end of namespace PLearn
+
+#endif
