aa12386ea99afcd0a0926a61c8ee267e1c6040a3
plearner <plearner@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-01-13 15:28:43 +0000

    *** empty log message ***

--- scripts/Transformations/005_includes_for_apstat_donations.py
+++ scripts/Transformations/005_includes_for_apstat_donations.py
@@ -3,5 +3,6 @@ def transformfunc(fname, text):
     text = re.sub(r'apstatlib/vmat/TextFilesVMatrix', 'plearn/vmat/TextFilesVMatrix', text)
     text = re.sub(r'apstatlib/learners/UniformizeLearner', 'plearn_learners/unsupervised/UniformizeLearner', text)    
     text = re.sub(r'commands/ApstatCommands/TxtmatCommand','commands/PLearnCommands/TxtmatCommand',text)
+    text = re.sub(r'\"ApstatCommands/TxtmatCommand.h\"','<commands/PLearnCommands/TxtmatCommand.h>',text)
     return text
     
