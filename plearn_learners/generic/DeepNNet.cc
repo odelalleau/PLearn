600dd870127700e165bb786856b11904ab7fac48
yoshua <yoshua@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-01-18 01:21:01 +0000

    *** empty log message ***

---
+++ plearn_learners/generic/DeepNNet.cc
@@ -0,0 +1,199 @@
+// -*- C++ -*-
+
+// DeepNNet.cc
+//
+// Copyright (C) 2005 Yoshua Bengio 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: DeepNNet.cc,v 1.1 2005/01/18 01:21:00 yoshua Exp $ 
+   ******************************************************* */
+
+// Authors: Yoshua Bengio
+
+/*! \file DeepNNet.cc */
+
+
+#include "DeepNNet.h"
+
+namespace PLearn {
+using namespace std;
+
+DeepNNet::DeepNNet() 
+/* ### Initialize all fields to their default value here */
+{
+  // ...
+
+  // ### You may or may not want to call build_() to finish building the object
+  // build_();
+}
+
+PLEARN_IMPLEMENT_OBJECT(DeepNNet, 
+                        "Deep multi-layer neural networks with sparse adaptive connections", 
+                        "This feedforward neural network can have many layers, but its weight\n"
+                        "matrices are sparse and can be optionally adapted (adding new connections\n"
+                        "where that would create the largest gradient).");
+
+void DeepNNet::declareOptions(OptionList& ol)
+{
+  // ### Declare all of this object's options here
+  // ### For the "flags" of each option, you should typically specify  
+  // ### one of OptionBase::buildoption, OptionBase::learntoption or 
+  // ### OptionBase::tuningoption. Another possible flag to be combined with
+  // ### is OptionBase::nosave
+
+  // ### ex:
+  // declareOption(ol, "myoption", &DeepNNet::myoption, OptionBase::buildoption,
+  //               "Help text describing this option");
+  // ...
+
+  // Now call the parent class' declareOptions
+  inherited::declareOptions(ol);
+}
+
+void DeepNNet::build_()
+{
+  // ### This method should do the real building of the object,
+  // ### according to set 'options', in *any* situation. 
+  // ### Typical situations include:
+  // ###  - Initial building of an object from a few user-specified options
+  // ###  - Building of a "reloaded" object: i.e. from the complete set of all serialised options.
+  // ###  - Updating or "re-building" of an object after a few "tuning" options have been modified.
+  // ### You should assume that the parent class' build_() has already been called.
+}
+
+// ### Nothing to add here, simply calls build_
+void DeepNNet::build()
+{
+  inherited::build();
+  build_();
+}
+
+
+void DeepNNet::makeDeepCopyFromShallowCopy(CopiesMap& copies)
+{
+  inherited::makeDeepCopyFromShallowCopy(copies);
+
+  // ### Call deepCopyField on all "pointer-like" fields 
+  // ### that you wish to be deepCopied rather than 
+  // ### shallow-copied.
+  // ### ex:
+  // deepCopyField(trainvec, copies);
+
+  // ### Remove this line when you have fully implemented this method.
+  PLERROR("DeepNNet::makeDeepCopyFromShallowCopy not fully (correctly) implemented yet!");
+}
+
+
+int DeepNNet::outputsize() const
+{
+  // Compute and return the size of this learner's output (which typically
+  // may depend on its inputsize(), targetsize() and set options).
+}
+
+void DeepNNet::forget()
+{
+  //! (Re-)initialize the PLearner in its fresh state (that state may depend on the 'seed' option)
+  //! And sets 'stage' back to 0   (this is the stage of a fresh learner!)
+    /*!
+      A typical forget() method should do the following:
+         - initialize a random number generator with the seed option
+         - initialize the learner's parameters, using this random generator
+         - stage = 0
+    */
+}
+    
+void DeepNNet::train()
+{
+    // The role of the train method is to bring the learner up to stage==nstages,
+    // updating train_stats with training costs measured on-line in the process.
+
+    /* TYPICAL CODE:
+
+      static Vec input  // static so we don't reallocate/deallocate memory each time...
+      static Vec target // (but be careful that static means shared!)
+      input.resize(inputsize())    // the train_set's inputsize()
+      target.resize(targetsize())  // the train_set's targetsize()
+      real weight
+
+      if(!train_stats)  // make a default stats collector, in case there's none
+         train_stats = new VecStatsCollector()
+
+      if(nstages<stage) // asking to revert to a previous stage!
+         forget()  // reset the learner to stage=0
+
+      while(stage<nstages)
+        {
+          // clear statistics of previous epoch
+          train_stats->forget() 
+          
+          //... train for 1 stage, and update train_stats,
+          // using train_set->getSample(input, target, weight)
+          // and train_stats->update(train_costs)
+          
+          ++stage
+          train_stats->finalize() // finalize statistics for this epoch
+        }
+    */
+}
+
+
+void DeepNNet::computeOutput(const Vec& input, Vec& output) const
+{
+  // Compute the output from the input.
+  // int nout = outputsize();
+  // output.resize(nout);
+  // ...
+}    
+
+void DeepNNet::computeCostsFromOutputs(const Vec& input, const Vec& output, 
+                                           const Vec& target, Vec& costs) const
+{
+// Compute the costs from *already* computed output. 
+// ...
+}                                
+
+TVec<string> DeepNNet::getTestCostNames() const
+{
+  // Return the names of the costs computed by computeCostsFromOutpus
+  // (these may or may not be exactly the same as what's returned by getTrainCostNames).
+  // ...
+}
+
+TVec<string> DeepNNet::getTrainCostNames() const
+{
+  // Return the names of the objective costs that the train method computes and 
+  // for which it updates the VecStatsCollector train_stats
+  // (these may or may not be exactly the same as what's returned by getTestCostNames).
+  // ...
+}
+
+
+} // end of namespace PLearn
---
+++ plearn_learners/generic/DeepNNet.h
@@ -0,0 +1,180 @@
+// -*- C++ -*-
+
+// DeepNNet.h
+//
+// Copyright (C) 2005 Yoshua Bengio 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: DeepNNet.h,v 1.1 2005/01/18 01:21:01 yoshua Exp $ 
+   ******************************************************* */
+
+// Authors: Yoshua Bengio
+
+/*! \file DeepNNet.h */
+
+
+#ifndef DeepNNet_INC
+#define DeepNNet_INC
+
+#include <plearn_learners/generic/PLearner.h>
+
+namespace PLearn {
+
+class DeepNNet: public PLearner
+{
+
+private:
+
+  typedef PLearner inherited;
+  
+protected:
+
+  // *********************
+  // * protected options *
+  // *********************
+
+  // ### declare protected option fields (such as learnt parameters) here
+  // ...
+    
+public:
+
+  // ************************
+  // * public build options *
+  // ************************
+
+  // ### declare public option fields (such as build options) here
+  // ...
+
+  // ****************
+  // * Constructors *
+  // ****************
+
+  //! Default constructor.
+  // (Make sure the implementation in the .cc
+  // initializes all fields to reasonable default values)
+  DeepNNet();
+
+
+  // ********************
+  // * PLearner methods *
+  // ********************
+
+private: 
+
+  //! This does the actual building. 
+  // (Please implement in .cc)
+  void build_();
+
+protected: 
+  
+  //! Declares this class' options.
+  // (Please implement in .cc)
+  static void declareOptions(OptionList& ol);
+
+public:
+
+  // ************************
+  // **** Object methods ****
+  // ************************
+
+  //! Simply calls inherited::build() then build_().
+  virtual void build();
+
+  //! Transforms a shallow copy into a deep copy.
+  virtual void makeDeepCopyFromShallowCopy(CopiesMap& copies);
+
+  // Declares other standard object methods.
+  // If your class is not instantiatable (it has pure virtual methods)
+  // you should replace this by PLEARN_DECLARE_ABSTRACT_OBJECT.
+  PLEARN_DECLARE_OBJECT(DeepNNet);
+
+
+  // **************************
+  // **** PLearner methods ****
+  // **************************
+
+  //! Returns the size of this learner's output, (which typically
+  //! may depend on its inputsize(), targetsize() and set options).
+  // (PLEASE IMPLEMENT IN .cc)
+  virtual int outputsize() const;
+
+  //! (Re-)initializes the PLearner in its fresh state (that state may depend on the 'seed' option)
+  //! And sets 'stage' back to 0 (this is the stage of a fresh learner!).
+  // (PLEASE IMPLEMENT IN .cc)
+  virtual void forget();
+
+    
+  //! The role of the train method is to bring the learner up to stage==nstages,
+  //! updating the train_stats collector with training costs measured on-line in the process.
+  // (PLEASE IMPLEMENT IN .cc)
+  virtual void train();
+
+
+  //! Computes the output from the input.
+  // (PLEASE IMPLEMENT IN .cc)
+  virtual void computeOutput(const Vec& input, Vec& output) const;
+
+  //! Computes the costs from already computed output. 
+  // (PLEASE IMPLEMENT IN .cc)
+  virtual void computeCostsFromOutputs(const Vec& input, const Vec& output, 
+                                       const Vec& target, Vec& costs) const;
+                                
+
+  //! Returns the names of the costs computed by computeCostsFromOutpus (and thus the test method).
+  // (PLEASE IMPLEMENT IN .cc)
+  virtual TVec<std::string> getTestCostNames() const;
+
+  //! Returns the names of the objective costs that the train method computes and 
+  //! for which it updates the VecStatsCollector train_stats.
+  // (PLEASE IMPLEMENT IN .cc)
+  virtual TVec<std::string> getTrainCostNames() const;
+
+
+  // *** SUBCLASS WRITING: ***
+  // While in general not necessary, in case of particular needs 
+  // (efficiency concerns for ex) you may also want to overload
+  // some of the following methods:
+  // virtual void computeOutputAndCosts(const Vec& input, const Vec& target, Vec& output, Vec& costs) const;
+  // virtual void computeCostsOnly(const Vec& input, const Vec& target, Vec& costs) const;
+  // virtual void test(VMat testset, PP<VecStatsCollector> test_stats, VMat testoutputs=0, VMat testcosts=0) const;
+  // virtual int nTestCosts() const;
+  // virtual int nTrainCosts() const;
+  // virtual void resetInternalState();
+  // virtual bool isStatefulLearner() const;
+
+};
+
+// Declares a few other classes and functions related to this class.
+DECLARE_OBJECT_PTR(DeepNNet);
+  
+} // end of namespace PLearn
+
+#endif
