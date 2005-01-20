a9e5ac00e1eff95133c59d98fa4a935b909c8d73
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-01-20 21:35:51 +0000

    Includes for plearn_full (classes that require fancy external libraries)

---
+++ commands/plearn_full_inc.h
@@ -0,0 +1,68 @@
+// -*- C++ -*-
+
+// plearn_full_inc.h
+//
+// Copyright (C) 2005 Olivier Delalleau 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: plearn_full_inc.h,v 1.1 2005/01/20 21:35:51 tihocan Exp $ 
+   ******************************************************* */
+
+// Authors: Olivier Delalleau
+
+/*! \file plearn_full_inc.h */
+
+/*! Include here all classes available in the PLearn CVS repository
+    that are dependent upon some external libraries.
+ */
+
+#ifndef plearn_full_inc_INC
+#define plearn_full_inc_INC
+
+/*********
+ * Boost *
+ *********/
+#include <commands/PLearnCommands/HTMLHelpCommand.h>
+
+/**********
+ * Curses *
+ **********/
+#include <commands/PLearnCommands/VMatCommand.h>
+
+/*********
+ * Torch *
+ *********/
+#include <plearn_learners/classifiers/SVMClassificationTorch.h>
+#include <plearn_torch/TorchKernel.h>
+#include <plearn_torch/TorchDataSet.h>
+
+#endif
+
