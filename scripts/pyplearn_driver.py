06c948d47cfc1c6949d97d455b214ee868d3a449
chapados <chapados@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-02-20 06:55:54 +0000

    Add the absolute directory portion of the current script to the path

--- scripts/pyplearn_driver.py
+++ scripts/pyplearn_driver.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python2.3
-__cvs_id__ = "$Id: pyplearn_driver.py,v 1.8 2005/02/18 14:16:47 tihocan Exp $"
+__cvs_id__ = "$Id: pyplearn_driver.py,v 1.9 2005/02/20 06:55:54 chapados Exp $"
 
 from plearn.pyplearn import *
 
 import sys
+import os.path
 from plearn.pyplearn.pyplearn import _parse_plargs, _postprocess_refs
+
+### Add the absolute directory portion of the current script to the path
+sys.path = [os.path.dirname(os.path.abspath(sys.argv[1]))] + sys.path
 
 pyplearn_file = open(sys.argv[1], 'U')
 lines = pyplearn_file.read()
@@ -26,5 +30,5 @@ else:
     _parse_plargs(sys.argv[2:])
     lines += 'print _postprocess_refs(str(main()))\n'
 
-del pyplearn_file, sys, _parse_plargs
+# del pyplearn_file, sys, _parse_plargs
 exec ''.join(lines) in globals()
