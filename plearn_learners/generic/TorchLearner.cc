0b88c4baeefcb1a7e12d5d4617eea77ba6f0a00a
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-02-23 01:28:40 +0000

    A generic learner that can use Torch learning algorithms

---
+++ plearn_learners/generic/TorchLearner.cc
@@ -0,0 +1,244 @@
+// -*- C++ -*-
+
+// TorchLearner.cc
+//
+// Copyright (C) 2005 Olivier Delalleau 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: TorchLearner.cc,v 1.1 2005/02/23 01:28:40 tihocan Exp $ 
+   ******************************************************* */
+
+// Authors: Olivier Delalleau
+
+/*! \file TorchLearner.cc */
+
+
+#include "TorchLearner.h"
+#include <plearn_torch/TMachine.h>
+#include <plearn_torch/TTorchDataSetFromVMat.h>
+#include <plearn_torch/TTrainer.h>
+
+namespace PLearn {
+using namespace std;
+
+//////////////////
+// TorchLearner //
+//////////////////
+TorchLearner::TorchLearner() 
+: outputsize_(-1)
+{
+  allocator = new Torch::Allocator;
+  inputs = 0;
+}
+
+PLEARN_IMPLEMENT_OBJECT(TorchLearner,
+    "A generic learner that can use Torch learning algorithms.",
+    ""
+);
+
+////////////////////
+// declareOptions //
+////////////////////
+void TorchLearner::declareOptions(OptionList& ol)
+{
+  // ### For the "flags" of each option, you should typically specify  
+  // ### one of OptionBase::buildoption, OptionBase::learntoption or 
+  // ### OptionBase::tuningoption. Another possible flag to be combined with
+  // ### is OptionBase::nosave
+
+  // Build options.
+
+  declareOption(ol, "machine", &TorchLearner::machine, OptionBase::buildoption,
+      "The Torch learning machine.");
+
+  declareOption(ol, "trainer", &TorchLearner::trainer, OptionBase::buildoption,
+      "The Torch trainer, responsible for training the machine.");
+
+  // Learnt options.
+
+  declareOption(ol, "outputsize", &TorchLearner::outputsize_, OptionBase::learntoption,
+      "Saves the output size of this learner for faster access.");
+
+  // Now call the parent class' declareOptions.
+  inherited::declareOptions(ol);
+}
+
+///////////
+// build //
+///////////
+void TorchLearner::build()
+{
+  inherited::build();
+  build_();
+}
+
+////////////
+// build_ //
+////////////
+void TorchLearner::build_()
+{
+  // ### This method should do the real building of the object,
+  // ### according to set 'options', in *any* situation. 
+  // ### Typical situations include:
+  // ###  - Initial building of an object from a few user-specified options
+  // ###  - Building of a "reloaded" object: i.e. from the complete set of all serialised options.
+  // ###  - Updating or "re-building" of an object after a few "tuning" options have been modified.
+  // ### You should assume that the parent class' build_() has already been called.
+  if (trainer) {
+    trainer->machine = machine;
+    trainer->build();
+  }
+  if (machine && machine->outputs)
+    outputsize_ = machine->outputs->frame_size;
+}
+
+/////////////////////////////
+// computeCostsFromOutputs //
+/////////////////////////////
+void TorchLearner::computeCostsFromOutputs(const Vec& input, const Vec& output, 
+                                           const Vec& target, Vec& costs) const
+{
+  // No cost computed for now.
+}                                
+
+///////////////////
+// computeOutput //
+///////////////////
+void TorchLearner::computeOutput(const Vec& input, Vec& output) const
+{
+  assert( outputsize_ >= 0);
+  output.resize(outputsize_);
+  inputs->copyFrom(input.data());
+  machine->forward(inputs);
+  machine->outputs->copyTo(output.data());
+}    
+
+////////////
+// forget //
+////////////
+void TorchLearner::forget()
+{
+  stage = 0;
+  outputsize_ = -1;
+  if (machine)
+    machine->reset();
+}
+    
+//////////////////////
+// getTestCostNames //
+//////////////////////
+TVec<string> TorchLearner::getTestCostNames() const
+{
+  // No cost computed for now.
+  return TVec<string>();
+}
+
+///////////////////////
+// getTrainCostNames //
+///////////////////////
+TVec<string> TorchLearner::getTrainCostNames() const
+{
+  // No cost computed for now.
+  return TVec<string>();
+}
+
+/////////////////////////////////
+// makeDeepCopyFromShallowCopy //
+/////////////////////////////////
+void TorchLearner::makeDeepCopyFromShallowCopy(CopiesMap& copies)
+{
+  inherited::makeDeepCopyFromShallowCopy(copies);
+
+  // ### ex:
+  // deepCopyField(trainvec, copies);
+
+  // ### Remove this line when you have fully implemented this method.
+  PLERROR("TorchLearner::makeDeepCopyFromShallowCopy not fully (correctly) implemented yet!");
+}
+
+////////////////
+// outputsize //
+////////////////
+int TorchLearner::outputsize() const
+{
+  // Compute and return the size of this learner's output, (which typically
+  // may depend on its inputsize(), targetsize() and set options).
+  if (!machine)
+    PLERROR("In TorchLearner::outputsize - Cannot return the outputsize without setting "
+            "the 'machine' option");
+  return machine->outputs->frame_size;
+}
+
+////////////////////
+// setTrainingSet //
+////////////////////
+void TorchLearner::setTrainingSet(VMat training_set, bool call_forget) {
+  inherited::setTrainingSet(training_set, call_forget);
+  torch_train_set = new TTorchDataSetFromVMat(training_set);
+  pout << "Yo: " << torch_train_set->dataset->n_examples << endl;
+  pout << "Yo: " << torch_train_set->dataset->n_real_examples << endl;
+  pout << "Yo: " << torch_train_set->torch_dataset_from_vmat->n_examples << endl;
+  pout << "Yo: " << torch_train_set->torch_dataset_from_vmat->n_real_examples << endl;
+  allocator->free(inputs); // Free old input sequence.
+  inputs = new(allocator) Torch::Sequence(1, training_set->inputsize());
+}
+
+///////////
+// train //
+///////////
+void TorchLearner::train()
+{
+  if (stage >= nstages) {
+    PLWARNING("In TorchLearner::train - Learner has already been trained, skipping training");
+    return;
+  }
+  if (!trainer || !machine)
+    PLERROR("In TorchLearner::train - You must set both the 'trainer' and 'machine' options "
+            "before calling train()");
+  pout << torch_train_set->dataset->n_examples << endl;
+  pout << torch_train_set->dataset->n_real_examples << endl;
+  pout << torch_train_set->torch_dataset_from_vmat->n_examples << endl;
+  pout << torch_train_set->torch_dataset_from_vmat->n_real_examples << endl;
+  trainer->train((TTorchDataSetFromVMat*) torch_train_set);
+  if (machine->outputs)
+    // Update outputsize_
+    outputsize_ = machine->outputs->frame_size;
+  stage = 1;
+}
+
+///////////////////
+// ~TorchLearner //
+///////////////////
+TorchLearner::~TorchLearner() {
+  delete allocator;
+}
+
+} // end of namespace PLearn
