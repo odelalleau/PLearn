12c36d3b3cb4989b0d665731044cba60db912cdb
dorionc <dorionc@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-03-11 02:42:52 +0000

    New cluster utilities

---
+++ python_modules/plearn/taskutils/ArgumentsOracle.py
@@ -0,0 +1,157 @@
+"""Cluster task arguments iterator.
+
+See http://www.python.org/doc/2.3.4/lib/typeiter.html for an introduction to Python's iterator protocol.
+"""
+import os, re
+
+class ArgumentsOracle:
+    """A base class for task arguments iterators.
+
+    The ArgumentsOracle is iterator whose next() method returns a
+    formatted string representing the task arguments.
+    """
+    name_value_joining_token = '='
+    arguments_joining_token  = " "
+    
+    def __iter__( self ):
+        return self
+
+    def next( self ):
+        """Returns a formatted string representing the task arguments."""
+        raise NotImplementedError
+
+    def reset( self ):
+        raise NotImplementedError
+        
+class Cursor:
+    def __init__( self, start = 0, restart = None ):
+        self._value   = start
+        self.restart = restart
+
+    def inc( self ):
+        """Returns True if the cursor was reset to 0."""
+        self._value  += 1
+
+        if ( self.restart is not None and
+             self.restart == self._value ):
+            self._value = 0
+            return True
+
+        ## Return False otherwise
+        return False        
+
+    def set_value( self, val ):
+        self._value = val
+        
+    def value( self ):
+        return self._value
+    
+class CrossProductOracle( ArgumentsOracle ):
+    def __init__( self, name_values_pairs ):
+        """Sets the internal state of the iterator.
+
+        @param name_values_pairs: A list of pairs having (I{name},
+        I{values}) form where I{name} must be a string. The I{values} entry
+        can be a single value or a list of values.
+        @type  name_values_pairs: List of pairs.
+        """
+        self.nargs             = len(name_values_pairs)
+        self.cursors           = []
+        self.name_values_pairs = []
+        
+        for (name, values) in name_values_pairs:
+            ## Support for non-list values
+            if not isinstance(values, list):
+                values = [values]
+
+            ## Initialize cursors
+            self.cursors.append( Cursor(0, len(values)) )
+            self.name_values_pairs.append((name,values))
+
+        ## Starting before the first sequence
+        self.cursors[-1].set_value(-1)
+        
+    def next( self ):
+        argc               = self.nargs        
+        reversed_arguments = []
+        do_inc             = True  ## Always increment the last cursor
+        while argc > 0:
+            argc -= 1
+            (name, values) = self.name_values_pairs[argc]
+            cursor         = self.cursors[argc]
+            
+            if do_inc:
+                do_inc = cursor.inc()
+
+            reversed_arguments.append( "%s%s%s"
+                                       % ( name, self.name_value_joining_token,
+                                           str(values[cursor.value()])
+                                           ))
+
+        ## If the first list of values has expired, the old set of
+        ## arguments was processed
+        if do_inc:
+            raise StopIteration
+
+        reversed_arguments.reverse()
+        return self.arguments_joining_token.join( reversed_arguments )
+
+    def reset( self ):
+        for i, pair in enumerate(self.name_values_pairs):
+            self.cursors[i].set_value(0)
+        ## Starting before the first sequence
+        self.cursors[-1].set_value(-1)
+
+class DirectorySplittedOracle( ArgumentsOracle ):
+    def __init__( self, arguments_oracle, *build_dirname_from ):                
+        self.underlying_oracle = arguments_oracle
+
+        nvjoin      = self.underlying_oracle.name_value_joining_token
+        self.ajoin  = self.underlying_oracle.arguments_joining_token
+
+        regexp = "" 
+        for param in build_dirname_from:
+            regexp += "%s%s\S%s" % (param, nvjoin, self.ajoin)
+        self.regexp = "(%s)"%regexp
+
+        self.curdir                = ""
+        self.directory_when_called = os.getcwd()
+
+    def next( self ):
+        _next   = self.underlying_oracle.next()
+        match   = re.match( self.regexp, _next ).groups()[0]
+        dirname = match.rstrip(self.ajoin)
+
+        if self.curdir != dirname:
+            os.chdir( self.directory_when_called )
+            os.mkdir( dirname )
+            os.chdir( dirname )
+            self.curdir = dirname
+
+        return _next
+        
+    def reset( self ):
+        self.underlying_oracle.reset()
+    
+if __name__ == '__main__':
+    pairs = []
+    pairs.append(( "first",  [1,2,3] ))
+    pairs.append(( "second", 10 ))
+    pairs.append(( "third",  [100, 200] ))
+
+    cporacle = CrossProductOracle( pairs )
+    print "CrossProductOracle( %s )" % pairs
+    
+    for arguments in cporacle:
+        print arguments
+    print "## End of cporacle arguments"
+
+
+    print
+    print "DirectorySplittedOracle"
+    cporacle.reset()
+    dsoracle = DirectorySplittedOracle( cporacle, "first" )
+    
+    for arguments in dsoracle:
+        print arguments
+    print "## End of dsoracle arguments"
---
+++ python_modules/plearn/taskutils/cluster.py
@@ -0,0 +1,143 @@
+import os, shutil, time
+
+from ArgumentsOracle                 import *
+from plearn.utilities.toolkit        import command_output 
+from plearn.pyplearn.PyPLearnObject  import PyPLearnObject
+
+class Cluster(PyPLearnObject):
+    class Defaults:            
+        expdir_pattern           = "expdir_.*"
+        command_format           = 'cluster --execute "%s" --force --wait --duree=120h'
+        max_nmachines            = 15
+        sleep_time               = 120
+        logdir_path              = "LOGS"
+        wait_for_expdir_creation = True
+
+    def dispatch( self, program_call, arguments_oracle ):
+        if not isinstance(arguments_oracle, ArgumentsOracle): raise TypeError
+
+        expdir_count = None
+        if self.wait_for_expdir_creation:
+            expdir_count = count_expdirs( self.expdir_pattern )
+
+        ## The ArgumentsOracle is an iterator whose next() method returns a
+        ## formatted string representing the program arguments.
+        for arguments in arguments_oracle:
+
+            ## Building the raw command and, afterwards, the cluster command
+            ## from the program_call and stringified arguments
+            raw_command = self.join_prog_and_arguments( program_call, arguments )
+            cluster_cmd = command( raw_command, self.logdir_path, self.command_format )
+
+            print "Launching %s on cluster" % raw_command
+            os.system( cluster_cmd )
+
+            ## This hack is a turnaround to the cluster command bug of
+            ## possibly returning before the task is actually launched
+            if self.wait_for_expdir_creation:
+                current_count = count_expdirs( self.expdir_pattern )
+                while current_count == expdir_count:
+                    time.sleep(2)
+                    current_count = count_expdirs( self.expdir_pattern )
+
+    ##     experiment_directories = []
+    ##     for rav in risk_aversion:
+    ##         dirname    = dirname_format  % str(rav)
+    ##         vsystem( "mkdir %s" % dirname )
+    ##         vsystem( "(cd %s; ln -s ../pca_inputs.pmat; ln -s ../pca_inputs.pmat.metadata)"%dirname )
+
+    ##         ## Proper call to apdispatch (will be replaced by CLUSTER)
+    ##         dispatch_command  = 'apdispatch finlearn ../kernel_based_model.pyplearn '
+    ##         dispatch_command += '"master_ncomps=%s" '         % str(master_ncomps)
+    ##         dispatch_command += '"gaussian_kernel_sigma=%s" ' % str(gaussian_kernel_sigma)
+    ##         dispatch_command += '"risk_aversion=%s" '         % str(rav)
+    ##         dispatch_command += ( '"weight_decay={{%s}}" '
+    ##                               % ",".join([ str(wd) for wd in weight_decay])
+    ##                               )
+
+    ##         vsystem( '(cd %s; %s)' % ( dirname, dispatch_command ) )
+
+    ##         while ( machines_used_by_user() >= max_nmachines ):
+    ##             time.sleep( sleep_time )
+    ##         experiment_directories.append( dirname )
+    ##         print
+
+    ##     ### Wait for all experiments completion
+    ##     while ( machines_used_by_user() > 0 ):
+    ##         time.sleep( sleep_time )
+
+    ##     for directory in experiment_directories:
+    ##         vsystem( '(cd %s; %s)' % (directory, cluster(report_command)) )
+
+    ##     ### Wait for all reports completion
+    ##     while ( machines_used_by_user() > 0 ):
+    ##         time.sleep( sleep_time / 4 )
+
+    def join_prog_and_arguments( self, program_call, arguments ):
+        return " ".join([ program_call, arguments ])
+    
+def command( raw_command, logdir_path = None, format = Cluster.Defaults.command_format ):
+    processed_cmd = raw_command
+
+    ## Processing the raw command and piping the output in a logfile
+    if logdir_path is not None:
+        if not os.path.exists( logdir_path ):
+            os.mkdir( logdir_path )
+        
+        logfile_name = raw_command
+        for s in [" ", ".", "/"]:
+            logfile_name = logfile_name.replace(s, "_")
+        processed_cmd = "%s > %s" % (
+            raw_command, os.path.join( logdir_path, logfile_name)
+            )
+
+    ## Enclosing the command in a cluster call
+    cluster_cmd = format % processed_cmd
+    return cluster_cmd
+
+def count_expdirs( expdir_pattern = Cluster.Defaults.expdir_pattern ):
+    xpdirs = command_output( 'ls -1 | grep "%s"' % expdir_pattern )
+    return len(xpdirs)
+
+def machines_used_by_user():
+    cl = command_output( 'cluster --list | grep "cpu.*%s"'
+                         % os.getenv("USER")
+                         )
+    return len(cl)
+
+if __name__ == '__main__':
+    pairs = []
+    pairs.append(( "first",  [1,2,3] ))
+    pairs.append(( "second", 10 ))
+    pairs.append(( "third",  [100, 200] ))
+
+    cporacle = CrossProductOracle( pairs )
+    print "CrossProductOracle( %s )" % pairs
+
+    ## Simply echoing
+    cluster = Cluster( command_format = "%s", wait_for_expdir_creation=False )
+    cluster.dispatch( "echo", cporacle )
+    
+    print "## End of basic dispatch"
+
+    ## Creating a directory
+    cporacle.reset()
+    cporacle.arguments_joining_token = "_"    
+
+    cluster = Cluster( command_format = "%s",
+                       logdir_path    = "LOGS_MKDIR",
+                       expdir_pattern = "first=.*"  )    
+    cluster.dispatch( "mkdir", cporacle )
+    print "## End of mkdir dispatch"
+
+    ## Creating a directory
+    cporacle.reset()
+    dsoracle = DirectorySplittedOracle( cporacle, "first" )
+
+    cluster  = Cluster( command_format = "%s",
+                        logdir_path    = "LOGS_DSORACLE",
+                        wait_for_expdir_creation=False  )
+    cluster.dispatch( "touch", dsoracle )
+    
+    print "## End of dsoracle dispatch"
+    
