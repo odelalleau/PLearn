e0803f27662216c87cfcf42c459831d27ca5f95b
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-03-24 15:27:57 +0000

    Added a debug check

--- plearn/math/pl_erf.cc
+++ plearn/math/pl_erf.cc
@@ -109,6 +109,8 @@ real pl_gser(real a, real x) {
 // the incomplete gamma function
 real  pl_gcf(real a, real x)
 {
+  assert( !is_missing(a) && !is_missing(x) );
+  
 	int i;
 	real an,b,c,d,del,h;
 
