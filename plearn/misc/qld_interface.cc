d80e375fe041be07925a9013bdd167e91eba2271
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-04-12 15:32:05 +0000

    Replaced ApSTAT header by PLearn's one

--- plearn/misc/qld_interface.cc
+++ plearn/misc/qld_interface.cc
@@ -2,18 +2,38 @@
 
 // qld_interface.cc
 //
-// Copyright (C) 2004 ApSTAT Technologies Inc. 
-// All rights reserved.
-//
-// This program may not be modified, translated, 
-// copied or distributed in whole or in part or 
-// as part of a derivative work, in any form, 
-// whether as source code or binary code or any 
-// other form of translation, except if authorized 
-// by a prior agreement signed with ApSTAT Technologies Inc.
+// Copyright (C) 2005 ApSTAT Technologies Inc. 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
 
 /* *******************************************************      
-   * $Id: qld_interface.cc,v 1.1 2005/04/06 23:40:45 chapados Exp $ 
+   * $Id: qld_interface.cc,v 1.2 2005/04/12 15:32:05 tihocan Exp $ 
    ******************************************************* */
 
 // Authors: Nicolas Chapados
