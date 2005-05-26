d20fdd4a8bd1077ae2104095785bc91e51779fef
crompb <crompb@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-05-26 19:57:14 +0000

    Initial deposit

---
+++ plearn/ker/VMatKernel.cc
@@ -0,0 +1,165 @@
+// -*- C++ -*-
+
+// VMatKernel.cc
+//
+// Copyright (C) 2005 Benoit Cromp 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: VMatKernel.cc,v 1.1 2005/05/26 19:57:14 crompb Exp $ 
+   ******************************************************* */
+
+// Authors: Benoit Cromp
+
+/*! \file VMatKernel.cc */
+
+
+#include "VMatKernel.h"
+
+namespace PLearn {
+using namespace std;
+
+//////////////////
+// VMatKernel //
+//////////////////
+VMatKernel::VMatKernel() 
+/* ### Initialize all fields to their default value here */
+{
+  // ...
+
+  // ### You may or may not want to call build_() to finish building the object
+  // build_();
+}
+
+PLEARN_IMPLEMENT_OBJECT(VMatKernel,
+    "wraper for treating a VMatrix as a kernel",
+    "MULTI-LINE \nHELP"
+);
+
+////////////////////
+// declareOptions //
+////////////////////
+void VMatKernel::declareOptions(OptionList& ol)
+{
+  // ### Declare all of this object's options here
+  // ### For the "flags" of each option, you should typically specify  
+  // ### one of OptionBase::buildoption, OptionBase::learntoption or 
+  // ### OptionBase::tuningoption. Another possible flag to be combined with
+  // ### is OptionBase::nosave
+
+  // ### ex:
+  // declareOption(ol, "myoption", &VMatKernel::myoption, OptionBase::buildoption,
+  //               "Help text describing this option");
+  // ...
+
+  declareOption(ol,"source",&VMatKernel::source,OptionBase::buildoption,"Similarity matrix");
+
+  // Now call the parent class' declareOptions
+  inherited::declareOptions(ol);
+}
+
+///////////
+// build //
+///////////
+void VMatKernel::build()
+{
+  // ### Nothing to add here, simply calls build_
+  inherited::build();
+  build_();
+}
+
+////////////
+// build_ //
+////////////
+void VMatKernel::build_()
+{
+  // ### This method should do the real building of the object,
+  // ### according to set 'options', in *any* situation. 
+  // ### Typical situations include:
+  // ###  - Initial building of an object from a few user-specified options
+  // ###  - Building of a "reloaded" object: i.e. from the complete set of all serialised options.
+  // ###  - Updating or "re-building" of an object after a few "tuning" options have been modified.
+  // ### You should assume that the parent class' build_() has already been called.
+  
+}
+
+//////////////
+// evaluate //
+//////////////
+real VMatKernel::evaluate(const Vec& x1, const Vec& x2) const {
+  // ### Evaluate the kernel on a pair of points.
+  assert(x1.size()==1 && x2.size()==1);
+  return source->get(int(x1[0]),int(x2[0]));
+}
+
+void VMatKernel::computeGramMatrix(Mat K) const
+{
+  K << source->toMat();
+}
+
+
+/* ### This method will very often be overridden.
+//////////////////
+// evaluate_i_j //
+//////////////////
+real VMatKernel::evaluate_i_j(int i, int j) const {
+  // ### Evaluate the kernel on a pair of training points.
+}
+*/
+
+/////////////////////////////////
+// makeDeepCopyFromShallowCopy //
+/////////////////////////////////
+void VMatKernel::makeDeepCopyFromShallowCopy(CopiesMap& copies)
+{
+  inherited::makeDeepCopyFromShallowCopy(copies);
+
+  // ### Call deepCopyField on all "pointer-like" fields 
+  // ### that you wish to be deepCopied rather than 
+  // ### shallow-copied.
+  // ### ex:
+  // deepCopyField(trainvec, copies);
+
+  // ### Remove this line when you have fully implemented this method.
+  PLERROR("VMatKernel::makeDeepCopyFromShallowCopy not fully (correctly) implemented yet!");
+}
+
+/* ### This method will be overridden if computations need to be done,
+   ### or to forward the call to another object.
+   ### In this case, be careful that it may be called BEFORE the build_()
+   ### method has been called, if the 'specify_dataset' option is used.
+////////////////////////////
+// setDataForKernelMatrix //
+////////////////////////////
+void VMatKernel::setDataForKernelMatrix(VMat the_data) {
+}
+*/
+
+} // end of namespace PLearn
---
+++ plearn/ker/VMatKernel.h
@@ -0,0 +1,152 @@
+// -*- C++ -*-
+
+// VMatKernel.h
+//
+// Copyright (C) 2005 Benoit Cromp 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: VMatKernel.h,v 1.1 2005/05/26 19:57:14 crompb Exp $ 
+   ******************************************************* */
+
+// Authors: Benoit Cromp
+
+/*! \file VMatKernel.h */
+
+
+#ifndef VMatKernel_INC
+#define VMatKernel_INC
+
+#include <plearn/ker/Kernel.h>
+#include <plearn/vmat/VMat.h>
+
+namespace PLearn {
+
+class VMatKernel: public Kernel
+{
+
+private:
+
+  typedef Kernel inherited;
+  
+protected:
+
+  // *********************
+  // * Protected options *
+  // *********************
+
+  // ### declare protected option fields (such as learnt parameters) here
+  // ...
+    
+public:
+
+  // ************************
+  // * Public build options *
+  // ************************ 
+
+  VMat source;
+
+  // ### declare public option fields (such as build options) here
+  // ...
+
+  // ****************
+  // * Constructors *
+  // ****************
+
+  //! Default constructor.
+  // Make sure the implementation in the .cc initializes all fields to
+  // reasonable default values.
+  VMatKernel();
+
+  // ******************
+  // * Kernel methods *
+  // ******************
+
+private: 
+
+  //! This does the actual building. 
+  // (Please implement in .cc)
+  void build_();
+
+protected: 
+  
+  //! Declares this class' options.
+  // (Please implement in .cc)
+  static void declareOptions(OptionList& ol);
+
+public:
+
+  // ************************
+  // **** Object methods ****
+  // ************************
+
+  //! Simply calls inherited::build() then build_().
+  virtual void build();
+
+  //! Transforms a shallow copy into a deep copy.
+  virtual void makeDeepCopyFromShallowCopy(CopiesMap& copies);
+
+  // Declares other standard object methods.
+  // If your class is not instantiatable (it has pure virtual methods)
+  // you should replace this by PLEARN_DECLARE_ABSTRACT_OBJECT_METHODS.
+  PLEARN_DECLARE_OBJECT(VMatKernel);
+
+  // ************************
+  // **** Kernel methods ****
+  // ************************
+
+  //! Compute K(x1,x2).
+  virtual real evaluate(const Vec& x1, const Vec& x2) const;
+
+  // *** SUBCLASS WRITING: ***
+  // While in general not necessary, in case of particular needs 
+  // (efficiency concerns for ex) you may also want to overload
+  // some of the following methods:
+  // virtual real evaluate_i_j(int i, int j) const;
+  // virtual real evaluate_i_x(int i, const Vec& x, real squared_norm_of_x=-1) const;
+  // virtual real evaluate_x_i(const Vec& x, int i, real squared_norm_of_x=-1) const;
+  // virtual real evaluate_i_x_again(int i, const Vec& x, real squared_norm_of_x=-1, bool first_time = false) const;
+  // virtual real evaluate_x_i_again(const Vec& x, int i, real squared_norm_of_x=-1, bool first_time = false) const;
+  virtual void computeGramMatrix(Mat K) const;// K << source->toMat();
+  // virtual void setDataForKernelMatrix(VMat the_data);
+  // virtual void addDataForKernelMatrix(const Vec& newRow);
+  // virtual void setParameters(Vec paramvec);
+  // virtual Vec getParameters() const;
+  
+
+};
+
+// Declares a few other classes and functions related to this class.
+DECLARE_OBJECT_PTR(VMatKernel);
+  
+} // end of namespace PLearn
+
+#endif
+
