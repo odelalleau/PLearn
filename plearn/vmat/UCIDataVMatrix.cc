e8639a674eae2d909d2cc6ac3fc4891083e20c73
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-05-31 18:35:34 +0000

    VMat that loads a UCI dataset

---
+++ plearn/vmat/UCIDataVMatrix.cc
@@ -0,0 +1,116 @@
+// -*- C++ -*-
+
+// UCIDataVMatrix.cc
+//
+// Copyright (C) 2005 Olivier Delalleau 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: UCIDataVMatrix.cc,v 1.1 2005/05/31 18:35:34 tihocan Exp $ 
+   ******************************************************* */
+
+// Authors: Olivier Delalleau
+
+/*! \file UCIDataVMatrix.cc */
+
+
+#include "UCIDataVMatrix.h"
+#include <plearn/db/databases.h>
+
+namespace PLearn {
+using namespace std;
+
+
+UCIDataVMatrix::UCIDataVMatrix()
+{
+}
+
+PLEARN_IMPLEMENT_OBJECT(UCIDataVMatrix,
+    "Loads a UCI machine learning dataset.",
+    ""
+);
+
+void UCIDataVMatrix::declareOptions(OptionList& ol)
+{
+  declareOption(ol, "uci_spec", &UCIDataVMatrix::uci_spec, OptionBase::buildoption,
+      "Specifications of a UCI dataset.");
+
+  // Now call the parent class' declareOptions
+  inherited::declareOptions(ol);
+
+  // Hide unused options.
+
+  redeclareOption(ol, "vm",          &UCIDataVMatrix::vm,          OptionBase::nosave, "Not used.");
+  redeclareOption(ol, "writable",    &UCIDataVMatrix::writable,    OptionBase::nosave, "Not used.");
+  redeclareOption(ol, "length",      &UCIDataVMatrix::length_,      OptionBase::nosave, "Not used.");
+  redeclareOption(ol, "width",       &UCIDataVMatrix::width_,       OptionBase::nosave, "Not used.");
+  redeclareOption(ol, "inputsize",   &UCIDataVMatrix::inputsize_,   OptionBase::nosave, "Not used.");
+  redeclareOption(ol, "targetsize",  &UCIDataVMatrix::targetsize_,  OptionBase::nosave, "Not used.");
+  redeclareOption(ol, "weightsize",  &UCIDataVMatrix::weightsize_,  OptionBase::nosave, "Not used.");
+  redeclareOption(ol, "metadatadir", &UCIDataVMatrix::metadatadir, OptionBase::nosave, "Not used.");
+}
+
+void UCIDataVMatrix::build_()
+{
+  // ### This method should do the real building of the object,
+  // ### according to set 'options', in *any* situation. 
+  // ### Typical situations include:
+  // ###  - Initial building of an object from a few user-specified options
+  // ###  - Building of a "reloaded" object: i.e. from the complete set of all serialised options.
+  // ###  - Updating or "re-building" of an object after a few "tuning" options have been modified.
+  // ### You should assume that the parent class' build_() has already been called.
+  if (uci_spec) {
+    loadUCISet(vm, uci_spec);
+  }
+  inherited::build();
+}
+
+void UCIDataVMatrix::build()
+{
+  inherited::build();
+  build_();
+}
+
+void UCIDataVMatrix::makeDeepCopyFromShallowCopy(CopiesMap& copies)
+{
+  inherited::makeDeepCopyFromShallowCopy(copies);
+
+  // ### Call deepCopyField on all "pointer-like" fields 
+  // ### that you wish to be deepCopied rather than 
+  // ### shallow-copied.
+  // ### ex:
+  // deepCopyField(trainvec, copies);
+
+  // ### Remove this line when you have fully implemented this method.
+  PLERROR("UCIDataVMatrix::makeDeepCopyFromShallowCopy not fully (correctly) implemented yet!");
+}
+
+} // end of namespace PLearn
+
---
+++ plearn/vmat/UCIDataVMatrix.h
@@ -0,0 +1,112 @@
+// -*- C++ -*-
+
+// UCIDataVMatrix.h
+//
+// Copyright (C) 2005 Olivier Delalleau 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: UCIDataVMatrix.h,v 1.1 2005/05/31 18:35:34 tihocan Exp $ 
+   ******************************************************* */
+
+// Authors: Olivier Delalleau
+
+/*! \file UCIDataVMatrix.h */
+
+
+#ifndef UCIDataVMatrix_INC
+#define UCIDataVMatrix_INC
+
+#include <plearn/vmat/ForwardVMatrix.h>
+#include <plearn/db/UCISpecification.h>
+
+namespace PLearn {
+
+class UCIDataVMatrix: public ForwardVMatrix
+{
+
+private:
+
+  typedef ForwardVMatrix inherited;
+
+protected:
+
+  // *********************
+  // * protected options *
+  // *********************
+
+public:
+
+  // ************************
+  // * public build options *
+  // ************************
+
+  PP<UCISpecification> uci_spec;
+
+  // ****************
+  // * Constructors *
+  // ****************
+
+  //! Default constructor.
+  UCIDataVMatrix();
+
+  // ******************
+  // * Object methods *
+  // ******************
+
+private: 
+
+  //! This does the actual building. 
+  // (Please implement in .cc)
+  void build_();
+
+protected: 
+
+  //! Declares this class' options
+  // (Please implement in .cc)
+  static void declareOptions(OptionList& ol);
+
+public:
+
+  // Simply call inherited::build() then build_().
+  virtual void build();
+
+  //! Transform a shallow copy into a deep copy.
+  virtual void makeDeepCopyFromShallowCopy(CopiesMap& copies);
+
+  //! Declare name and deepCopy methods.
+  PLEARN_DECLARE_OBJECT(UCIDataVMatrix);
+
+};
+
+DECLARE_OBJECT_PTR(UCIDataVMatrix);
+
+} // end of namespace PLearn
+#endif
