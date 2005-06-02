b8427c39ebd1525fdcd0784e95c8fac1f937a51b
plearner <plearner@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-06-02 21:40:44 +0000

    *** empty log message ***

--- python_modules/plearn/vmat/PMat.py
+++ python_modules/plearn/vmat/PMat.py
@@ -163,7 +163,7 @@ class PMat:
                 if len(row)>0:
                     self.fieldnames.append(row[0])
         else:
-            fieldnames = map(str,range(self.width))
+            self.fieldnames = [ str(i) for i in range(self.width) ]
 
     def save_fieldnames(self):
         metadatadir = self.fname+'.metadata'
