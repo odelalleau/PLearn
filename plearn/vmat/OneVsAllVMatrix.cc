64e02f3ec4665d840aef61bf08d621dcf97e3454
larocheh <larocheh@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-06-24 02:01:17 +0000

    VMatrix that takes a multiclass problem and makes it a binary (0/1) problem, by selecting one of the class.

---
+++ plearn/vmat/OneVsAllVMatrix.cc
@@ -0,0 +1,129 @@
+// -*- C++ -*-
+
+// OneVsAllVMatrix.cc
+//
+// Copyright (C) 2005 Hugo Larochelle 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: .pyskeleton_header,v 1.1 2003/09/01 00:05:31 plearner Exp $ 
+   ******************************************************* */
+
+// Authors: Hugo Larochelle
+
+/*! \file OneVsAllVMatrix.cc */
+
+
+#include "OneVsAllVMatrix.h"
+
+namespace PLearn {
+using namespace std;
+
+//////////////////
+// OneVsAllVMatrix //
+//////////////////
+OneVsAllVMatrix::OneVsAllVMatrix()
+  : inherited(),
+  target_class(0)
+{
+
+}
+
+OneVsAllVMatrix::OneVsAllVMatrix(VMat the_source,int the_target_class)
+  : inherited(the_source),
+  target_class(the_target_class)
+{
+  build();
+}
+
+
+PLEARN_IMPLEMENT_OBJECT(OneVsAllVMatrix,
+    "Changes the target so that it is 1 if the target is equal to target_class, 0 if not",
+    ""
+);
+
+////////////////////
+// declareOptions //
+////////////////////
+void OneVsAllVMatrix::declareOptions(OptionList& ol)
+{
+  // Now call the parent class' declareOptions
+  inherited::declareOptions(ol);
+}
+
+///////////
+// build //
+///////////
+void OneVsAllVMatrix::build()
+{
+  inherited::build();
+  build_();
+}
+
+////////////
+// build_ //
+////////////
+void OneVsAllVMatrix::build_()
+{
+  if(source)
+  {
+    inputsize_ = source->inputsize();
+    targetsize_ = source->targetsize();
+    weightsize_ = source->weightsize();
+    //fieldinfos = source->fieldinfos;
+    length_ = source.length();
+    width_ = inputsize_+targetsize_+weightsize_;
+    sourcerow.resize(source->width());
+    if(targetsize_ != 1)
+      PLERROR("OneVsAllVMatrix::build_(): targetsize_ should be 1");
+  }
+}
+
+///////////////
+// getNewRow //
+///////////////
+void OneVsAllVMatrix::getNewRow(int i, const Vec& v) const
+{
+  source->getRow(i,v);
+  v[inputsize_] = int(v[inputsize_]) == target_class;
+}
+
+/////////////////////////////////
+// makeDeepCopyFromShallowCopy //
+/////////////////////////////////
+void OneVsAllVMatrix::makeDeepCopyFromShallowCopy(CopiesMap& copies)
+{
+  inherited::makeDeepCopyFromShallowCopy(copies);
+
+  //PLERROR("OneVsAllVMatrix::makeDeepCopyFromShallowCopy not fully (correctly) implemented yet!");
+}
+
+} // end of namespace PLearn
+
