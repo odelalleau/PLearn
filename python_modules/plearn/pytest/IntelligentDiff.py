c205d8b87257d0aae84aeb3caf44a0e154aabec2
plearner <plearner@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-07-08 18:08:42 +0000

    IntelligentDiff now lets you know the name of the files that differ... Wow! :-)

--- python_modules/plearn/pytest/IntelligentDiff.py
+++ python_modules/plearn/pytest/IntelligentDiff.py
@@ -188,6 +188,7 @@ class IntelligentDiff:
         ## self.preprocess(other, bench_dir, other_dir)
         some_diff = toolkit.command_output(diff_template % (bench, other))
         if some_diff:
+            self.differences.append("%s and %s differ" % (bench,other) )
             self.differences.extend( some_diff )
             self.differences.append( "" )
 
