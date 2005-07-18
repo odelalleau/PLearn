1016f8442d898f9f324a99a79a3ab3a24a638e24
plearner <plearner@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-07-18 21:08:07 +0000

    The PL_LOG system now writes to perr in raw_ascii mode (it used to log to cerr in plearn_ascii mode). This may change the output format somewhat.

--- plearn/io/pl_log.cc
+++ plearn/io/pl_log.cc
@@ -33,7 +33,7 @@
 // library, go to the PLearn Web site at www.plearn.org
 
 /* *******************************************************      
-   * $Id: pl_log.cc,v 1.6 2005/05/24 19:03:27 chrish42 Exp $ 
+   * $Id$ 
    ******************************************************* */
 
 // Authors: Nicolas Chapados, Christian Dorion
@@ -50,7 +50,7 @@ using namespace std;
 
 PL_Log::PL_Log( )
   : runtime_verbosity(VLEVEL_NORMAL),
-    output_stream(&cerr, false),
+    output_stream(get_perr()),
     null_stream(get_pnull()),
     logger_count(0)
 {}
