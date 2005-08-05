a5fa487e403951bcd6da7f0ed447d7e2951f2dbc
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-08-05 16:20:55 +0000

    Command to perform diff between two PLearn objects

---
+++ commands/PLearnCommands/DiffCommand.cc
@@ -0,0 +1,187 @@
+// -*- C++ -*-
+
+// DiffCommand.cc
+//
+// Copyright (C) 2005 Olivier Delalleau 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: .pyskeleton_header 544 2003-09-01 00:05:31Z plearner $ 
+   ******************************************************* */
+
+// Authors: Olivier Delalleau
+
+/*! \file DiffCommand.cc */
+
+
+#include "DiffCommand.h"
+#include <plearn/base/Object.h>
+#include <plearn/base/PLearnDiff.h>
+#include <plearn/io/fileutils.h>
+#include <plearn/io/openFile.h>
+#include <plearn/io/openString.h>
+#include <plearn/math/TVec_decl.h>
+//#include <plearn/math/TMat_impl.h>
+
+namespace PLearn {
+using namespace std;
+
+//! This allows to register the 'DiffCommand' command in the command registry
+PLearnCommandRegistry DiffCommand::reg_(new DiffCommand);
+
+DiffCommand::DiffCommand():
+  PLearnCommand("diff",
+                "Compare PLearn objects",
+                "The files with the objects' specifications are given in argument,\n"
+                "the first one being the reference object"
+  ) 
+{}
+
+//! The actual implementation of the 'DiffCommand' command 
+void DiffCommand::run(const vector<string>& args)
+{
+  if (args.size() < 2)
+    PLERROR("In DiffCommand::run - You need to provide at least two file names");
+  // Parse arguments.
+  TVec<PPath> obj_spec;
+  for (vector<string>::size_type i = 0; i < args.size(); i++)
+    obj_spec.append(args[i]);
+  // Load objects.
+  TVec< PP<Object> > obj;
+  int n = int(args.size());
+  for (int i = 0; i < n; i++) {
+    PP<Object> new_object;
+    string object_spec = readFileAndMacroProcess(obj_spec[i]);
+    PStream in = openString(object_spec, PStream::plearn_ascii);
+    in >> new_object;
+    if (!new_object)
+      PLERROR("In DiffCommand::run - Unable to serialize file %s as an Object",
+              obj_spec[i].absolute().c_str());
+    obj.append(new_object);
+  }
+  // Compare objects.
+  PStream& out = pout;
+  PP<Object> refer = obj[0];
+  PP<PLearnDiff> diffs = new PLearnDiff();
+  for (int i = 1; i < n; i++) {
+    PP<Object> other = obj[i];
+    diffs->forget();
+    int n_diffs = diff(refer, other, diffs);
+    if (n_diffs > 0) {
+      out << "Reference (" << obj_spec[0] << ") and object " << i << " ( "
+          << obj_spec[i] << ") differ:" << endl;
+      diffs->printDiffs(out);
+    }
+    /*
+    if (!diffs.empty()) {
+      for (vector<string>::size_type j = 0; j < diffs.size(); j += 3)
+        out << "  " << diffs[j] << " = " << diffs[j+1] << " != " << diffs[j+2] << endl;
+    }
+    */
+  }
+}
+
+/*
+/////////////
+// newDiff //
+/////////////
+void DiffCommand::newDiff(vector<string>& diffs, const string& name,
+                          const string& val1,  const string& val2)
+{
+  static TVec<string> new_diff;
+  new_diff.resize(3);
+  new_diff[0] = name;
+  new_diff[1] = val1;
+  new_diff[2] = val2;
+  diffs.appendRow(new_diff);
+}
+*/
+
+/*
+//////////
+// diff //
+//////////
+int DiffCommand::diff(PP<Object> refer, PP<Object> other,
+    vector<string>& diffs)
+{
+  int n_diffs = 0;
+  OptionBase::newDiff("classname", refer->classname(),
+                      other->classname(), diffs, n_diffs);
+  if (n_diffs > 0)
+    return n_diffs; // We cannot compare two objects from different classes.
+  OptionList& options = refer->getOptionList();
+  for (OptionList::const_iterator it = options.begin(); it != options.end(); it++) {
+    // pout << "Comparing " << (*it)->optionname() << endl;
+    n_diffs += (*it)->isDiff(refer->getOption((*it)->optionname()),
+                             other->getOption((*it)->optionname()),
+                             diffs);
+  }
+  return n_diffs;
+}
+*/
+
+/*
+bool DiffCommand::diff(PP<Object> refer, PP<Object> other, PP<OptionBase> opt,
+                       vector<string>& diffs)
+{
+  string optionname = opt->optionname();
+  string optiontype = opt->optiontype();
+  string refer_opt = refer->getOption(optionname);
+  string other_opt = other->getOption(optionname);
+  // pout << "Comparing " << opt->optionname() << ":" << endl;
+  // pout << "Refer = " << refer_opt << endl;
+  // pout << "Other = " << other_opt << endl;
+  return diff(refer_opt, other_opt, optionname, optiontype, diffs);
+  // pout << "Total: " << diffs.length() << " differences" << endl;
+}
+
+bool DiffCommand::diff(const string& refer, const string& other, const string& name,
+                       const string& type, vector<string>& diffs)
+{
+  bool is_diff;
+  if (type == "double") {
+    double val_refer = todouble(refer);
+    double val_other = todouble(other);
+    is_diff = !is_equal(val_refer, val_other);
+  } else if (type == "float") {
+    float val_refer = tofloat(refer);
+    float val_other = tofloat(other);
+    is_diff = !is_equal(val_refer, val_other);
+  } else
+    // Default: just compare the strings.
+    is_diff = (refer != other);
+  if (is_diff)
+    newDiff(diffs, name, refer, other);
+  return is_diff;
+}
+*/
+
+} // end of namespace PLearn
+
---
+++ commands/PLearnCommands/DiffCommand.h
@@ -0,0 +1,92 @@
+// -*- C++ -*-
+
+// DiffCommand.h
+//
+// Copyright (C) 2005 Olivier Delalleau 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: .pyskeleton_header 544 2003-09-01 00:05:31Z plearner $ 
+   ******************************************************* */
+
+// Authors: Olivier Delalleau
+
+/*! \file DiffCommand.h */
+
+
+#ifndef DiffCommand_INC
+#define DiffCommand_INC
+
+#include <commands/PLearnCommands/PLearnCommand.h>
+#include <commands/PLearnCommands/PLearnCommandRegistry.h>
+#include <plearn/io/PStream.h>
+#include <plearn/base/PP.h>
+
+namespace PLearn {
+
+class Object;
+
+class DiffCommand: public PLearnCommand
+{
+
+public:
+
+  DiffCommand();                    
+  virtual void run(const std::vector<std::string>& args);
+
+protected:
+
+  static PLearnCommandRegistry reg_;
+
+public:
+
+//  static int diff(PP<Object> refer, PP<Object> other, vector<string>& diffs);
+      /*
+                   const string& refer_name,
+                   const string& other_name,
+                   PStream& out = pout, PStream& err = err);
+                   */
+
+  /*
+  static bool diff(PP<Object> refer, PP<Object> other, PP<OptionBase> opt,
+                   vector<string>& diffs);
+
+  static bool diff(const string& refer, const string& other, const string& name,
+                   const string& type, TMat<string>& diffs);
+
+  static void newDiff(TMat<string>& diffs,
+                      const string& name, const string& val1, const string& val2);
+                      */
+};
+
+  
+} // end of namespace PLearn
+
+#endif
