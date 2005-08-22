6884c088d92cdc95d4fc07c43c920f07d611ab0b
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-08-22 09:26:51 +0000

    Added notice to prevent myself from committing non-gcc 3.4.4-compliant code

--- plearn/base/Option.h
+++ plearn/base/Option.h
@@ -116,6 +116,7 @@ public:
 
   virtual int diff(const string& refer, const string& other, PLearnDiff* diffs) const
     {
+      // TODO COMMIT WITH THE PLERROR UNTIL IT IS FIXED.
       PLERROR("OLIVIER: gcc 3.4.4 n'aime pas la ligne ci-dessous...");
       // return PLearn::diff(refer, other, this, diffs);
       return -1;
