28a79665998844c72e086a946c34a09c669e00d6
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-09-02 23:47:01 +0000

    VMatrix that reorders the samples according to their missing features

---
+++ plearn/vmat/ReorderByMissingVMatrix.cc
@@ -0,0 +1,191 @@
+// -*- C++ -*-
+
+// ReorderByMissingVMatrix.cc
+//
+// Copyright (C) 2005 Olivier Delalleau 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: .pyskeleton_header 544 2003-09-01 00:05:31Z plearner $ 
+   ******************************************************* */
+
+// Authors: Olivier Delalleau
+
+/*! \file ReorderByMissingVMatrix.cc */
+
+
+#include "ReorderByMissingVMatrix.h"
+#include <vector>
+
+namespace PLearn {
+using namespace std;
+
+PLEARN_IMPLEMENT_OBJECT(
+    ReorderByMissingVMatrix,
+    "Re-order samples in a source VMat by their missing attributes",
+
+    "Each sample is associated with a binary string indicating which elements\n"
+    "are missing or not.\n"
+    "For instance, the string '1001' indicates that the second and third\n"
+    "features are missing, while the first and fourth ones are present.\n"
+    "Samples are then sorted according to the usual lexicographic order on\n"
+    "these strings.\n"
+);
+
+/////////////////////////////
+// ReorderByMissingVMatrix //
+/////////////////////////////
+ReorderByMissingVMatrix::ReorderByMissingVMatrix()
+{}
+
+////////////////////
+// declareOptions //
+////////////////////
+void ReorderByMissingVMatrix::declareOptions(OptionList& ol)
+{
+    // ### Declare all of this object's options here
+    // ### For the "flags" of each option, you should typically specify  
+    // ### one of OptionBase::buildoption, OptionBase::learntoption or 
+    // ### OptionBase::tuningoption. Another possible flag to be combined with
+    // ### is OptionBase::nosave
+
+    // ### ex:
+    // declareOption(ol, "myoption", &ReorderByMissingVMatrix::myoption, OptionBase::buildoption,
+    //               "Help text describing this option");
+    // ...
+
+    // Now call the parent class' declareOptions
+    inherited::declareOptions(ol);
+
+    // Hide unused options.
+
+    redeclareOption(ol, "indices", &ReorderByMissingVMatrix::indices,
+                    OptionBase::nosave, "Not used.");
+
+    redeclareOption(ol, "indices_vmat", &ReorderByMissingVMatrix::indices_vmat,
+                    OptionBase::nosave, "Not used.");
+
+}
+
+///////////
+// build //
+///////////
+void ReorderByMissingVMatrix::build()
+{
+    inherited::build();
+    build_();
+}
+
+//! Simple class representing one sample (given by its index) with a string of
+//! '0' and '1' where '0' represents a missing value and '1' a non-missing one.
+struct index_and_missing_flags {
+    int index;
+    string missing_flags;
+};
+
+//! Comparison function used in sorting.
+struct {
+    bool operator() (const index_and_missing_flags& x,
+                     const index_and_missing_flags& y)
+    {
+        return (x.missing_flags < y.missing_flags);
+    }
+} compare_index_and_missing_flags;
+
+
+////////////
+// build_ //
+////////////
+void ReorderByMissingVMatrix::build_()
+{
+    if (source) {
+        // Construct a vector containing each sample index associated with its
+        // missing flags.
+        vector<index_and_missing_flags> vec;
+        int n = source.length();
+        int w = source.width();
+        sourcerow.resize(w);
+        for (int i = 0; i < n; i++) {
+            source->getRow(i, sourcerow);
+            string missing_flags;
+            for (int j = 0; j < w; j++)
+                if (is_missing(sourcerow[j]))
+                    missing_flags += '0';
+                else
+                    missing_flags += '1';
+            index_and_missing_flags ex;
+            ex.index = i;
+            ex.missing_flags = missing_flags;
+            vec.push_back(ex);
+        }
+        // Sort this vector.
+        sort(vec.begin(), vec.end(), compare_index_and_missing_flags);
+        // Build the 'indices' vector.
+        indices.resize(n);
+        indices.resize(0);
+        vector<index_and_missing_flags>::const_iterator it = vec.begin();
+        for (; it != vec.end(); it++)
+            indices.append(it->index);
+        // Re-build the parent class according to the new 'indices' vector.
+        inherited::build();
+    }
+}
+
+/////////////////////////////////
+// makeDeepCopyFromShallowCopy //
+/////////////////////////////////
+void ReorderByMissingVMatrix::makeDeepCopyFromShallowCopy(CopiesMap& copies)
+{
+    inherited::makeDeepCopyFromShallowCopy(copies);
+
+    // ### Call deepCopyField on all "pointer-like" fields 
+    // ### that you wish to be deepCopied rather than 
+    // ### shallow-copied.
+    // ### ex:
+    // deepCopyField(trainvec, copies);
+
+    // ### Remove this line when you have fully implemented this method.
+    PLERROR("ReorderByMissingVMatrix::makeDeepCopyFromShallowCopy not fully (correctly) implemented yet!");
+}
+
+} // end of namespace PLearn
+
+
+/*
+  Local Variables:
+  mode:c++
+  c-basic-offset:4
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:79
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=79 :
---
+++ plearn/vmat/ReorderByMissingVMatrix.h
@@ -0,0 +1,118 @@
+// -*- C++ -*-
+
+// ReorderByMissingVMatrix.h
+//
+// Copyright (C) 2005 Olivier Delalleau 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: .pyskeleton_header 544 2003-09-01 00:05:31Z plearner $ 
+   ******************************************************* */
+
+// Authors: Olivier Delalleau
+
+/*! \file ReorderByMissingVMatrix.h */
+
+
+#ifndef ReorderByMissingVMatrix_INC
+#define ReorderByMissingVMatrix_INC
+
+#include <plearn/vmat/SelectRowsVMatrix.h>
+
+namespace PLearn {
+
+/**
+ * Re-order samples in a source VMat by their missing attributes.
+ */
+class ReorderByMissingVMatrix : public SelectRowsVMatrix
+{
+    typedef SelectRowsVMatrix inherited;
+
+public:
+    //#####  Public Build Options  ############################################
+
+public:
+    //#####  Public Member Functions  #########################################
+
+    //! Default constructor
+    ReorderByMissingVMatrix();
+
+    
+    //#####  PLearn::Object Protocol  #########################################
+
+    // Declares other standard object methods.
+    PLEARN_DECLARE_OBJECT(ReorderByMissingVMatrix);
+
+    // Simply calls inherited::build() then build_() 
+    virtual void build();
+
+    //! Transforms a shallow copy into a deep copy
+    virtual void makeDeepCopyFromShallowCopy(CopiesMap& copies);
+
+protected:
+    //#####  Protected Options  ###############################################
+
+protected:
+    //#####  Protected Member Functions  ######################################
+    
+    //! Declares the class options.
+    static void declareOptions(OptionList& ol);
+
+private: 
+    //#####  Private Member Functions  ########################################
+
+    //! This does the actual building. 
+    void build_();
+
+private:
+    //#####  Private Data Members  ############################################
+
+    // The rest of the private stuff goes here
+};
+
+// Declares a few other classes and functions related to this class
+DECLARE_OBJECT_PTR(ReorderByMissingVMatrix);
+  
+} // end of namespace PLearn
+
+#endif
+
+
+/*
+  Local Variables:
+  mode:c++
+  c-basic-offset:4
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:79
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=79 :
