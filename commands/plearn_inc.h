1c4e09f12d4ab0916466681625107532a66bd427
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-10-05 13:14:05 +0000

    Removed include of a file that has been deleted in the svn repository

--- commands/plearn_inc.h
+++ commands/plearn_inc.h
@@ -250,7 +250,6 @@
 #include <plearn/vmat/RegularGridVMatrix.h>
 #include <plearn/vmat/RemoveDuplicateVMatrix.h>
 #include <plearn/vmat/ReorderByMissingVMatrix.h>
-#include <plearn/vmat/SelectAttributsSequenceVMatrix.h>
 #include <plearn/vmat/SelectRowsMultiInstanceVMatrix.h>
 #include <plearn/vmat/ShuffleColumnsVMatrix.h>
 #include <plearn/vmat/SortRowsVMatrix.h>
