67348a422db7921b61109bd2d4baf2738f0b2049
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-10-20 00:26:01 +0000

    Just like DiskVMatrix, but the data directory is automatically erased when it is not used anymore

---
+++ plearn/vmat/TemporaryDiskVMatrix.cc
@@ -0,0 +1,126 @@
+// -*- C++ -*-
+
+// TemporaryDiskVMatrix.cc
+//
+// Copyright (C) 2005 Olivier Delalleau 
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+   * $Id: .pyskeleton_header 544 2003-09-01 00:05:31Z plearner $ 
+   ******************************************************* */
+
+// Authors: Olivier Delalleau
+
+/*! \file TemporaryDiskVMatrix.cc */
+
+
+#include "TemporaryDiskVMatrix.h"
+#include <plearn/io/fileutils.h>    //!< For 'force_rmdir'.
+
+namespace PLearn {
+using namespace std;
+
+PLEARN_IMPLEMENT_OBJECT(
+    TemporaryDiskVMatrix,
+    "DiskVMatrix to store data temporarily.",
+    "When a TemporaryDiskVMatrix object is deleted, it automatically removes\n"
+    "its data directory, as well as its metadata directory.\n"
+    );
+
+//////////////////////////
+// TemporaryDiskVMatrix //
+//////////////////////////
+TemporaryDiskVMatrix::TemporaryDiskVMatrix()
+{}
+
+TemporaryDiskVMatrix::TemporaryDiskVMatrix(const PPath& filename,
+                                           bool writable):
+    inherited(filename, writable)
+{
+    build_();
+}
+
+////////////////////
+// declareOptions //
+////////////////////
+void TemporaryDiskVMatrix::declareOptions(OptionList& ol)
+{
+    // Now call the parent class' declareOptions
+    inherited::declareOptions(ol);
+}
+
+///////////
+// build //
+///////////
+void TemporaryDiskVMatrix::build()
+{
+    inherited::build();
+    build_();
+}
+
+////////////
+// build_ //
+////////////
+void TemporaryDiskVMatrix::build_()
+{
+}
+
+/////////////////////////////////
+// makeDeepCopyFromShallowCopy //
+/////////////////////////////////
+void TemporaryDiskVMatrix::makeDeepCopyFromShallowCopy(CopiesMap& copies)
+{
+    inherited::makeDeepCopyFromShallowCopy(copies);
+}
+
+///////////////////////////
+// ~TemporaryDiskVMatrix //
+///////////////////////////
+TemporaryDiskVMatrix::~TemporaryDiskVMatrix()
+{
+    force_rmdir(dirname);
+    if (hasMetaDataDir())
+        force_rmdir(getMetaDataDir());
+}
+
+} // end of namespace PLearn
+
+
+/*
+  Local Variables:
+  mode:c++
+  c-basic-offset:4
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:79
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=79 :
