92c450d595b7530e7fa4a31defdffcc05c93ab0c
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-11-07 17:30:04 +0000

    English version of Copyright name: accents are to be avoided because of UTF8 / ISOLatin conflicts

--- plearn_learners/distributions/GaussMix.h
+++ plearn_learners/distributions/GaussMix.h
@@ -2,7 +2,7 @@
 
 // GaussMix.h
 // 
-// Copyright (C) 2004-2005 Université de Montréal
+// Copyright (C) 2004-2005 University of Montreal
 // 
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
