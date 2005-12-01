767a90be9c56949263d6c58014213adcd96a7e41
chapados <chapados@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-12-01 16:47:46 +0000

    Added KernelRidgeRegressor to plearn_inc.h

--- commands/plearn_inc.h
+++ commands/plearn_inc.h
@@ -156,6 +156,7 @@
 
 // Regressors
 #include <plearn_learners/regressors/ConstantRegressor.h>
+#include <plearn_learners/regressors/KernelRidgeRegressor.h>
 #include <plearn_learners/regressors/KNNRegressor.h>
 #include <plearn_learners/regressors/LinearRegressor.h>
 #include <plearn_learners/regressors/PLS.h>
