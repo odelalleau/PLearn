d27b4193c401319544c16a33e555ea0bc28b8175
larocheh <larocheh@15d43a8f-0efa-0310-bc34-a8a306da697e>
2005-12-13 20:13:27 +0000

    This PLearner uses a PConditionalDistribution that models P(y|x) where y is a possible target, to classify data points in the space of x, using the simple classification rule argmax_y_c P(y_c|x)...

---
+++ plearn_learners/classifiers/ClassifierFromPConditionalDistribution.cc
@@ -0,0 +1,273 @@
+
+// -*- C++ -*-
+
+// ClassifierFromPConditionalDistribution.cc
+//
+// Copyright (C) 2003-2005  Pascal Vincent & Olivier Delalleau
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+ * $Id: ClassifierFromPConditionalDistribution.cc 4412 2005-11-02 19:00:20Z tihocan $ 
+ ******************************************************* */
+
+/*! \file ClassifierFromPConditionalDistribution.cc */
+#include "ClassifierFromPConditionalDistribution.h"
+#include <plearn/io/PPath.h>
+
+namespace PLearn {
+using namespace std;
+
+ClassifierFromPConditionalDistribution::ClassifierFromPConditionalDistribution() 
+    : nclasses(-1),
+      output_type("predicted_class")
+{}
+
+PLEARN_IMPLEMENT_OBJECT(
+    ClassifierFromPConditionalDistribution,
+    "Classifier that takes a PConditionalDistribution and classifies with it.", 
+    "ClassifierFromPConditionalDistribution classifies by finding the target\n"
+    "class y that maximizes p(y|x), where x is the input.\n");
+
+////////////////////
+// declareOptions //
+////////////////////
+void ClassifierFromPConditionalDistribution::declareOptions(OptionList& ol)
+{
+
+    // Build options.
+
+    declareOption(ol, "nclasses", &ClassifierFromPConditionalDistribution::nclasses, OptionBase::buildoption,
+                  "The number of classes");
+
+    declareOption(ol, "conditional_distribution", &ClassifierFromPConditionalDistribution::pcd, OptionBase::buildoption,
+                  "PConditionalDistribution that computes p(y|x) for all possible classes y.");
+
+    declareOption(ol, "output_type", &ClassifierFromPConditionalDistribution::output_type, OptionBase::buildoption,
+                  "Output type. Choose among: \n"
+                  "- \"predicted_class\"\n"
+                  "- \"class_probabilities\"\n"
+                  "- \"class_log-probabilities\")\n"
+                  "Note that this may change the value of output_defs of the conditional_distribution.");
+ 
+    // Now call the parent class' declareOptions
+    inherited::declareOptions(ol);
+}
+
+///////////
+// build //
+///////////
+void ClassifierFromPConditionalDistribution::build()
+{
+    inherited::build();
+    build_();
+}
+
+////////////
+// build_ //
+////////////
+void ClassifierFromPConditionalDistribution::build_()
+{
+    if(pcd && train_set)
+    {
+        PP<Dictionary> target_dict = train_set->getDictionary(inputsize());
+        if(!target_dict && nclasses <= 0) PLERROR("In ClassifierFromPConditionalDistribution::build_(): There is not way to know what are the possible targets (nclasses <= 0 and no Dictionary for target field)");
+        if(target_dict) nclasses = -1;
+        if(output_type == "predicted_class" || output_type == "class_log-probabilities") pcd->outputs_def = "l";
+        else if (output_type == "class_probabilities") pcd->outputs_def = "d";
+        else PLERROR("ClassifierFromPConditionalDistribution::build_(): output_type %s is not supported", output_type.c_str());
+
+        pcd_input.resize(train_set->inputsize() + train_set->targetsize());
+        pcd_output.resize(1);
+    }
+
+}
+
+/////////////////////////////////
+// makeDeepCopyFromShallowCopy //
+/////////////////////////////////
+void ClassifierFromPConditionalDistribution::makeDeepCopyFromShallowCopy(CopiesMap& copies)
+{
+    inherited::makeDeepCopyFromShallowCopy(copies);
+    deepCopyField(pcd, copies);
+    deepCopyField(target_values, copies);
+    deepCopyField(pcd_input, copies);
+}
+
+////////////////
+// outputsize //
+////////////////
+int ClassifierFromPConditionalDistribution::outputsize() const
+{
+    if(nclasses > 0) return nclasses;
+    else return train_set->getValues(0,inputsize()).length();
+}
+
+////////////
+// forget //
+////////////
+void ClassifierFromPConditionalDistribution::forget()
+{
+    stage=0;
+    pcd->forget();
+}
+
+///////////
+// train //
+///////////
+void ClassifierFromPConditionalDistribution::train()
+{
+    if(targetsize()!=1)
+        PLERROR("In ClassifierFromPConditionalDistribution::train - Expecting a targetsize of 1, not %d !!",targetsize());
+
+    if(nstages<stage) // asking to revert to a previous stage!
+        forget();  // reset the learner to stage=0
+
+    if(stage==0)
+    {
+        pcd->setExperimentDirectory(getExperimentDirectory() / "PCD");
+        train_set->defineSizes(inputsize()+1,targetsize()-1,weightsize());
+        pcd->setTrainingSet(train_set);
+        PP<VecStatsCollector> train_stats = new VecStatsCollector();
+        train_stats->setFieldNames(pcd->getTrainCostNames());
+        pcd->setTrainStatsCollector(train_stats);
+        pcd->nstages = nstages;
+        if (verbosity >= 2)
+        pout << ">>> Training PConditionalDistribution" << endl;    
+        pcd->train();
+        if (verbosity >= 2)
+        pout << ">>> Training is over" << endl;    
+        train_set->defineSizes(inputsize(),targetsize(),weightsize());
+    }
+    stage = nstages; // trained!
+}
+
+///////////////////
+// computeOutput //
+///////////////////
+void ClassifierFromPConditionalDistribution::computeOutput(const Vec& input, Vec& output) const
+{    
+    pcd_input.subVec(0,inputsize()) << input;
+    if(nclasses <= 0)
+    {   
+        target_values = train_set->getValues(input, inputsize());
+        output.resize(target_values.length());
+        for(int i=0; i<target_values.length(); i++)
+        {
+            pcd_input[inputsize()] = target_values[i];            
+            pcd->computeOutput(pcd_input, pcd_output);
+            output[i] = pcd_output[0];
+        }
+    }
+    else
+    {
+        output.resize(nclasses);
+        for(int c=0; c<nclasses; c++)
+        {
+            pcd_input[inputsize()] = c;
+            pcd->computeOutput(pcd_input, pcd_output);
+            output[c] = pcd_output[0];
+        }     
+    }
+
+    if(output_type == "predicted_class") 
+    {
+        if(nclasses <= 0)
+            output[0] = (int)target_values[argmax(output)];
+        else
+            output[0] = argmax(output);
+        output.resize(1);
+    }
+}
+
+/////////////////////////////
+// computeCostsFromOutputs //
+/////////////////////////////
+void ClassifierFromPConditionalDistribution::computeCostsFromOutputs(const Vec& input, const Vec& output, 
+                                                    const Vec& target, Vec& costs) const
+{
+    if(output_type == "predicted_class")
+    {
+        costs[0] = (int) (target[0] != output[0]);
+    }
+    else
+    {
+        int c;
+        if(nclasses <= 0)
+            c = train_set->getValues(input,inputsize()).find(target[0]);
+        else
+            c = (int)target[0];
+        costs[0] = (int) (argmax(output) != c);
+        if(output_type == "class_probabilities") costs[1] = safeflog(output[c]);
+        else costs[1] = output[c];
+    }
+}                                
+
+//////////////////////
+// getTestCostNames //
+//////////////////////
+TVec<string> ClassifierFromPConditionalDistribution::getTestCostNames() const
+{
+    TVec<string> cnames;    
+    if(output_type == "predicted_class")
+    {
+        cnames.resize(1);
+        cnames[0] = "class_error";
+    }
+    else
+    {
+        cnames.resize(2);
+        cnames[0] = "class_error";
+        cnames[1] = "NLL";
+    }
+    return cnames;
+}
+
+///////////////////////
+// getTrainCostNames //
+///////////////////////
+TVec<string> ClassifierFromPConditionalDistribution::getTrainCostNames() const
+{
+    return TVec<string>();
+}
+
+} // end of namespace PLearn
+
+
+/*
+  Local Variables:
+  mode:c++
+  c-basic-offset:4
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:79
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=79 :
---
+++ plearn_learners/classifiers/ClassifierFromPConditionalDistribution.h
@@ -0,0 +1,172 @@
+
+// -*- C++ -*-
+
+// ClassifierFromPConditionalDistribution.h
+//
+// Copyright (C) 20032-2005  Pascal Vincent & Olivier Delalleau
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+/* *******************************************************      
+ * $Id: ClassifierFromPConditionalDistribution.h 3994 2005-08-25 13:35:03Z chapados $ 
+ ******************************************************* */
+
+/*! \file ClassifierFromPConditionalDistribution.h */
+#ifndef ClassifierFromPConditionalDistribution_INC
+#define ClassifierFromPConditionalDistribution_INC
+
+#include <plearn_learners/distributions/PDistribution.h>
+#include <plearn_learners/generic/PLearner.h>
+
+namespace PLearn {
+
+//! Classifier that takes a PConditionalDistribution and classifies with it,
+//! by finding the target class y that maximizes p(y|x), where x is
+//! the input.
+class ClassifierFromPConditionalDistribution: public PLearner
+{
+
+private:
+
+    typedef PLearner inherited;
+
+protected:
+
+    mutable Vec target_values;
+    mutable Vec pcd_input;
+    mutable Vec pcd_output;
+
+public:
+
+    // ************************
+    // * public build options *
+    // ************************
+
+    //! Number of classes (doesn't need to be specified if train_set has
+    //! a Dictionary for the target column.
+    int nclasses;
+    //! PConditionalDistribution, that will be trained if this learner is trained
+    PP<PDistribution> pcd;
+    //! Output type (choose among: "predicted_class", "class_probabilities" or "class_log-probabilities")
+    string output_type;
+
+    // ****************
+    // * Constructors *
+    // ****************
+
+    //! Default constructor.
+    ClassifierFromPConditionalDistribution();
+
+    // ******************
+    // * PLearner methods *
+    // ******************
+
+private: 
+
+    //! This does the actual building. 
+    void build_();
+
+protected: 
+
+    //! Declares this class' options
+    static void declareOptions(OptionList& ol);
+
+public:
+
+    // ************************
+    // **** Object methods ****
+    // ************************
+
+    //! Simply calls inherited::build() then build_() 
+    virtual void build();
+
+    //! Transforms a shallow copy into a deep copy
+    virtual void makeDeepCopyFromShallowCopy(CopiesMap& copies);
+
+    // Declares other standard object methods
+    //  If your class is not instantiatable (it has pure virtual methods)
+    // you should replace this by PLEARN_DECLARE_ABSTRACT_OBJECT_METHODS 
+    PLEARN_DECLARE_OBJECT(ClassifierFromPConditionalDistribution);
+
+
+    // **************************
+    // **** PLearner methods ****
+    // **************************
+
+    //! returns the size of this learner's output, (which typically
+    //! may depend on its inputsize(), targetsize() and set options)
+    virtual int outputsize() const;
+
+    //! (Re-)initializes the PLearner in its fresh state (that state may depend on the 'seed' option)
+    //! And sets 'stage' back to 0   (this is the stage of a fresh learner!)
+    virtual void forget();
+
+    
+    //! The role of the train method is to bring the learner up to stage==nstages,
+    //! updating the train_stats collector with training costs measured on-line in the process.
+    virtual void train();
+
+
+    //! Computes the output from the input
+    virtual void computeOutput(const Vec& input, Vec& output) const;
+
+    //! Computes the costs from already computed output. 
+    virtual void computeCostsFromOutputs(const Vec& input, const Vec& output, 
+                                         const Vec& target, Vec& costs) const;
+                                
+
+    //! Returns the names of the costs computed by computeCostsFromOutpus (and thus the test method)
+    virtual TVec<string> getTestCostNames() const;
+
+    //! Returns the names of the objective costs that the train method computes and 
+    //! for which it updates the VecStatsCollector train_stats
+    virtual TVec<string> getTrainCostNames() const;
+
+
+};
+
+// Declares a few other classes and functions related to this class
+DECLARE_OBJECT_PTR(ClassifierFromPConditionalDistribution);
+  
+} // end of namespace PLearn
+
+#endif
+
+
+/*
+  Local Variables:
+  mode:c++
+  c-basic-offset:4
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:79
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=79 :
