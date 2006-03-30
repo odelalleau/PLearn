3bfa5d98497089dd2eee2da6dc20947e3c6b2dfa
dorionc <dorionc@15d43a8f-0efa-0310-bc34-a8a306da697e>
2006-03-30 12:54:40 +0000

    random.py --- Functions generating AR, MA and ARMA processes (requires numarray)

--- python_modules/plearn/math/__init__.py
+++ python_modules/plearn/math/__init__.py
@@ -1,9 +1,9 @@
 from math import *
-from numarray import asarray, zeros, shape, ufunc
+from numarray import asarray, choose, zeros, shape, ufunc
 
 def sign(m):
-   m = asarray(m)
-   return zeros(shape(m))-ufunc.less(m,0)+ufunc.greater(m,0)
+    m = asarray(m)
+    return zeros(shape(m))-ufunc.less(m,0)+ufunc.greater(m,0)
 
 def fast_softmax( x ):
     s = 0
@@ -23,9 +23,17 @@ def floats_are_equal(a, b, numtol=1e-6):
     return abs(a-b) <= numtol*minabs
 
 def isNaN(f):
-    """Return True is f is or contains NaN values"""
+    """Return True where f contains NaN values"""
     from numarray import ieeespecial
     return ieeespecial.mask(f, ieeespecial.NAN)
+
+def isNotNaN(f):
+    """Return False where f contains NaN values"""
+    return ufunc.equal(isNaN(f), 0.0)
+
+def replace_nans(a, with=0.0):
+    return choose(isNotNaN(a), (with, a))
+
 
 if __name__ == '__main__':
     print fast_softmax([ 0, 0, 100 ])
@@ -34,3 +42,8 @@ if __name__ == '__main__':
     print isNaN(2.0)
     print isNaN([1.0, float('NaN'), 3.0])
     print isNaN([1.0, 2.0, 3.0])
+    print
+    
+    a = [1.0, float('NaN'), 3.0, float('NaN')]
+    print a
+    print replace_nans(a)
---
+++ python_modules/plearn/math/random.py
@@ -0,0 +1,67 @@
+from numarray import *
+
+def arma(P, RHO, ma_weights, T=None, epsilon=None):
+    """Returns an ARMA(P, Q) process. (Q <- len(ma_weights))
+
+    Returns $X$ such that, for $t \in {0, \ldots, T-1}$, 
+
+    \[
+        X_t = \sum_{k=1}^P \rho_k X_{t-k} +
+                \sum_{k=1}^Q \alpha_k \epsilon_{t-k} + \epsilon_t 
+    \]
+
+    where $\rho_k = \rho^k$ (for now) and $\alpha_k = ma_weights[k-1]$. Of
+    course, the summation is truncated whenever $t < P$.
+    """
+    if epsilon is None:
+        epsilon = random_array.normal(0, 1, (T,))
+    elif T is None:
+        raise ValueError("You must provide T or epsilon.")
+
+    if T is None:        
+        T = shape(epsilon)[0]
+    else:
+        assert T == shape(epsilon)[0], "T and shape(epsilon) are incoherent."
+
+    u = array(type=Float32, shape=(T,))
+    for t in range(T):
+        u[t] = epsilon[t]
+
+        for k in range(1, min(t, len(ma_weights))+1):
+            alpha_k = ma_weights[k-1] # offset by 1 since lists start at 0 in Python 
+            u[t] += alpha_k * epsilon[t-k]
+
+        for p in range(1, min(t, P)+1):
+            u[t] += RHO**p * u[t-p]
+
+    return u
+
+def autoregressive(P, RHO, T=None, epsilon=None):
+    """Returns an AR(P) process.
+
+    Returns $X$ such that, for $t \in {0, \ldots, T-1}$, 
+
+    \[
+        X_t = \sum_{k=1}^P \rho_k X_{t-k} + \epsilon_t 
+    \]
+
+    where $\rho_k = \rho^k$ (for now). Of course, the summation is
+    truncated whenever $t < P$.
+    """
+    return arma(P, RHO, T=T, epsilon=epsilon,
+                ma_weights=[] ) # Disabling MA                
+            
+def moving_average(ma_weights, T=None, epsilon=None):
+    """Returns a MA(Q) process (Q <- len(ma_weights)).
+
+    Returns $X$ such that, for $t \in {0, \ldots, T-1}$, 
+
+    \[
+        X_t = \epsilon_t + \sum_{k=1}^Q \alpha_k \epsilon_{t-k}
+    \]
+
+    where $Q = len(ma_length)$ and $\alpha_k = ma_weights[k-1]$. Of course,
+    the summation is truncated whenever $t < Q$.
+    """
+    return arma(-1, float('NaN'), # Disabling AR 
+                ma_weights, T, epsilon=epsilon)
