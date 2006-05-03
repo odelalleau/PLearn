41a2906587ad24963dd796874cbc3bce5f50bd2c
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2006-05-03 15:56:43 +0000

    Fixed debug message to correspond to the reality

--- plearn/db/getDataSet.cc
+++ plearn/db/getDataSet.cc
@@ -154,7 +154,7 @@ VMat getDataSet(const PPath& dataset_path)
     if (vm->inputsize() < 0 && vm->targetsize() < 0 && vm->weightsize() < 0) 
     {
         DBG_LOG << "In getDataSet - The loaded VMat has no inputsize, targetsize "
-                << "or weightsize specified, setting them to (" << vm->width() << ",0,0,0)"
+                << "or weightsize specified, setting them to (" << vm->width() << ",0,0)"
                 << endl;
         vm->defineSizes(vm->width(), 0, 0);
     }
