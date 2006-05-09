5e4a80857b4bc7dfa83cf8fed2a398f537adb74e
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2006-05-09 20:47:10 +0000

    Added fix for Windows platform: Python must be given the Windows path, not the Cygwin path

--- python_modules/plearn/math/test/StatsCollector_link.py
+++ python_modules/plearn/math/test/StatsCollector_link.py
@@ -1,10 +1,17 @@
 # A hack to use StatsCollector.py without needing a symbolic link.
 
-import os
+import os, sys
+from plearn.utilities import toolkit
 
 homedir = os.environ.get('HOME', '/')
 plearndir = os.environ.get('PLEARNDIR', os.path.join(homedir,'PLearn'))
 
-execfile(os.path.join(os.path.join(os.path.join(os.path.join(
-        plearndir, 'python_modules'), 'plearn'), 'math'), 'StatsCollector.py'))
+full_path = os.path.join(plearndir, 'python_modules', 'plearn', 'math', \
+                         'StatsCollector.py')
+
+if sys.platform == 'win32':
+    full_path = toolkit.command_output("cygpath -w %s" % full_path)
+    full_path = full_path[0].rstrip('\n')
+
+execfile(full_path)
 
