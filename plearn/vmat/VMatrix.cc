1d7c9634c7b618fe6b2dc6dc15820aabbf3b5ae5
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2006-08-01 17:23:37 +0000

    Reverting back to r6154: the changes I had made were redundant with another concurrent change, and may not be safe if a VMat has string mapping files not ending with .smap

--- plearn/vmat/VMatrix.cc
+++ plearn/vmat/VMatrix.cc
@@ -1131,21 +1131,7 @@ void VMatrix::loadAllStringMappings()
 {
     if (! hasMetaDataDir() || ! isdir(getSFIFDirectory()))
         return;
-
-    PPath finfo = getSFIFDirectory();
-    if (!force_mkdir(finfo))
-        return;
-
-    // If there are no .smap files, there is no need to waste time checking for
-    // their existence.
-    vector<PPath> entries = PLearn::lsdir_fullpath(finfo);
-    vector<PPath>::const_iterator it = entries.begin();
-    for (; it != entries.end(); it++)
-        if (it->extension(true) == ".smap")
-            break;
-    if (it == entries.end())
-        return;
-
+    
     for(int i=0;i<width();i++)
         loadStringMapping(i);
 }
