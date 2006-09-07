7addddb9e96278d6dcab288147b8e80141aae835
dorionc <dorionc@15d43a8f-0efa-0310-bc34-a8a306da697e>
2006-09-07 18:40:39 +0000

    Added hasNaN

--- python_modules/plearn/math/__init__.py
+++ python_modules/plearn/math/__init__.py
@@ -22,13 +22,19 @@ def floats_are_equal(a, b, numtol=1e-6):
         return abs(a-b) <= numtol
     return abs(a-b) <= numtol*minabs
 
+def hasNaN(f):
+    has = sum(isNaN(f))
+    while not isinstance(has, int):
+        has = sum(has)
+    return has
+    
 def isNaN(f):
-    """Return True where f contains NaN values"""
+    """Return 1 where f contains NaN values, 0 elsewhere."""
     from numarray import ieeespecial
     return ieeespecial.mask(f, ieeespecial.NAN)
 
 def isNotNaN(f):
-    """Return False where f contains NaN values"""
+    """Return 0 where f contains NaN values, 1 elsewhere."""
     return ufunc.equal(isNaN(f), 0.0)
 
 def replace_nans(a, with=0.0):
@@ -41,8 +47,20 @@ if __name__ == '__main__':
     print isNaN(float('NaN'))
     print isNaN(2.0)
     print isNaN([1.0, float('NaN'), 3.0])
+    print hasNaN([1.0, float('NaN'), 3.0])
     print isNaN([1.0, 2.0, 3.0])
+    print hasNaN([1.0, 2.0, 3.0])
     print
+
+    print "2d"
+    print hasNaN([[1.0, 2.0, 3.0],
+                  [1.0, 2.0, 3.0]])
+    print hasNaN([[1.0, 2.0,          3.0],
+                  [1.0, float('NaN'), 3.0]])
+    print hasNaN([[1.0, float('NaN'), 3.0],
+                  [1.0, 2.0,          3.0]])
+    print
+
     
     a = [1.0, float('NaN'), 3.0, float('NaN')]
     print a
