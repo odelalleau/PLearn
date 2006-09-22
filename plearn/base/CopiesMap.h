55b77f102143f373efdf96c34c1557be83d520cd
chapados <chapados@15d43a8f-0efa-0310-bc34-a8a306da697e>
2006-09-22 21:06:01 +0000

    deepCopyField now recursively defined for all STL containers

--- plearn/base/CopiesMap.h
+++ plearn/base/CopiesMap.h
@@ -1,8 +1,45 @@
+// -*- C++ -*-
+
+// PLearn (A C++ Machine Learning Library)
+// Copyright (C) 1999-2006 University of Montreal and individual contributors
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+// 
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
 
 #ifndef CopiesMap_INC
 #define CopiesMap_INC
 
+#include <deque>
 #include <map>
+#include <set>
+#include <list>
+#include <vector>
 #include <string>
 #include <time.h>	//!< For definition of 'clock_t'.
 #include <utility>      //!< For pair.
@@ -19,7 +56,13 @@
 
 namespace PLearn {
 using std::string;
+using std::deque;
 using std::map;
+using std::multimap;
+using std::set;
+using std::multiset;
+using std::list;
+using std::vector;
 using std::pair;
 
 class PPath;
@@ -42,15 +85,20 @@ typedef map<double, double> map_double_double;
 typedef map<string, int> map_string_int;
 typedef map<int, string> map_int_string;
 
-/*! Support for generic deep copying
-    
-Deep copying is defined for objects in the following manner:
-+ copy constructors should always do a shallow copy.
-+ a public method OBJTYPE* deepCopy(CopiesMap& copies) const 
-should be defined to allow deepCopying
-+ the deepCopy method should be virtual for classes that are designed to be subclassed
-Take a close look at the Object class in Object.h to see how this is done.
-*/
+/**
+ *  Support for generic deep copying
+ *      
+ *  Deep copying is defined for objects in the following manner:
+ *
+ *  - copy constructors should always do a shallow copy
+ *
+ *  - a public method OBJTYPE* deepCopy(CopiesMap& copies) const 
+ *    should be defined to allow deepCopying
+ *
+ *  - the deepCopy method should be virtual for classes that are designed to be
+ *    subclassed Take a close look at the Object class in Object.h to see how
+ *    this is done.
+ */
 
 //! Types that do not require deep copy.
 NODEEPCOPY(double)
@@ -74,14 +122,78 @@ NODEEPCOPY(VMField)
 NODEEPCOPY(VMFieldStat)
 NODEEPCOPY(FILE*)   //!< There is currently no proper way to deep copy these.
 
+
+//#####  Some Standard STL Containers  ########################################
+
 //! Pairs handle deepCopying by distributing it to each element
-    template <class T, class U>
+template <class T, class U>
 inline void deepCopyField(pair<T,U>& p, CopiesMap& copies)
 {
     deepCopyField(p.first, copies);
     deepCopyField(p.second, copies);
 }
-  
+
+//! Standard containers handle deepcopying by distributing it to each element
+template <class T, class Alloc>
+void deepCopyField(deque<T,Alloc>& c, CopiesMap& copies)
+{
+    for (typename deque<T,Alloc>::iterator it = c.begin(), end=c.end()
+             ; it != end ; ++it)
+        deepCopyField(*it, copies);
+}
+
+template <class T, class U, class Compare, class Alloc>
+void deepCopyField(map<T,U,Compare,Alloc>& c, CopiesMap& copies)
+{
+    for (typename map<T,U,Compare,Alloc>::iterator it = c.begin(), end=c.end()
+             ; it != end ; ++it)
+        deepCopyField(*it, copies);
+}
+
+template <class T, class U, class Compare, class Alloc>
+void deepCopyField(multimap<T,U,Compare,Alloc>& c, CopiesMap& copies)
+{
+    for (typename multimap<T,U,Compare,Alloc>::iterator it = c.begin(), end=c.end()
+             ; it != end ; ++it)
+        deepCopyField(*it, copies);
+}
+
+template <class T, class Compare, class Alloc>
+void deepCopyField(set<T,Compare,Alloc>& c, CopiesMap& copies)
+{
+    for (typename set<T,Compare,Alloc>::iterator it = c.begin(), end=c.end()
+             ; it != end ; ++it)
+        deepCopyField(*it, copies);
+}
+
+template <class T, class Compare, class Alloc>
+void deepCopyField(multiset<T,Compare,Alloc>& c, CopiesMap& copies)
+{
+    for (typename multiset<T,Compare,Alloc>::iterator it = c.begin(), end=c.end()
+             ; it != end ; ++it)
+        deepCopyField(*it, copies);
+}
+
+template <class T, class Alloc>
+void deepCopyField(list<T,Alloc>& c, CopiesMap& copies)
+{
+    for (typename list<T,Alloc>::iterator it = c.begin(), end=c.end()
+             ; it != end ; ++it)
+        deepCopyField(*it, copies);
+}
+
+template <class T, class Alloc>
+void deepCopyField(vector<T,Alloc>& c, CopiesMap& copies)
+{
+    for (typename vector<T,Alloc>::iterator it = c.begin(), end=c.end()
+             ; it != end ; ++it)
+        deepCopyField(*it, copies);
+}
+
+
+
+//#####  Fallbacks  ###########################################################
+
 //!  Any type not handled below: do nothing
 template <class T>
 inline void deepCopyField(T&, CopiesMap&)
