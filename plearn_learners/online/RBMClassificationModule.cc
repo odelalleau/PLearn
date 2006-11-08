d91d74a9bae25f2f668a2154ff6710c42653e369
lamblin <lamblin@15d43a8f-0efa-0310-bc34-a8a306da697e>
2006-11-08 06:03:38 +0000

    Fix another sign bug, which was hidden by the one in RBMMultinomialLayer (or vice-versa)

--- plearn_learners/online/RBMClassificationModule.cc
+++ plearn_learners/online/RBMClassificationModule.cc
@@ -167,7 +167,7 @@ void RBMClassificationModule::fprop(const Vec& input, Vec& output) const
     last_layer->getAllActivations( previous_to_last );
 
     // target_layer->activation =
-    //      bias - sum_j softplus(-(W_ji + C_j + last_layer->activation[j]))
+    //      bias - sum_j softplus(-(W_ji + last_layer->activation[j]))
     Vec target_act = target_layer->activation;
     for( int i=0 ; i<output_size ; i++ )
     {
@@ -233,8 +233,9 @@ void RBMClassificationModule::bpropUpdate(const Vec& input, const Vec& output,
         for( int k=0 ; k<output_size ; k++ )
         {
             // dC/d( w_ik + target_act_i )
-            real d_z = d_target_act[k]*(-sigmoid(-w[k] - last_act[i]));
-            w[k] -= target_layer->learning_rate * d_z;
+            //real d_z = d_target_act[k]*(-sigmoid(-w[k] - last_act[i]));
+            real d_z = d_target_act[k]*(sigmoid(-w[k] - last_act[i]));
+            w[k] -= last_to_target->learning_rate * d_z;
 
             d_last_act[i] += d_z;
         }
