19ca65e21ba4dae6f23cb84d8a9f3af195ba9c2c
ducharme <ducharme@15d43a8f-0efa-0310-bc34-a8a306da697e>
2007-01-24 20:23:24 +0000

    Permet d'envoyer une exception a une thread qui roule.

---
+++ python_modules/plearn/utilities/thread_utils.py
@@ -0,0 +1,18 @@
+import inspect
+import ctypes
+ 
+ 
+## Allows one thread to raise exceptions in the context of another thread
+def _async_raise(tid, exctype):
+    """Raises the exception, performs cleanup if needed"""
+
+    if not inspect.isclass(exctype):
+        raise TypeError("Only types can be raised (not instances)")
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
+    if res == 0:
+        raise ValueError("Invalid thread id")
+    elif res != 1:
+        # """if it returns a number greater than one, you're in trouble, 
+        # and you should call it again with exc=NULL to revert the effect"""
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, 0)
+        raise SystemError("PyThreadState_SetAsyncExc failed")
