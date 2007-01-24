bdbe3055cf7dae1b23edffcfe46a57062410406b
lamblin <lamblin@15d43a8f-0efa-0310-bc34-a8a306da697e>
2007-01-24 02:06:03 +0000

    New skeleton for CostModule (subclass of OnlineLearningModule)

---
+++ scripts/Skeletons/CostModule.cc
@@ -0,0 +1,169 @@
+
+#include "DERIVEDCLASS.h"
+
+namespace PLearn {
+using namespace std;
+
+PLEARN_IMPLEMENT_OBJECT(
+    DERIVEDCLASS,
+    "ONE LINE DESCRIPTION",
+    "MULTI-LINE \nHELP");
+
+DERIVEDCLASS::DERIVEDCLASS() :
+/* ### Initialize all fields to their default value here */
+{
+    // ...
+
+    // ### You may (or not) want to call build_() to finish building the object
+    // ### (doing so assumes the parent classes' build_() have been called too
+    // ### in the parent classes' constructors, something that you must ensure)
+}
+
+void DERIVEDCLASS::declareOptions(OptionList& ol)
+{
+    // ### Declare all of this object's options here.
+    // ### For the "flags" of each option, you should typically specify
+    // ### one of OptionBase::buildoption, OptionBase::learntoption or
+    // ### OptionBase::tuningoption. If you don't provide one of these three,
+    // ### this option will be ignored when loading values from a script.
+    // ### You can also combine flags, for example with OptionBase::nosave:
+    // ### (OptionBase::buildoption | OptionBase::nosave)
+
+    // ### ex:
+    // declareOption(ol, "myoption", &DERIVEDCLASS::myoption,
+    //               OptionBase::buildoption,
+    //               "Help text describing this option");
+    // ...
+
+    // Now call the parent class' declareOptions
+    inherited::declareOptions(ol);
+}
+
+void DERIVEDCLASS::build_()
+{
+    // ### This method should do the real building of the object,
+    // ### according to set 'options', in *any* situation.
+    // ### Typical situations include:
+    // ###  - Initial building of an object from a few user-specified options
+    // ###  - Building of a "reloaded" object: i.e. from the complete set of
+    // ###    all serialised options.
+    // ###  - Updating or "re-building" of an object after a few "tuning"
+    // ###    options have been modified.
+    // ### You should assume that the parent class' build_() has already been
+    // ### called.
+}
+
+// ### Nothing to add here, simply calls build_
+void DERIVEDCLASS::build()
+{
+    inherited::build();
+    build_();
+}
+
+
+void DERIVEDCLASS::makeDeepCopyFromShallowCopy(CopiesMap& copies)
+{
+    inherited::makeDeepCopyFromShallowCopy(copies);
+
+    // ### Call deepCopyField on all "pointer-like" fields
+    // ### that you wish to be deepCopied rather than
+    // ### shallow-copied.
+    // ### ex:
+    // deepCopyField(trainvec, copies);
+
+    // ### Remove this line when you have fully implemented this method.
+    PLERROR("DERIVEDCLASS::makeDeepCopyFromShallowCopy not fully (correctly) implemented yet!");
+}
+
+///////////
+// fprop //
+///////////
+void DERIVEDCLASS::fprop(const Vec& input, const Vec& target, Vec& cost) const
+{
+}
+
+/* OPTIONAL
+void DERIVEDCLASS::fprop(const Vec& input, const Vec& target, real& cost) const
+{
+}
+*/
+
+/////////////////
+// bpropUpdate //
+/////////////////
+void DERIVEDCLASS::bpropUpdate(const Vec& input, const Vec& target, real cost,
+                               Vec& input_gradient)
+{
+}
+
+/* THIS METHOD IS OPTIONAL
+void DERIVEDCLASS::bpropUpdate(const Vec& input, const Vec& target, real cost)
+{
+}
+*/
+
+/////////////////
+// bpropUpdate //
+/////////////////
+/* THIS METHOD IS OPTIONAL
+void DERIVEDCLASS::bbpropUpdate(const Vec& input, const Vec& target, real cost,
+                                Vec& input_gradient, Vec& input_diag_hessian)
+{
+}
+*/
+
+/* THIS METHOD IS OPTIONAL
+void DERIVEDCLASS::bbpropUpdate(const Vec& input, const Vec& target, real cost)
+{
+}
+*/
+
+///////////
+// reset //
+///////////
+void DERIVEDCLASS::forget()
+{
+}
+
+//////////
+// name //
+//////////
+TVec<string> DERIVEDCLASS::name()
+{
+    // ### Usually, the name of the class without the trailing "CostModule"
+    return TVec<string>(1, "DERIVEDCLASS");
+}
+
+//////////////
+// finalize //
+//////////////
+/* THIS METHOD IS OPTIONAL
+void DERIVEDCLASS::finalize()
+{
+}
+*/
+
+//////////////////////
+// bpropDoesNothing //
+//////////////////////
+/* THIS METHOD IS OPTIONAL
+bool DERIVEDCLASS::bpropDoesNothing()
+{
+}
+*/
+
+
+} // end of namespace PLearn
+
+
+/*
+  Local Variables:
+  mode:c++
+  c-basic-offset:4
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:79
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=79 :
---
+++ scripts/Skeletons/CostModule.h
@@ -0,0 +1,159 @@
+#ifndef DERIVEDCLASS_INC
+#define DERIVEDCLASS_INC
+
+#include <plearn_learners/online/CostModule.h>
+
+namespace PLearn {
+
+/**
+ * The first sentence should be a BRIEF DESCRIPTION of what the class does.
+ * Place the rest of the class programmer documentation here.  Doxygen supports
+ * Javadoc-style comments.  See http://www.doxygen.org/manual.html
+ *
+ * @todo Write class to-do's here if there are any.
+ *
+ * @deprecated Write deprecated stuff here if there is any.  Indicate what else
+ * should be used instead.
+ */
+class DERIVEDCLASS : public CostModule
+{
+    typedef CostModule inherited;
+
+public:
+    //#####  Public Build Options  ############################################
+
+    //! ### declare public option fields (such as build options) here
+    //! Start your comments with Doxygen-compatible comments such as //!
+
+public:
+    //#####  Public Member Functions  #########################################
+
+    //! Default constructor
+    // ### Make sure the implementation in the .cc
+    // ### initializes all fields to reasonable default values.
+    DERIVEDCLASS();
+
+    // Your other public member functions go here
+
+    //! Given the input and the target, compute a vector of costs
+    //! (possibly resize it appropriately)
+    virtual void fprop(const Vec& input, const Vec& target, Vec& cost) const;
+
+    /* Optional, if you want to optimize it for performance reasons.
+       Default implementation computes the other costs, then ignore them.
+    //! Given the input and the target, compute only the first cost
+    //! (of which we will compute the gradient)
+    virtual void fprop(const Vec& input, const Vec& target, real& cost) const;
+    */
+
+    //! Adapt based on the cost, and compute input gradient to backpropagate.
+    virtual void bpropUpdate(const Vec& input, const Vec& target, real cost,
+                             Vec& input_gradient);
+
+    /* Optional
+       N.B. A DEFAULT IMPLEMENTATION IS PROVIDED IN THE SUPER-CLASS, WHICH
+       JUST CALLS
+            bpropUpdate(input, target, cost, input_gradient)
+       AND IGNORES INPUT GRADIENT.
+    //! Adapt based on the the cost.
+    virtual void bpropUpdate(const Vec& input, const Vec& target,
+                             real cost);
+    */
+
+    /* Optional
+       N.B. A DEFAULT IMPLEMENTATION IS PROVIDED IN THE SUPER-CLASS, WHICH
+       RAISES A PLERROR.
+    //! Similar to bpropUpdate, but adapt based also on the estimation
+    //! of the diagonal of the Hessian matrix, and propagates this back.
+    //! If these methods are defined, you can use them INSTEAD of
+    //! bpropUpdate(...)
+    virtual void bbpropUpdate(const Vec& input, const Vec& target, real cost,
+                              Vec& input_gradient, Vec& input_diag_hessian);
+    */
+
+    /* Optional
+       N.B. A DEFAULT IMPLEMENTATION IS PROVIDED IN THE SUPER-CLASS,
+       WHICH JUST CALLS
+            bbpropUpdate(input, target, cost, input_gradient, in_hess)
+       AND IGNORES INPUT HESSIAN AND INPUT GRADIENT.
+    //! This version does not obtain the input gradient and diag_hessian.
+    virtual void bbpropUpdate(const Vec& input, const Vec& target,
+                              real cost);
+    */
+
+    //! Reset the parameters to the state they would be BEFORE starting
+    //! training.  Note that this method is necessarily called from
+    //! build().
+    virtual void forget();
+
+
+    /* Optional
+       THE DEFAULT IMPLEMENTATION PROVIDED IN THE SUPER-CLASS DOES NOT
+       DO ANYTHING.
+    //! Perform some processing after training, or after a series of
+    //! fprop/bpropUpdate calls to prepare the model for truly out-of-sample
+    //! operation.
+    virtual void finalize();
+    */
+
+    /* Optional
+       THE DEFAULT IMPLEMENTATION PROVIDED IN THE SUPER-CLASS RETURNS false
+    //! In case bpropUpdate does not do anything, make it known
+    virtual bool bpropDoesNothing();
+    */
+
+    //! Indicates the name of the computed costs
+    virtual TVec<string> name();
+
+
+    //#####  PLearn::Object Protocol  #########################################
+
+    // Declares other standard object methods.
+    // ### If your class is not instantiatable (it has pure virtual methods)
+    // ### you should replace this by PLEARN_DECLARE_ABSTRACT_OBJECT
+    PLEARN_DECLARE_OBJECT(DERIVEDCLASS);
+
+    // Simply calls inherited::build() then build_()
+    virtual void build();
+
+    //! Transforms a shallow copy into a deep copy
+    virtual void makeDeepCopyFromShallowCopy(CopiesMap& copies);
+
+
+protected:
+    //#####  Protected Member Functions  ######################################
+
+    //! Declares the class options.
+    static void declareOptions(OptionList& ol);
+
+private:
+    //#####  Private Member Functions  ########################################
+
+    //! This does the actual building.
+    void build_();
+
+private:
+    //#####  Private Data Members  ############################################
+
+    // The rest of the private stuff goes here
+};
+
+// Declares a few other classes and functions related to this class
+DECLARE_OBJECT_PTR(DERIVEDCLASS);
+
+} // end of namespace PLearn
+
+#endif
+
+
+/*
+  Local Variables:
+  mode:c++
+  c-basic-offset:4
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:79
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=79 :
