dd56997dd589783c681c347f3ef6161b97065db6
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2007-04-05 16:36:58 +0000

    - Better help - Implemented deep copy

--- plearn/vmat/BootstrapSplitter.cc
+++ plearn/vmat/BootstrapSplitter.cc
@@ -2,7 +2,7 @@
 
 // BootstrapSplitter.cc
 //
-// Copyright (C) 2003 Olivier Delalleau
+// Copyright (C) 2003,2007 Olivier Delalleau
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
@@ -44,34 +44,35 @@
 namespace PLearn {
 using namespace std;
 
-BootstrapSplitter::BootstrapSplitter()
-    :Splitter(),
-     frac(0.6667),
-     n_splits(0),
-     allow_repetitions(false),
-     seed(1827),
-     rgen(new PRandom())
-    /* ### Initialise all fields to their default value */
-{
-}
+///////////////////////
+// BootstrapSplitter //
+///////////////////////
+BootstrapSplitter::BootstrapSplitter():
+    frac(0.6667),
+    n_splits(0),
+    allow_repetitions(false),
+    seed(1827),
+    rgen(new PRandom())
+{}
 
 PLEARN_IMPLEMENT_OBJECT(BootstrapSplitter,
-                        "A splitter whose splits are bootstrap samples of the original dataset",
-                        "BootstrapSplitter implements a ...");
+        "A splitter whose splits are bootstrap samples of the original dataset.",
+        "Each split of this splitter contains a single set, which is a\n"
+        "bootstrap sample of the original dataset. Note that, by default,\n"
+        "samples are taken without repetition: in order to perform a 'real'\n"
+        "bootstrap, the option 'allow_repetitions thus must be set to 1.\n"
+);
 
+////////////////////
+// declareOptions //
+////////////////////
 void BootstrapSplitter::declareOptions(OptionList& ol)
 {
-    // ### Declare all of this object's options here
-    // ### For the "flags" of each option, you should typically specify
-    // ### one of OptionBase::buildoption, OptionBase::learntoption or
-    // ### OptionBase::tuningoption. Another possible flag to be combined with
-    // ### is OptionBase::nosave
-
     declareOption(ol, "n_splits", &BootstrapSplitter::n_splits, OptionBase::buildoption,
-                  "the number of splits wanted");
+                  "Number of splits wanted.");
 
     declareOption(ol, "frac", &BootstrapSplitter::frac, OptionBase::buildoption,
-                  "the fraction of elements to take in each bootstrap");
+                  "Fraction of elements to take in each bootstrap.");
 
     declareOption(ol, "allow_repetitions", &BootstrapSplitter::allow_repetitions, 
                   OptionBase::buildoption,
@@ -79,21 +80,17 @@ void BootstrapSplitter::declareOptions(OptionList& ol)
 
     declareOption(ol, "seed", &BootstrapSplitter::seed, 
                   OptionBase::buildoption,
-                  "Allows each row to appear more than once per split.");
+                  "Seed for the random number generator.");
 
     // Now call the parent class' declareOptions
     inherited::declareOptions(ol);
 }
 
+////////////
+// build_ //
+////////////
 void BootstrapSplitter::build_()
 {
-    // ### This method should do the real building of the object,
-    // ### according to set 'options', in *any* situation.
-    // ### Typical situations include:
-    // ###  - Initial building of an object from a few user-specified options
-    // ###  - Building of a "reloaded" object: i.e. from the complete set of all serialised options.
-    // ###  - Updating or "re-building" of an object after a few "tuning" options have been modified.
-    // ### You should assume that the parent class' build_() has already been called.
     if (dataset) {
         rgen->manual_seed(seed);
         bootstrapped_sets.resize(0,0); // First clear the current sets.
@@ -112,36 +109,39 @@ void BootstrapSplitter::build_()
     }
 }
 
-// ### Nothing to add here, simply calls build_
+///////////
+// build //
+///////////
 void BootstrapSplitter::build()
 {
     inherited::build();
     build_();
 }
 
+/////////////////////////////////
+// makeDeepCopyFromShallowCopy //
+/////////////////////////////////
 void BootstrapSplitter::makeDeepCopyFromShallowCopy(CopiesMap& copies)
 {
     Splitter::makeDeepCopyFromShallowCopy(copies);
-
-    // ### Call deepCopyField on all "pointer-like" fields
-    // ### that you wish to be deepCopied rather than
-    // ### shallow-copied.
-    // ### ex:
-    // deepCopyField(trainvec, copies);
-
-    // ### Remove this line when you have fully implemented this method.
-    PLERROR("BootstrapSplitter::makeDeepCopyFromShallowCopy not fully (correctly) implemented yet!");
+    deepCopyField(bootstrapped_sets, copies);
+    deepCopyField(rgen,              copies);
 }
 
+/////////////
+// nsplits //
+/////////////
 int BootstrapSplitter::nsplits() const
 {
-    // ### Return the number of available splits
     return n_splits;
 }
 
+///////////////////
+// nSetsPerSplit //
+///////////////////
 int BootstrapSplitter::nSetsPerSplit() const
 {
-    // ### Return the number of sets per split
+    // One single set per split.
     return 1;
 }
 
