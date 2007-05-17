ac9f4432fcb381dba13d761e44a7840472144f9e
nouiz <nouiz@15d43a8f-0efa-0310-bc34-a8a306da697e>
2007-05-17 21:02:38 +0000

    -Modified so that it is the execution script that set the environnement variable instead of being set by dbi.py. -Corrected a bug so that the launch.sh file is reated when requested

--- python_modules/plearn/parallel/dbi.py
+++ python_modules/plearn/parallel/dbi.py
@@ -471,38 +471,36 @@ class DBICondor(DBIBase):
                 output         = main.%s.%s.out
                 error          = main.%s.%s.error
                 log            = main.%s.log
-                environment    = LD_LIBRARY_PATH=/u/lisa/local/%s/lib:/cluster/diro/home/lisa/local/%s/lib/python2.4/site-packages/numarray:/cluster/diro/home/lisa/local/%s/lib:/u/lisa/local/%s/lib:/cluster/diro/home/lisa/local/%s/lib/python2.4/site-packages/numarray:/cluster/diro/home/lisa/local/%s/lib:/cluster/diro/home/lisa/lib:/usr/local/lib:/soft/lisa/linux/lib;\
-                PATH=/cluster/diro/home/lisa/PLearn/scripts:/soft/lisa/linux/bin:/u/lisa/local/%s/bin:/u/%s/PLearn/commands:/u/%s/PLearn/scripts:/Scripts:/commands:/u/%s/Scripts:/soft/diro/share/moe/bin-i4lx:/u/lamblinp/code/usr/bin:/u/%s/PLearn:/u/%s/PLearn/scripts:/u/%s/PLearn/commands:/usr/kerberos/bin:/usr/GNUstep/System/Tools:/usr/local/bin:/bin:/usr/bin:/usr/X11R6/bin:/opt/diro/bin:/u/lisa/local/linux-i386/bin;\
-                PYTHONPATH=/cluster/diro/home/lisa/local/%s/lib/python2.4/site-packages:/cluster/diro/home/lisa/local/%s/lib/python2.3/site-packages:/cluster/diro/home/lisa/local/%s/lib/python2.2/site-packages:/cluster/diro/home/lisa/local/%s/lib/python2.4/site-packages/vtk_python:/cluster/diro/home/lisa/local/%s/lib/python2.3/site-packages/Numeric:/cluster/diro/home/lisa/PLearn/python_modules:/u/%s/PLearn/python_modules:/u/%s/projects/apstatsoft/python_modules;
+#                environment    = 
                 #getenv         = True
                 queue
-                ''' % (self.tmp_dir,condor_data, tcplat1,tcplat2,tcplat1,task.unique_id,tcplat1,task.unique_id,tcplat1,'linux-x86_64','linux-x86_64','linux-x86_64','linux-i386','linux-i386','linux-i386',tplat,u,u,u,u,u,u,tplat,tplat,tplat,tplat,tplat,u,u)) )
+                ''' % (self.tmp_dir,condor_data, tcplat1,tcplat2,tcplat1,task.unique_id,tcplat1,task.unique_id,tcplat1)))
 #                preBatch = ''' + pre_batch_command + '''
 #                postBatch = ''' + post_batch_command +'''
         condor_dat.close()
-                #environment    = LD_LIBRARY_PATH=/u/lisa/local/byhost/%s/lib:/u/lisa/local/%s/lib:/cluster/diro/home/lisa/local/%s/lib/python2.4/site-packages/numarray:/cluster/diro/home/lisa/local/%s/lib:/usr/local/lib:/cluster/diro/home/lisa/local/%s/lib32:/soft/lisa/linux/lib;PATH=/cluster/diro/home/lisa/PLearn/scripts:/soft/lisa/linux/bin:/u/lisa/local/%s/bin:/u/%s/PLearn/commands:/u/%s/PLearn/scripts:/Scripts:/commands:/u/%s/Scripts:/soft/diro/share/moe/bin-i4lx:/u/lamblinp/code/usr/bin:/u/%s/PLearn:/u/%s/PLearn/scripts:/u/%s/PLearn/commands:/u/%s/projects/apstatsoft:/u/%s/projects/apstatsoft/scripts:/u/%s/projects/apstatsoft/commands:/usr/kerberos/bin:/u/%s/GNUstep/Tools:/usr/GNUstep/Local/Tools:/usr/GNUstep/System/Tools:/usr/local/bin:/bin:/usr/bin:/usr/X11R6/bin:/opt/diro/bin:/u/lisa/local/linux-i386/bin;PYTHONPATH=/cluster/diro/home/lisa/PLearn/python_modules:/cluster/diro/home/lisa/local/%s/lib/python2.4/site-packages:/u/lisa/local/%s/lib/python2.3/site-packages:/u/lisa/local/%s/lib/python2.2/site-packages:/u/lisa/local/%s/lib/python2.4/site-packages/vtk_python:/u/lisa/local/%s/lib/python2.3/site-packages/Numeric:/u/%s/PLearn/python_modules:/u/%s/projects/apstatsoft/python_modules:
 
         launch_file = os.path.join(self.tmp_dir, 'launch.sh')
         dbi_file=get_plearndir()+'/python_modules/plearn/parallel/dbi.py'
+        overwrite_launch_file=False
         if not os.path.exists(dbi_file):
             print 'WARNING: Can\' locate dbi.py file. Meaby the file "'+launch_file+'" is not up to date!'
-
-        mtimel=os.stat(launch_file)[8]
-        mtimed=os.stat(dbi_file)[8]
-        if mtimed>mtimel:
-            print 'WARNING: We overwrite the file "'+launch_file+'" with a new version. Update it to your need!'
+        else:
+            if os.path.exists(launch_file):
+                mtimed=os.stat(dbi_file)[8]
+                mtimel=os.stat(launch_file)[8]
+                if mtimed>mtimel:
+                    print 'WARNING: We overwrite the file "'+launch_file+'" with a new version. Update it to your need!'
+                    overwrite_launch_file=True
         
-        if not os.path.exists(launch_file) or mtimed>mtimel:            
+        if not os.path.exists(launch_file) or overwrite_launch_file:
             self.temp_files.append(launch_file)
             launch_dat = open(launch_file,'w')
             launch_dat.write(dedent('''\
             #!/bin/sh
             PROGRAM=$1
             shift
-            export HOSTNAME=`hostname`
-            export HOST=${HOSTNAME}
-            export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/cluster/diro/home/lisa/local/byhost/${HOSTNAME}/lib:/cluster/diro/home/lisa/local/byhost/${HOSTNAME}/lib32
-            echo -n "Executing on ${HOSTNAME}"
+            source /cluster/diro/home/lisa/.local.condor
+            echo "Executing on ${HOSTNAME}"
             echo "PATH: $PATH"
             echo "PYTHONPATH: $PYTHONPATH"
             echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
