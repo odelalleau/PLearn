1b73b4f12ee5201619f4486a3844b7a16babc3d1
larocheh <larocheh@15d43a8f-0efa-0310-bc34-a8a306da697e>
2007-05-20 17:23:37 +0000

    Manifold version of k-NN density estimator

---
+++ plearn_learners_experimental/ManifoldKNNDistribution.cc
@@ -0,0 +1,295 @@
+// -*- C++ -*-
+
+// ManifoldKNNDistribution.cc
+//
+// Copyright (C) 2007 Hugo Larochelle
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+// Authors: Hugo Larochelle
+
+/*! \file ManifoldKNNDistribution.cc */
+
+
+#include "ManifoldKNNDistribution.h"
+#include <plearn/math/pl_erf.h>
+#include <plearn/math/plapack.h>
+
+namespace PLearn {
+using namespace std;
+
+PLEARN_IMPLEMENT_OBJECT(
+    ManifoldKNNDistribution,
+    "K nearest neighbors density estimator locally taking into account the manifold",
+    ""
+    );
+
+/////////////////////////////
+// ManifoldKNNDistribution //
+/////////////////////////////
+ManifoldKNNDistribution::ManifoldKNNDistribution()
+    : manifold_dimensionality(5),
+      min_sigma_square(1e-5)
+{}
+
+////////////////////
+// declareOptions //
+////////////////////
+void ManifoldKNNDistribution::declareOptions(OptionList& ol)
+{
+    declareOption(ol, "knn_manifold", &ManifoldKNNDistribution::knn_manifold,
+                  OptionBase::buildoption,
+                  "Nearest neighbors search algorithms for local manifold structure"
+                  "estimation.");
+
+    declareOption(ol, "knn_density", &ManifoldKNNDistribution::knn_density,
+                  OptionBase::buildoption,
+                  "Nearest neighbors search algorithms for density "
+                  "estimation from ellipsoid\n"
+                  "volume.");
+
+    declareOption(ol, "manifold_dimensionality", 
+                  &ManifoldKNNDistribution::manifold_dimensionality,
+                  OptionBase::buildoption,
+                  "Dimensionality of the manifold.");
+
+    declareOption(ol, "min_sigma_square", 
+                  &ManifoldKNNDistribution::min_sigma_square,
+                  OptionBase::buildoption,
+                  "Minimum variance in all directions on the manifold. This value"
+                  "is added\n"
+                  "to the estimated covariance matrix.");
+
+    // Now call the parent class' declareOptions().
+    inherited::declareOptions(ol);
+}
+
+///////////
+// build //
+///////////
+void ManifoldKNNDistribution::build()
+{
+    // ### Nothing to add here, simply calls build_().
+    inherited::build();
+    build_();
+}
+
+////////////
+// build_ //
+////////////
+void ManifoldKNNDistribution::build_()
+{
+    if(min_sigma_square < 0)
+        PLERROR("In ManifoldKNNDistribution::build_(): min_sigma_square should be"
+                " >= 0.");
+
+    if(!knn_manifold)
+        PLERROR("In ManifoldKNNDistribution::build_(): knn_manifold must be"
+                " provided.");
+
+    if(!knn_density)
+        PLERROR("In ManifoldKNNDistribution::build_(): knn_density must be"
+                " provided.");
+
+    if(inputsize_ > 0)
+    {
+        if(manifold_dimensionality > inputsize_)
+            manifold_dimensionality = inputsize_;
+        if(manifold_dimensionality < 1)
+            PLERROR("In ManifoldKNNDistribution::build_(): manifold_dimensionality"
+                    " should be > 0.");
+        eig_vectors.resize(manifold_dimensionality,inputsize_);
+        eig_values.resize(manifold_dimensionality);
+        Ut.resize(inputsize_,inputsize_);
+        V.resize(knn_manifold->num_neighbors,knn_manifold->num_neighbors);
+        eig_vectors_projection.resize(manifold_dimensionality);
+    }
+
+    if(train_set)
+    {
+        knn_manifold->setTrainingSet(train_set,true);
+        knn_density->setTrainingSet(train_set,true);
+    }
+}
+
+/////////
+// cdf //
+/////////
+real ManifoldKNNDistribution::cdf(const Vec& y) const
+{
+    PLERROR("cdf not implemented for ManifoldKNNDistribution"); return 0;
+}
+
+/////////////////
+// expectation //
+/////////////////
+void ManifoldKNNDistribution::expectation(Vec& mu) const
+{
+    PLERROR("expectation not implemented for ManifoldKNNDistribution");
+}
+
+// ### Remove this method if your distribution does not implement it.
+////////////
+// forget //
+////////////
+void ManifoldKNNDistribution::forget()
+{}
+
+//////////////
+// generate //
+//////////////
+void ManifoldKNNDistribution::generate(Vec& y) const
+{
+    PLERROR("generate not implemented for ManifoldKNNDistribution");
+}
+
+/////////////////
+// log_density //
+/////////////////
+real ManifoldKNNDistribution::log_density(const Vec& y) const
+{
+    computeLocalPrincipalComponents(y,eig_values,eig_vectors);
+    
+    // Find volume of ellipsoid defined by eig_values, eig_vectors and
+    // min_sigma_square that covers all the nearest_neighbors found by knn_density
+    knn_density->computeOutput(y,nearest_neighbors_density_vec);
+    nearest_neighbors_density = 
+        nearest_neighbors_density_vec.toMat(knn_density->num_neighbors,inputsize_);
+    nearest_neighbors_density -= y;
+    real max = -1;
+    real scaled_projection;
+    for(int i=0; i<nearest_neighbors_density.length(); i++)
+    {
+        scaled_projection = 0;
+        product(eig_vectors_projection,eig_vectors,nearest_neighbors_density(i));
+        for(int j=0; j<eig_values.length(); j++)
+            scaled_projection += mypow(eig_vectors_projection[j],2) * 
+                (1/(eig_values[j]+min_sigma_square) 
+                 - 1/min_sigma_square) ;
+        scaled_projection += pownorm(nearest_neighbors_density(i),2)
+            /min_sigma_square;
+        if(max < scaled_projection)
+            max = scaled_projection;
+    }
+
+    // Compute log-volume of the ellipsoid: pi
+    real log_vol = 0.5 * inputsize_ * pl_log(scaled_projection);
+    for(int i=0; i<manifold_dimensionality; i++)
+        log_vol += 0.5 * pl_log(eig_values[i]+min_sigma_square);
+    log_vol += (inputsize_-manifold_dimensionality)*0.5*pl_log(min_sigma_square);
+    log_vol += 0.5*inputsize_*pl_log(Pi) - pl_gammln(0.5*inputsize_+1);
+    
+    return pl_log(knn_density->num_neighbors)-pl_log(n_examples)-log_vol;
+}
+
+/////////////////////////////////
+// makeDeepCopyFromShallowCopy //
+/////////////////////////////////
+void ManifoldKNNDistribution::makeDeepCopyFromShallowCopy(CopiesMap& copies)
+{
+    inherited::makeDeepCopyFromShallowCopy(copies);
+
+    deepCopyField(knn_manifold, copies);
+    deepCopyField(knn_density, copies);
+    deepCopyField(nearest_neighbors_manifold, copies);
+    deepCopyField(nearest_neighbors_manifold_vec, copies);
+    deepCopyField(nearest_neighbors_density, copies);
+    deepCopyField(nearest_neighbors_density_vec, copies);
+    deepCopyField(eig_vectors, copies);
+    deepCopyField(eig_values, copies);
+    deepCopyField(Ut, copies);
+    deepCopyField(V, copies);
+    deepCopyField(S, copies);
+    deepCopyField(eig_vectors_projection, copies);
+
+}
+
+////////////////////
+// resetGenerator //
+////////////////////
+void ManifoldKNNDistribution::resetGenerator(long g_seed) const
+{
+    PLERROR("resetGenerator not implemented for ManifoldKNNDistribution");
+}
+
+/////////////////
+// survival_fn //
+/////////////////
+real ManifoldKNNDistribution::survival_fn(const Vec& y) const
+{
+    PLERROR("survival_fn not implemented for ManifoldKNNDistribution"); return 0;
+}
+
+// ### Remove this method, if your distribution does not implement it.
+///////////
+// train //
+///////////
+void ManifoldKNNDistribution::train()
+{}
+
+//////////////
+// variance //
+//////////////
+void ManifoldKNNDistribution::variance(Mat& covar) const
+{
+    PLERROR("variance not implemented for ManifoldKNNDistribution");
+}
+
+void ManifoldKNNDistribution::computeLocalPrincipalComponents(const Vec& x, 
+                                         Vec& eig_values, Mat& eig_vectors) const
+{
+    knn_manifold->computeOutput(x,nearest_neighbors_manifold_vec);
+    nearest_neighbors_manifold = 
+        nearest_neighbors_manifold_vec.toMat(knn_manifold->num_neighbors,inputsize_);
+    nearest_neighbors_manifold -= x;
+    
+    // Compute principal components
+    // N.B. this is the SVD of F'
+    lapackSVD(nearest_neighbors_manifold, Ut, S, V,'A',1.5);
+    for (int k=0;k<manifold_dimensionality;k++)
+    {
+        eig_values[k] = mypow(S[k],2);
+        eig_vectors(k) << Ut(k);
+    }  
+}
+
+} // end of namespace PLearn
+
+
+/*
+  Local Variables:
+  mode:c++
+  c-basic-offset:4
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:79
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=79 :
---
+++ plearn_learners_experimental/ManifoldKNNDistribution.h
@@ -0,0 +1,195 @@
+// -*- C++ -*-
+
+// ManifoldKNNDistribution.h
+//
+// Copyright (C) 2007 Hugo Larochelle
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+// Authors: Hugo Larochelle
+
+/*! \file ManifoldKNNDistribution.h */
+
+
+#ifndef ManifoldKNNDistribution_INC
+#define ManifoldKNNDistribution_INC
+
+#include <plearn_learners/distributions/UnconditionalDistribution.h>
+#include <plearn_learners/nearest_neighbors/GenericNearestNeighbors.h>
+
+namespace PLearn {
+
+// -*- C++ -*-
+
+
+/**
+ * K nearest neighbors density estimator that takes into accound the local manifold
+ * structure.
+ */
+class ManifoldKNNDistribution : public UnconditionalDistribution
+{
+    typedef UnconditionalDistribution inherited;
+
+public:
+    //#####  Public Build Options  ############################################
+
+    //! Nearest neighbors search algorithms for local manifold structure estimation.
+    PP<GenericNearestNeighbors> knn_manifold;
+
+    //! Nearest neighbors search algorithms for density estimation from ellipsoid's
+    //! volume.
+    PP<GenericNearestNeighbors> knn_density;
+
+    //! Dimensionality of the manifold.
+    int manifold_dimensionality;
+    
+    //! Minimum variance in all directions on the manifold. This value is added
+    //! to the estimated covariance matrix.
+    real min_sigma_square;
+
+public:
+    //#####  Public Member Functions  #########################################
+
+    //! Default constructor
+    // ### Make sure the implementation in the .cc
+    // ### initializes all fields to reasonable default values.
+    ManifoldKNNDistribution();
+
+
+    //#####  UnconditionalDistribution Member Functions  ######################
+
+    //! Return log of probability density log(p(y)).
+    virtual real log_density(const Vec& x) const;
+
+    //! Return survival function: P(Y>y).
+    virtual real survival_fn(const Vec& y) const;
+
+    //! Return cdf: P(Y<y).
+    virtual real cdf(const Vec& y) const;
+
+    //! Return E[Y].
+    virtual void expectation(Vec& mu) const;
+
+    //! Return Var[Y].
+    virtual void variance(Mat& cov) const;
+
+    //! Return a pseudo-random sample generated from the distribution.
+    virtual void generate(Vec& y) const;
+
+    //! Reset the random number generator used by generate() using the
+    //! given seed.
+    virtual void resetGenerator(long g_seed) const;
+
+    //#####  PLearner Member Functions  #######################################
+
+    //! (Re-)initializes the PDistribution in its fresh state (that state may
+    //! depend on the 'seed' option) and sets 'stage' back to 0 (this is the
+    //! stage of a fresh learner!).
+    virtual void forget();
+
+    //! The role of the train method is to bring the learner up to
+    //! stage == nstages, updating the train_stats collector with training
+    //! costs measured on-line in the process.
+    virtual void train();
+
+
+    //#####  PLearn::Object Protocol  #########################################
+
+    // Declares other standard object methods.
+    PLEARN_DECLARE_OBJECT(ManifoldKNNDistribution);
+
+    // Simply calls inherited::build() then build_()
+    virtual void build();
+
+    //! Transforms a shallow copy into a deep copy
+    virtual void makeDeepCopyFromShallowCopy(CopiesMap& copies);
+
+protected:
+    //#####  Protected Options  ###############################################
+
+    //! Nearest neighbors for manifold estimation
+    mutable Mat nearest_neighbors_manifold;
+    //! Nearest neighbors for manifold estimation as a vector
+    mutable Vec nearest_neighbors_manifold_vec;
+    //! Nearest neighbors for density estimation
+    mutable Mat nearest_neighbors_density;
+    //! Nearest neighbors for density estimation as a vector
+    mutable Vec nearest_neighbors_density_vec;
+    //! Manifold local tangent vectors
+    mutable Mat eig_vectors;
+    //! Manifold local standard deviations
+    mutable Vec eig_values;
+    //! SVD temporary variables
+    mutable Mat Ut, V;
+    //! SVD temporary variable
+    mutable Vec S;
+    //! Projection on the local tangent vectors
+    mutable Vec eig_vectors_projection;
+
+protected:
+    //#####  Protected Member Functions  ######################################
+
+    //! Declares the class options.
+    static void declareOptions(OptionList& ol);
+
+    void computeLocalPrincipalComponents(const Vec& x, 
+                                         Vec& eig_values, 
+                                         Mat& eig_vectors) const;
+
+private:
+    //#####  Private Member Functions  ########################################
+
+    //! This does the actual building.
+    void build_();
+
+private:
+    //#####  Private Data Members  ############################################
+
+    // The rest of the private stuff goes here
+};
+
+// Declares a few other classes and functions related to this class
+DECLARE_OBJECT_PTR(ManifoldKNNDistribution);
+
+} // end of namespace PLearn
+
+#endif
+
+
+/*
+  Local Variables:
+  mode:c++
+  c-basic-offset:4
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:79
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=79 :
