f2e22a356d72e197cc981f13b6f8ac73172d4e22
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2007-05-21 17:22:52 +0000

    Removed outdated comment

--- plearn_learners/online/ModuleLearner.cc
+++ plearn_learners/online/ModuleLearner.cc
@@ -200,7 +200,6 @@ void ModuleLearner::makeDeepCopyFromShallowCopy(CopiesMap& copies)
     deepCopyField(store_outputs,      copies);
     deepCopyField(store_costs,        copies);
     deepCopyField(network,            copies);
-    // does not compile right now
     deepCopyField(null_pointers,      copies);
 }
 
