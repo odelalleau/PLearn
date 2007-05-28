9547ba6296adeff2c967f643608d591df3c63b61
dumitruerhan <dumitruerhan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2007-05-28 22:04:15 +0000

    Added a module that computes the cross entropy between a vector of activations and a target vector

---
+++ plearn_learners/online/CrossEntropyCostModule.cc
@@ -0,0 +1,199 @@
+// -*- C++ -*-
+
+// CrossEntropyCostModule.cc
+//
+// Copyright (C) 2007 Pascal Lamblin, Dumitru Erhan and Hugo Larochelle
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+// Authors: Pascal Lamblin, Dumitru Erhan and Hugo Larochelle
+
+/*! \file CrossEntropyCostModule.cc */
+
+
+
+#include "CrossEntropyCostModule.h"
+
+namespace PLearn {
+using namespace std;
+
+PLEARN_IMPLEMENT_OBJECT(CrossEntropyCostModule,
+    "Computes the CrossEntropy, given two activation vectors",
+    "and back-propagates the gradient\n");
+
+CrossEntropyCostModule::CrossEntropyCostModule()
+{
+    output_size = 1;
+}
+
+void CrossEntropyCostModule::declareOptions(OptionList& ol)
+{
+    // Now call the parent class' declareOptions
+    inherited::declareOptions(ol);
+
+    redeclareOption(ol, "target_size", &CrossEntropyCostModule::target_size,
+                     OptionBase::nosave,
+                     "equals to input_size");
+
+}
+
+void CrossEntropyCostModule::build_()
+{
+    target_size = input_size;
+}
+
+// ### Nothing to add here, simply calls build_
+void CrossEntropyCostModule::build()
+{
+    inherited::build();
+    build_();
+}
+
+
+void CrossEntropyCostModule::makeDeepCopyFromShallowCopy(CopiesMap& copies)
+{
+    inherited::makeDeepCopyFromShallowCopy(copies);
+}
+
+
+///////////
+// fprop //
+///////////
+void CrossEntropyCostModule::fprop(const Vec& input, const Vec& target, real& cost) const
+{
+    PLASSERT( input.size() == input_size );
+    PLASSERT( target.size() == target_size );
+
+    cost = 0;
+
+    real target_i, activation_i;
+    for( int i=0 ; i < target_size ; i++ )
+    {
+        target_i = target[i];
+        activation_i = input[i];
+        if(!fast_exact_is_equal(target_i,0.0))
+            // nll -= target[i] * pl_log(expectations[i]);
+            // but it is numerically unstable, so use instead
+            // log (1/(1+exp(-x))) = -log(1+exp(-x)) = -softplus(-x)
+            // but note that expectation = sigmoid(-activation)
+            cost += target_i * softplus(activation_i);
+        if(!fast_exact_is_equal(target_i,1.0))
+            // ret -= (1-target_i) * pl_log(1-expectation_i);
+            // log (1 - 1/(1+exp(-x))) = log(exp(-x)/(1+exp(-x))) =
+            //                         = -x -log(1+exp(-x)) = -x-softplus(-x)
+            cost += (1-target_i) * (softplus(activation_i) - activation_i);
+    }
+
+}
+
+void CrossEntropyCostModule::fprop(const Vec& input, const Vec& target, Vec& cost) const
+{
+    cost.resize( output_size );
+    fprop( input, target, cost[0] );
+}
+
+void CrossEntropyCostModule::fprop(const Mat& inputs, const Mat& targets, Mat& costs) const
+{
+    costs.resize( inputs.length(), output_size );
+
+    for (int i = 0; i < inputs.length(); i++)
+        fprop(inputs(i), targets(i), costs(i,0));
+ 
+}
+
+void CrossEntropyCostModule::bpropAccUpdate(const TVec<Mat*>& ports_value,
+                                   const TVec<Mat*>& ports_gradient)
+{
+    PLASSERT( ports_value.length() == nPorts() );
+    PLASSERT( ports_gradient.length() == nPorts() );
+
+    Mat* prediction = ports_value[0];
+    Mat* target = ports_value[1];
+    Mat* cost = ports_value[2];
+    Mat* prediction_grad = ports_gradient[0];
+    Mat* target_grad = ports_gradient[1];
+    Mat* cost_grad = ports_gradient[2];
+
+    // If we have cost_grad and we want prediction_grad
+    if( prediction_grad && prediction_grad->isEmpty()
+        && cost_grad && !cost_grad->isEmpty() )
+    {
+        PLASSERT( prediction );
+        PLASSERT( target );
+        PLASSERT( cost );
+        PLASSERT( !target_grad );
+
+        PLASSERT( prediction->width() == port_sizes(0,1) );
+        PLASSERT( target->width() == port_sizes(1,1) );
+        PLASSERT( cost->width() == port_sizes(2,1) );
+        PLASSERT( prediction_grad->width() == port_sizes(0,1) );
+        PLASSERT( cost_grad->width() == port_sizes(2,1) );
+
+        int batch_size = prediction->length();
+        PLASSERT( target->length() == batch_size );
+        PLASSERT( cost->length() == batch_size );
+        PLASSERT( cost_grad->length() == batch_size );
+
+        prediction_grad->resize(batch_size, port_sizes(0,1));
+
+        for( int i=0; i < batch_size; i++ )
+            for ( int j=0; j < target->width(); j++ ) 
+                (*prediction_grad)(i, j) += (*target)(i,j) - sigmoid(-(*prediction)(i,j) );
+    }
+
+    else if( !prediction_grad && !target_grad && !cost_grad )
+        return;
+    else if( !cost_grad && prediction_grad && prediction_grad->isEmpty() )
+        PLERROR("In CrossEntropyCostModule::bpropAccUpdate - cost gradient is NULL,\n"
+                "cannot compute prediction gradient. Maybe you should set\n"
+                "\"propagate_gradient = 0\" on the incoming connection.\n");
+    else
+        PLERROR("In OnlineLearningModule::bpropAccUpdate - Port configuration "
+                "not implemented for class '%s'", classname().c_str());
+}
+
+TVec<string> CrossEntropyCostModule::name()
+{
+    return TVec<string>(1, "CrossEntropy");
+}
+
+} // end of namespace PLearn
+
+
+/*
+  Local Variables:
+  mode:c++
+  c-basic-offset:4
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:79
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=79 :
---
+++ plearn_learners/online/CrossEntropyCostModule.h
@@ -0,0 +1,131 @@
+// -*- C++ -*-
+
+// CrossEntropyCostModule.h
+//
+// Copyright (C) 2007 Pascal Lamblin, Dumitru Erhan and Hugo Larochelle
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+// Authors: Pascal Lamblin, Dumitru Erhan and Hugo Larochelle
+
+/*! \file CrossEntropyCostModule.h */
+
+
+#ifndef CrossEntropyCostModule_INC
+#define CrossEntropyCostModule_INC
+
+#include <plearn_learners/online/CostModule.h>
+
+namespace PLearn {
+
+/**
+ * Computes the cross-entropy, given two activation vectors. Backpropagates it.
+ */
+class CrossEntropyCostModule : public CostModule
+{
+    typedef CostModule inherited;
+
+public:
+    //#####  Public Build Options  ############################################
+
+
+public:
+    //#####  Public Member Functions  #########################################
+
+    //! Default constructor
+    CrossEntropyCostModule();
+
+    // Your other public member functions go here
+
+    //! given the input and target, compute the cost
+    virtual void fprop(const Vec& input, const Vec& target, real& cost) const;
+    virtual void fprop(const Vec& input, const Vec& target, Vec& cost) const;
+    virtual void fprop(const Mat& input, const Mat& target, Mat& cost) const;
+   
+    //! New version of backpropagation
+    virtual void bpropAccUpdate(const TVec<Mat*>& ports_value,
+                                const TVec<Mat*>& ports_gradient);
+
+    //! Overridden to do nothing (in particular, no warning).
+    virtual void setLearningRate(real dynamic_learning_rate) {}
+
+    //! Indicates the name of the computed costs
+    virtual TVec<string> name();
+
+    //#####  PLearn::Object Protocol  #########################################
+
+    // Declares other standard object methods.
+    // ### If your class is not instantiatable (it has pure virtual methods)
+    // ### you should replace this by PLEARN_DECLARE_ABSTRACT_OBJECT
+    PLEARN_DECLARE_OBJECT(CrossEntropyCostModule);
+
+    // Simply calls inherited::build() then build_()
+    virtual void build();
+
+    //! Transforms a shallow copy into a deep copy
+    virtual void makeDeepCopyFromShallowCopy(CopiesMap& copies);
+
+
+protected:
+    //#####  Protected Member Functions  ######################################
+
+    //! Declares the class options.
+    static void declareOptions(OptionList& ol);
+
+private:
+    //#####  Private Member Functions  ########################################
+
+    //! This does the actual building.
+    void build_();
+
+private:
+    //#####  Private Data Members  ############################################
+
+    // The rest of the private stuff goes here
+};
+
+// Declares a few other classes and functions related to this class
+DECLARE_OBJECT_PTR(CrossEntropyCostModule);
+
+} // end of namespace PLearn
+
+#endif
+
+
+/*
+  Local Variables:
+  mode:c++
+  c-basic-offset:4
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:79
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=79 :
