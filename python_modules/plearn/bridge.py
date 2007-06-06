2e2129b78f76ee4e0a482d04d1a442d325b81a69
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2007-06-06 15:00:07 +0000

    Fixed parsing of command line arguments when in server mode, and gave .5 more seconds to the server to launch for safety

--- python_modules/plearn/bridge.py
+++ python_modules/plearn/bridge.py
@@ -8,9 +8,10 @@ if plearn.bridgemode.useserver:
     from plearn.pyplearn import *
     from plearn.io.server import *
     import time
+    plargs.parse([ arg for arg in sys.argv if arg.find('=') != -1 ]) # Parse command-line.
     server_command = plearn.bridgemode.server_exe + ' server'
     serv = launch_plearn_server(command = server_command)
-    time.sleep(0.5) # give some time to the server to get born and well alive
+    time.sleep(1) # give some time to the server to get born and well alive
 else:
     from plearn.pyext import *
 # from gdb do the following to see PLearn symbols AFTER pyext has been loaded
