e203957ea80ba87aa1e72512d61d3bca9ea9d389
lamblin <lamblin@15d43a8f-0efa-0310-bc34-a8a306da697e>
2007-07-27 20:36:36 +0000

    Inject all types defined in <boost/cstdint.hpp> into namespace PLearn

--- plearn/base/pl_stdint.h
+++ plearn/base/pl_stdint.h
@@ -44,16 +44,37 @@
 
 namespace PLearn {
 
-// inject principal boost integer types into namespace PLearn
-// @TODO: use them all?
+// Inject boost integer types into namespace PLearn
 using boost::int8_t;
+using boost::int_least8_t;
+using boost::int_fast8_t;
 using boost::uint8_t;
+using boost::uint_least8_t;
+using boost::uint_fast8_t;
+
 using boost::int16_t;
+using boost::int_least16_t;
+using boost::int_fast16_t;
 using boost::uint16_t;
+using boost::uint_least16_t;
+using boost::uint_fast16_t;
+
 using boost::int32_t;
+using boost::int_least32_t;
+using boost::int_fast32_t;
 using boost::uint32_t;
+using boost::uint_least32_t;
+using boost::uint_fast32_t;
+
 using boost::int64_t;
+using boost::int_least64_t;
+using boost::int_fast64_t;
 using boost::uint64_t;
+using boost::uint_least64_t;
+using boost::uint_fast64_t;
+
+using boost::intmax_t;
+using boost::uintmax_t;
 
 } // end of namespace PLearn
 
