a4b836262a105543fdad407f81aef1655c5f8c65
simonl <simonl@15d43a8f-0efa-0310-bc34-a8a306da697e>
2007-08-23 17:10:17 +0000

    cleaned...

--- python_modules/plearn/plotting/netplot.py
+++ python_modules/plearn/plotting/netplot.py
@@ -39,7 +39,7 @@ def customColorBar(min, max, (x,y,width,height) = (0.9,0.1,0.1,0.8), nb_ticks = 
     axes((x, y, width,height))
     if(min == max):
         max=min + 1e-6
-    cbarh = arange(min, max,  (max-min)/50.)
+    cbarh = arange(min, max,  (max-min)/nb_ticks)
     cbar = vecToVerticalMatrix(cbarh)
     cbarh_str = []
     for el in cbarh:
@@ -48,52 +48,11 @@ def customColorBar(min, max, (x,y,width,height) = (0.9,0.1,0.1,0.8), nb_ticks = 
     xticks([],[])
     imshow(cbar, cmap = color_map, vmin=min, vmax=max)
 
+def formatFloat(float):
+    return "%.2e" % float
 
 
-
-def plotLayer1Old(W, M, width):
-    '''plots each row of W and M as if they where matrix (width is the width of one of these matrix)'''
-
-    #some calculations for plotting
-
-    nbPlotStyles = 3 # (normal and two times 1 sur 2)
-    subPlotHeight = nbPlotStyles*2
-    subPlotWidth = max(len(W), len(M))
-    axesHeight = 1./float(subPlotHeight)
-    axesWidth = 1./float(subPlotWidth)
-
-    colorMap = defaultColorMap
-    
-    matrices = [W,M]
-    names = ["W", "M"]
-
-    #THE plotting
-
-    for i, matrix in enumerate(matrices):
-        for j, row in enumerate(matrix):
-            
-            #normal
-        
-            #subplot(subPlotHeight, subPlotWidth,(i%2)*subPlotWidth + j + 1)
-            axes((j*axesWidth, i*axesHeight, axesWidth, axesHeight))
-            imshow(rowToMatrix(row, width), interpolation="nearest", cmap = colorMap)
-            setPlotParams(names[i%2] + "_" + str(i) + "_" + str(j), False, True)
-            
-            #1 sur 2 -
-        
-            #subplot(subPlotHeight, subPlotWidth, (2+i%2)*subPlotWidth + j + 1)
-            axes((j*axesWidth, (i+2)*axesHeight, axesWidth, axesHeight))
-            imshow(rowToMatrix(toMinusRow(row),width), interpolation="nearest", cmap = colorMap)
-            setPlotParams(names[i%2] + "_" + str(i) + "_" + str(j), False, True)
-            
-            #1 sur 2 +
-
-            #subplot(subPlotHeight, subPlotWidth, (4+i%2)*subPlotWidth + j + 1)
-            axes((j*axesWidth, (i+4)*axesHeight, axesWidth, axesHeight))
-            imshow(rowToMatrix(toPlusRow(row),width), interpolation="nearest", cmap = colorMap)
-            setPlotParams(names[i%2] + "_" + str(i) + "_" + str(j), False, True)
-
-def plotLayer1(M, width, plotWidth=.1, start=0, length=-1, space_between_images=.01, apply_to_rows = None, index_to_plot = [], names = []):
+def plotLayer1(M, width, plotWidth=.1, start=0, length=-1, space_between_images=.02, apply_to_rows = None, index_to_plot = [], names = [], same_scale = False):
 
     
     #some calculations for plotting
@@ -111,12 +70,13 @@ def plotLayer1(M, width, plotWidth=.1, start=0, length=-1, space_between_images=
 
     colorMap = defaultColorMap
 
-    mi,ma = findMinMax(M)
-    print 'min', mi
-    print 'max', ma
+    if same_scale:
+        mi,ma = findMinMax(M)
+        print 'min', mi
+        print 'max', ma
     
-    ma = max(abs(mi),abs(ma))
-    mi = -ma
+        ma = max(abs(mi),abs(ma))
+        mi = -ma
             
 
     #THE plotting
@@ -126,12 +86,11 @@ def plotLayer1(M, width, plotWidth=.1, start=0, length=-1, space_between_images=
 
     if index_to_plot == []:
         index_to_plot = arange(start,start+length)
-    print index_to_plot
 
     j=0
     for i in index_to_plot:
            
-        #normal
+        #normal        
         row = M[i]
         if apply_to_rows != None:
             row = apply_to_rows(row)
@@ -140,11 +99,15 @@ def plotLayer1(M, width, plotWidth=.1, start=0, length=-1, space_between_images=
        # print x,y,plotWidth, plotHeight
         
         axes((x, y, plotWidth, plotHeight))
-        imshow(rowToMatrix(row, width), interpolation="nearest", cmap = colorMap, vmin = mi, vmax = ma)
-        if names == []:
-            setPlotParams('row_' + str(i), False, True)
+        if same_scale:
+            imshow(rowToMatrix(row, width), interpolation="nearest", cmap = colorMap, vmin = mi, vmax = ma)
         else:
-            setPlotParams(names[j],False,True)
+            imshow(rowToMatrix(row, width), interpolation="nearest", cmap = colorMap)#, vmin = mi, vmax = ma)
+            
+        if names == []:
+            setPlotParams('row_' + str(i), 1-same_scale, True)
+        else:
+            setPlotParams(names[j], 1-same_scale, True)
         j+=1
         
 
@@ -158,7 +121,7 @@ def plotLayer1(M, width, plotWidth=.1, start=0, length=-1, space_between_images=
             break
 
     #custom color bar
-    customColorBar(mi,ma,(1.-cbw-sbi, sbi, sbi, 1.-2.*cbw))
+   # customColorBar(mi,ma,(1.-cbw-sbi, sbi, sbi, 1.-2.*cbw))
     return toReturn
 
 
@@ -313,6 +276,23 @@ def toMinusRow(row):
         row2.append(row[i])
     return row2
 
+def evenMinusOdd(row):
+    return row[0::2]-row[1::2]
+
+def softmaxGroup2(row):
+    er = exp(row)
+    return er[0::2]/(er[0::2]+er[1::2])
+    # return er[1::2]/(er[0::2]+er[1::2])
+
+def doubleSizedWeightVectorToImageMatrix(row):
+    # v = evenMinusOdd(row)
+    v = softmaxGroup2(row)
+    d = sqrt(len(v))
+    print "Built %d x %d matrix" % (d,d)
+    m = reshape(v, (-1,d))
+    return m
+
+
 def rowToMatrixOld(row, width, validate_size = True, fill_value = 0.):
     '''change a row [1,2,3,4,5,6] into a matrix [[1,2],[3,4],[5,6]] if width = 2
        or [1,2,3,4,5,6] -> [[1,2,3,4],[5,6,fill_value,fill_value]] if width = 4 and validate_size = False
@@ -355,45 +335,6 @@ def vecToVerticalMatrix(vec):
     '''
     return reshape(array(vec), (len(vec),-1))
 
-def truncateMatrixOld(mat, maxHeight=10, maxWidth=10):
-    
-    width = len(mat[0])
-    height = len(mat)
-
-    #si notre matrice est trop haute (seulement), on va la couper en morceaux
-    if width > maxWidth and height <= maxHeight:        
-        truncMat = []
-        a = -1
-        for indCol in arange(len(mat[0])):
-            if indCol % maxWidth == 0:               
-                truncMat.append([])
-                a=a+1
-                for l in arange(height):#on ajoute des lignes
-                    truncMat[a].append([])                
-            for l in arange(height):#on remplie les lignes
-                truncMat[a][l].append(mat[l][indCol])            
-            
-        return truncMat
-    #si notre matrice est trop large   (seulement)    
-    elif height > maxHeight and width <= maxWidth:
-        truncMat = [[]]
-        a,l=0,0
-        for ligne in mat:
-            if(l >= maxHeight):
-                truncMat.append([])
-                a=a+1
-                l=0
-            truncMat[a].append(ligne)
-            l=l+1
-            
-        return truncMat
-    elif height > maxHeight and width > maxWidth:
-        print 'matrice trop grosse... rien a faire..'
-        truncMat = [mat]
-    else:
-        truncMat = [mat]
-
-    return truncMat
 
 def truncateMatrix(mat, n=10.):
         
