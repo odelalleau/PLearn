c77ba22184bb2db3c9f8564e42bb00f47434e117
tihocan <tihocan@15d43a8f-0efa-0310-bc34-a8a306da697e>
2007-12-28 20:53:01 +0000

    Generic classifier to make an underlying classifier operate on bags

---
+++ plearn_learners/classifiers/ToBagClassifier.cc
@@ -0,0 +1,190 @@
+// -*- C++ -*-
+
+// ToBagClassifier.cc
+//
+// Copyright (C) 2007 Olivier Delalleau
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+// Authors: Olivier Delalleau
+
+/*! \file ToBagClassifier.cc */
+
+
+#include "ToBagClassifier.h"
+#include <plearn/vmat/SubVMatrix.h>
+
+namespace PLearn {
+using namespace std;
+
+PLEARN_IMPLEMENT_OBJECT(
+    ToBagClassifier,
+    "Makes an existing classifier operate on bags.",
+    "Training is performed by simply removing bag information.\n"
+    "For testing, a majority vote is performed on each bag: assuming the\n"
+    "inner learner's output is made of the probabilities for each class,\n"
+    "these probabilities are summed over a full bag, and the class with\n"
+    "highest sum is taken as prediction.");
+
+/////////////////////
+// ToBagClassifier //
+/////////////////////
+ToBagClassifier::ToBagClassifier()
+{
+}
+
+////////////////////
+// declareOptions //
+////////////////////
+void ToBagClassifier::declareOptions(OptionList& ol)
+{
+    // ### ex:
+    // declareOption(ol, "myoption", &ToBagClassifier::myoption,
+    //               OptionBase::buildoption,
+    //               "Help text describing this option");
+    // ...
+
+    // Now call the parent class' declareOptions
+    inherited::declareOptions(ol);
+}
+
+////////////
+// build_ //
+////////////
+void ToBagClassifier::build_()
+{
+}
+
+///////////
+// build //
+///////////
+void ToBagClassifier::build()
+{
+    inherited::build();
+    build_();
+}
+
+/////////////////////////////
+// computeCostsFromOutputs //
+/////////////////////////////
+void ToBagClassifier::computeCostsFromOutputs(const Vec& input,
+                                              const Vec& output,
+                                              const Vec& target,
+                                              Vec& costs) const
+{
+    sub_target.resize(target.length() - 1);
+    sub_target << target.subVec(0, sub_target.length());
+    inherited::computeCostsFromOutputs(input, output, sub_target, costs);
+    int bag_info = int(round(target.lastElement()));
+    if (bag_info % 2 == 1)
+        bag_output.resize(0, 0);
+    bag_output.appendRow(output);
+    costs.resize(1);
+    if (bag_info >= 2) {
+        // Perform majority vote.
+        votes.resize(bag_output.width());
+        columnSum(bag_output, votes);
+        int target_class = int(round(target[0]));
+        if (argmax(votes) == target_class)
+            costs[0] = 0;
+        else
+            costs[0] = 1;
+    } else
+        costs.fill(MISSING_VALUE);
+}
+
+//////////////////////
+// getTestCostNames //
+//////////////////////
+TVec<string> ToBagClassifier::getTestCostNames() const
+{
+    static TVec<string> costs;
+    if (costs.isEmpty())
+        costs.append("class_error");
+    return costs;
+}
+
+/////////////////////////////////
+// makeDeepCopyFromShallowCopy //
+/////////////////////////////////
+void ToBagClassifier::makeDeepCopyFromShallowCopy(CopiesMap& copies)
+{
+    inherited::makeDeepCopyFromShallowCopy(copies);
+
+    // ### Call deepCopyField on all "pointer-like" fields
+    // ### that you wish to be deepCopied rather than
+    // ### shallow-copied.
+    // ### ex:
+    // deepCopyField(trainvec, copies);
+
+    // ### Remove this line when you have fully implemented this method.
+    PLERROR("ToBagClassifier::makeDeepCopyFromShallowCopy not fully (correctly) implemented yet!");
+}
+
+////////////////////
+// setTrainingSet //
+////////////////////
+void ToBagClassifier::setTrainingSet(VMat training_set, bool call_forget)
+{
+    // Remove bag information (last target).
+    PLCHECK( training_set->weightsize() == 0 &&
+             training_set->extrasize() == 0 ); // Not compatible yet.
+    PP<SubVMatrix> sub_train_set = new SubVMatrix(training_set, 0, 0, 
+                                                  training_set->length(),
+                                                  training_set->width() - 1);
+    sub_train_set->defineSizes(training_set->inputsize(),
+                               training_set->targetsize() - 1,
+                               training_set->weightsize(),
+                               training_set->extrasize());
+    setInnerLearnerTrainingSet(get_pointer(sub_train_set), call_forget);
+    PLearner::setTrainingSet(training_set, call_forget);
+}
+
+////////////////
+// targetsize //
+////////////////
+int ToBagClassifier::targetsize() const
+{
+    return learner_->targetsize() + 1;
+}
+
+} // end of namespace PLearn
+
+
+/*
+  Local Variables:
+  mode:c++
+  c-basic-offset:4
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:79
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=79 :
---
+++ plearn_learners/classifiers/ToBagClassifier.h
@@ -0,0 +1,172 @@
+// -*- C++ -*-
+
+// ToBagClassifier.h
+//
+// Copyright (C) 2007 Olivier Delalleau
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+// Authors: Olivier Delalleau
+
+/*! \file ToBagClassifier.h */
+
+
+#ifndef ToBagClassifier_INC
+#define ToBagClassifier_INC
+
+#include <plearn_learners/generic/EmbeddedLearner.h>
+
+namespace PLearn {
+
+/**
+ * The first sentence should be a BRIEF DESCRIPTION of what the class does.
+ * Place the rest of the class programmer documentation here.  Doxygen supports
+ * Javadoc-style comments.  See http://www.doxygen.org/manual.html
+ *
+ * @todo Write class to-do's here if there are any.
+ *
+ * @deprecated Write deprecated stuff here if there is any.  Indicate what else
+ * should be used instead.
+ */
+class ToBagClassifier : public EmbeddedLearner
+{
+    typedef EmbeddedLearner inherited;
+
+public:
+    //#####  Public Build Options  ############################################
+
+    //! ### declare public option fields (such as build options) here
+    //! Start your comments with Doxygen-compatible comments such as //!
+
+public:
+    //#####  Public Member Functions  #########################################
+
+    //! Default constructor
+    // ### Make sure the implementation in the .cc
+    // ### initializes all fields to reasonable default values.
+    ToBagClassifier();
+
+    //#####  PLearner Member Functions  #######################################
+
+    //! Set training set.
+    virtual void setTrainingSet(VMat training_set, bool call_forget=true);
+
+    //! Targetsize is one more than the inner learner's targetsize.
+    virtual int targetsize() const;
+
+    //! Compute costs.
+    virtual void computeCostsFromOutputs(const Vec& input, const Vec& output,
+                                         const Vec& target, Vec& costs) const;
+
+    //! Currently using PLearner's simple version for code simplicity.
+    virtual void computeOutputAndCosts(const Vec& input, const Vec& target,
+                                       Vec& output, Vec& costs) const
+    {
+        PLearner::computeOutputAndCosts(input, target, output, costs);
+    }
+
+    //! Currently using PLearner's simple version for code simplicity.
+    virtual void computeOutputsAndCosts(const Mat& input, const Mat& target,
+                                        Mat& output, Mat& costs) const
+    {
+        PLearner::computeOutputsAndCosts(input, target, output, costs);
+    }
+
+    //! Currently only compute classification error.
+    virtual TVec<string> getTestCostNames() const;
+
+    //#####  PLearn::Object Protocol  #########################################
+
+    // Declares other standard object methods.
+    // ### If your class is not instantiatable (it has pure virtual methods)
+    // ### you should replace this by PLEARN_DECLARE_ABSTRACT_OBJECT_METHODS
+    PLEARN_DECLARE_OBJECT(ToBagClassifier);
+
+    // Simply calls inherited::build() then build_()
+    virtual void build();
+
+    //! Transforms a shallow copy into a deep copy
+    // (PLEASE IMPLEMENT IN .cc)
+    virtual void makeDeepCopyFromShallowCopy(CopiesMap& copies);
+
+protected:
+
+    //! Used to store the target forwarded to the inner learner.
+    mutable Vec sub_target;
+
+    //! Used to store outputs on a bag.
+    mutable Mat bag_output;
+
+    //! Used to store votes.
+    mutable Vec votes;
+
+    //#####  Protected Options  ###############################################
+
+    // ### Declare protected option fields (such as learned parameters) here
+    // ...
+
+protected:
+    //#####  Protected Member Functions  ######################################
+
+    //! Declares the class options.
+    // (PLEASE IMPLEMENT IN .cc)
+    static void declareOptions(OptionList& ol);
+
+private:
+    //#####  Private Member Functions  ########################################
+
+    //! This does the actual building.
+    // (PLEASE IMPLEMENT IN .cc)
+    void build_();
+
+private:
+    //#####  Private Data Members  ############################################
+
+    // The rest of the private stuff goes here
+};
+
+// Declares a few other classes and functions related to this class
+DECLARE_OBJECT_PTR(ToBagClassifier);
+
+} // end of namespace PLearn
+
+#endif
+
+
+/*
+  Local Variables:
+  mode:c++
+  c-basic-offset:4
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:79
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=79 :
