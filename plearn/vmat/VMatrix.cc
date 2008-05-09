dfe6aa8a15e52f78d22d97cf0a016e7679a61d25
nouiz <nouiz@15d43a8f-0efa-0310-bc34-a8a306da697e>
2008-05-09 19:10:35 +0000

    commented change that break test.

--- plearn/vmat/VMatrix.cc
+++ plearn/vmat/VMatrix.cc
@@ -152,11 +152,11 @@ void VMatrix::declareOptions(OptionList & ol)
         "And if it is the source inside another VMatrix that sets its \n"
         "metadatadir, it will often be set from that surrounding vmat's metadata.\n");
 
-    declareOption(
-        ol, "mtime", &VMatrix::mtime_, 
-        OptionBase::buildoption|OptionBase::nosave,
-        "DO NOT play with this if you don't know the implementation!\n"
-        "The modification time of this VMatrix. Defaults to 0(unknow)");
+//     declareOption(
+//         ol, "mtime", &VMatrix::mtime_, 
+//         OptionBase::buildoption|OptionBase::nosave,
+//         "DO NOT play with this if you don't know the implementation!\n"
+//         "The modification time of this VMatrix. Defaults to 0(unknow)");
 
     inherited::declareOptions(ol);
 }
