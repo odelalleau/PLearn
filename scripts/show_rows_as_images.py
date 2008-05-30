8cffeb48de8b4fa8d7d5d94fe0dbd02f91d0d7eb
plearner <plearner@15d43a8f-0efa-0310-bc34-a8a306da697e>
2008-05-30 18:56:45 +0000

    Added parameters to control the grid nrows and ncols

--- scripts/show_rows_as_images.py
+++ scripts/show_rows_as_images.py
@@ -10,21 +10,24 @@ from plearn.plotting.netplot import showRowsAsImages
 if __name__ == "__main__":
 
     try:
-        datapmatfile, imgheight, imgwidth = sys.argv[1:]
+        datapmatfile, imgheight, imgwidth, nrows, ncols = sys.argv[1:]
         imgheight = int(imgheight)
         imgwidth = int(imgwidth)
+        nrows = int(nrows)
+        ncols = int(ncols)
     except:
-        print "Usage: "+sys.argv[0]+" <datafile.pmat> <imgheight> <imgwidth>"
+        print "Usage: "+sys.argv[0]+" <datafile.pmat> <imgheight> <imgwidth> <nrows> <ncols>"
         print """
         Will load a pmat in memory and consider the beginning of each row a imgheight x imgwidth imagette.
-        It will interactively display those.
+        These will be interactively displayed in a nrows x ncols grid of imagettes.
         """
+        print "Ex: "+sys.argv[0]+" /home/fringant2/lisa/data/faces/olivetti/faces.pmat 64 64  5 7"
         raise
     # sys.exit()
 
     data = load_pmat_as_array(datapmatfile)
     inputs = data[:,0:imgheight*imgwidth]
-    showRowsAsImages(inputs, figtitle=datapmatfile, img_width=imgwidth)
+    showRowsAsImages(inputs, figtitle=datapmatfile, nrows=nrows, ncols=ncols, img_width=imgwidth)
 
 
 
