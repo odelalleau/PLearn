d2b88fe1b2621c88153f941a645760fda9a63cc1
nouiz <nouiz@15d43a8f-0efa-0310-bc34-a8a306da697e>
2008-07-18 13:53:15 +0000

    when their is not specified fiels and no train_set, we use the source as the train_set.

--- plearn/vmat/MissingIndicatorVMatrix.cc
+++ plearn/vmat/MissingIndicatorVMatrix.cc
@@ -151,8 +151,9 @@ void MissingIndicatorVMatrix::getColumn(int i, Vec v) const
 void MissingIndicatorVMatrix::build_()
 {
     if (!source) PLERROR("In MissingIndicatorVMatrix:: source vmat must be supplied");
-    if(!train_set && !fields)
-      PLERROR("In MissingIndicatorVMatrix:: train_set or fields must be supplied");
+    if(!train_set && !fields){
+      train_set = source;
+    }
     updateMtime(source);
     if(train_set)
       updateMtime(train_set);
