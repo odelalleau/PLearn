9aeaff02c53339428e64a7fb6a31f45c936e33ca
larocheh <larocheh@15d43a8f-0efa-0310-bc34-a8a306da697e>
2009-01-08 16:59:46 +0000

    This commit adds a VMatrix that contains samples of the MNIST dataset as well as transformed versions of these samples according to class-invariant transformations.
    The pymake.config.model file was updated to include the necessary library flag for compilation. This currently works only at the LISA lab...

---
+++ plearn/vmat/InfiniteMNISTVMatrix.cc
@@ -0,0 +1,251 @@
+// -*- C++ -*-
+
+// InfiniteMNISTVMatrix.cc
+//
+// Copyright (C) 2008 Hugo Larochelle
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+// Authors: Hugo Larochelle
+
+/*! \file InfiniteMNISTVMatrix.cc */
+
+
+#include "InfiniteMNISTVMatrix.h"
+
+namespace PLearn {
+using namespace std;
+
+
+// Initialize static variables
+mnistproblem_t* InfiniteMNISTVMatrix::dataset = 0;
+int InfiniteMNISTVMatrix::n_pointers_to_dataset = 0;
+
+PLEARN_IMPLEMENT_OBJECT(
+    InfiniteMNISTVMatrix,
+    "VMatrix containing an \"infinite\" stream of MNIST samples.",
+    "VMatrix that uses the code from \"Training Invariant Support Vector Machines\n"
+    "using Selective Sampling\" by Loosli, Canu and Bottou (JMLR 2007), to generate\n"
+    "\"infinite\" stream (i.e. INT_MAX sized set) of samples from the MNIST dataset. The samples\n"
+    "are obtained by applying some class-invariante transformations on the original MNIST\n"
+    "dataset.\n"
+    );
+
+InfiniteMNISTVMatrix::InfiniteMNISTVMatrix():
+    include_test_examples(false),
+    include_validation_examples(false),
+    input_divisor(1.),
+    test_images(TEST_IMAGES_PATH),
+    test_labels(TEST_LABELS_PATH),
+    train_images(TRAIN_IMAGES_PATH),
+    train_labels(TRAIN_LABELS_PATH),
+    fields(FIELDS_PATH),
+    tangent_vectors(TANGVEC_PATH)
+/* ### Initialize all fields to their default value */
+{
+    InfiniteMNISTVMatrix::n_pointers_to_dataset++;
+}
+
+InfiniteMNISTVMatrix::~InfiniteMNISTVMatrix()
+{
+    InfiniteMNISTVMatrix::n_pointers_to_dataset--;
+    if( InfiniteMNISTVMatrix::dataset && InfiniteMNISTVMatrix::n_pointers_to_dataset == 0 )
+    {
+            destroy_mnistproblem(dataset);
+            InfiniteMNISTVMatrix::dataset = 0;
+    }
+}
+
+void InfiniteMNISTVMatrix::getNewRow(int i, const Vec& v) const
+{
+    int i_dataset;
+    if( include_test_examples )
+        if( include_validation_examples )
+            i_dataset = i;
+        else
+            if( i < 10000)
+                i_dataset = i;
+            else
+                i_dataset = i + ((i-10000)/50000)*10000;
+    else
+        if( include_validation_examples )
+            i_dataset = i+10000;
+        else
+            i_dataset = i + (i/50000)*10000 + 10000;
+
+    unsigned char *image = cache_transformed_vector(InfiniteMNISTVMatrix::dataset, i_dataset);
+
+    unsigned char* xj=image;
+    real* vj=v.data();
+    for( int j=0; j<inputsize_; j++, xj++, vj++ )
+        *vj = *xj/input_divisor;
+    
+    v.last() = InfiniteMNISTVMatrix::dataset->y[ (i_dataset<10000) ? i_dataset : 10000 + ((i_dataset - 10000) % 60000) ];
+}
+
+void InfiniteMNISTVMatrix::declareOptions(OptionList& ol)
+{
+     declareOption(ol, "include_test_examples", &InfiniteMNISTVMatrix::include_test_examples,
+                   OptionBase::buildoption,
+                   "Indication that the test examples from the MNIST dataset should be included.\n"
+                   "This option is false by default. If true, these examples will be the first"
+                   "10000\n"
+                   "of this VMatrix.\n");
+
+     declareOption(ol, "include_validation_examples", &InfiniteMNISTVMatrix::include_validation_examples,
+                   OptionBase::buildoption,
+                   "Indication that the validation set examples (the last 10000 examples from the\n"
+                   "training set) should be included in this VMatrix.\n");     
+
+     declareOption(ol, "input_divisor", &InfiniteMNISTVMatrix::input_divisor,
+                   OptionBase::buildoption,
+                   "Value that the inputs should be divided by.\n");     
+
+     declareOption(ol, "test_images", &InfiniteMNISTVMatrix::test_images,
+                   OptionBase::buildoption,
+                   "File path of MNIST test images.\n");     
+
+     declareOption(ol, "test_labels", &InfiniteMNISTVMatrix::test_labels,
+                   OptionBase::buildoption,
+                   "File path of MNIST test labels.\n");     
+
+     declareOption(ol, "train_images", &InfiniteMNISTVMatrix::train_images,
+                   OptionBase::buildoption,
+                   "File path of MNIST train images.\n");     
+
+     declareOption(ol, "train_labels", &InfiniteMNISTVMatrix::train_labels,
+                   OptionBase::buildoption,
+                   "File path of MNIST train labels.\n");     
+
+     declareOption(ol, "fields", &InfiniteMNISTVMatrix::fields,
+                   OptionBase::buildoption,
+                   "File path of MNIST fields information.\n");     
+
+     declareOption(ol, "tangent_vectors", &InfiniteMNISTVMatrix::tangent_vectors,
+                   OptionBase::buildoption,
+                   "File paht of MNIST transformation tangent vectors.\n");
+
+    // Now call the parent class' declareOptions
+    inherited::declareOptions(ol);
+}
+
+void InfiniteMNISTVMatrix::build_()
+{
+
+    if( !InfiniteMNISTVMatrix::dataset )
+    {
+        char* test_images_char = new char[test_images.size()+1];
+        char* test_labels_char = new char[test_labels.size()+1];
+        char* train_images_char = new char[train_images.size()+1];
+        char* train_labels_char = new char[train_labels.size()+1];
+        char* fields_char = new char[fields.size()+1];
+        char* tangent_vectors_char = new char[tangent_vectors.size()+1];
+        
+        strcpy(test_images_char,test_images.c_str());
+        strcpy(test_labels_char,test_labels.c_str());
+        strcpy(train_images_char,train_images.c_str());
+        strcpy(train_labels_char,train_labels.c_str());
+        strcpy(fields_char,fields.c_str());
+        strcpy(tangent_vectors_char,tangent_vectors.c_str());
+        
+        InfiniteMNISTVMatrix::dataset = create_mnistproblem(
+            test_images_char,
+            test_labels_char,
+            train_images_char,
+            train_labels_char,
+            fields_char,
+            tangent_vectors_char);
+        if( !InfiniteMNISTVMatrix::dataset )
+            PLERROR("In InfiniteMNISTVMatrix(): could not load MNIST dataset");
+    }
+
+
+    if( include_test_examples )
+        if( include_validation_examples )
+            length_ = INT_MAX;
+        else
+            // Might be removing more samples than need, but we have so many anyways...
+            length_ = INT_MAX - ((INT_MAX-10000)/50000)*10000 + 1;
+    
+    else
+        if( include_validation_examples )
+            length_ = INT_MAX - 10000+1;
+        else
+            // Might be removing more samples than need, but we have so many anyways...
+            length_ = INT_MAX - ((INT_MAX-10000)/50000)*10000 - 10000 + 1;
+
+    inputsize_ = 784;
+    targetsize_ = 1;
+    weightsize_ = 0;
+    extrasize_ = 0;
+    width_ = 785;
+
+    // ### You should keep the line 'updateMtime(0);' if you don't implement the 
+    // ### update of the mtime. Otherwise you can have an mtime != 0 that is not valid.
+    updateMtime(0);
+    //updateMtime(filename);
+    //updateMtime(VMat);
+}
+
+// ### Nothing to add here, simply calls build_
+void InfiniteMNISTVMatrix::build()
+{
+    inherited::build();
+    build_();
+}
+
+void InfiniteMNISTVMatrix::makeDeepCopyFromShallowCopy(CopiesMap& copies)
+{
+    inherited::makeDeepCopyFromShallowCopy(copies);
+
+    // ### Call deepCopyField on all "pointer-like" fields
+    // ### that you wish to be deepCopied rather than
+    // ### shallow-copied.
+    // ### ex:
+    // deepCopyField(trainvec, copies);
+
+    PLWARNING("InfiniteMNISTVMatrix::makeDeepCopyFromShallowCopy is not totally implemented. Need "
+              "to figure out how to deep copy the \"dataset\" variable (mnistproblem_t*).\n");
+    InfiniteMNISTVMatrix::n_pointers_to_dataset++;
+}
+
+} // end of namespace PLearn
+
+
+/*
+  Local Variables:
+  mode:c++
+  c-basic-offset:4
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:79
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=79 :
---
+++ plearn/vmat/InfiniteMNISTVMatrix.h
@@ -0,0 +1,168 @@
+// -*- C++ -*-
+
+// InfiniteMNISTVMatrix.h
+//
+// Copyright (C) 2008 Hugo Larochelle
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//
+//  3. The name of the authors may not be used to endorse or promote
+//     products derived from this software without specific prior written
+//     permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+// NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// This file is part of the PLearn library. For more information on the PLearn
+// library, go to the PLearn Web site at www.plearn.org
+
+// Authors: Hugo Larochelle
+
+/*! \file InfiniteMNISTVMatrix.h */
+
+
+#ifndef InfiniteMNISTVMatrix_INC
+#define InfiniteMNISTVMatrix_INC
+
+#define TEST_IMAGES_PATH "/home/fringant2/lisa/data/mnist/t10k-images-idx3-ubyte"
+#define TEST_LABELS_PATH "/home/fringant2/lisa/data/mnist/t10k-labels-idx1-ubyte"
+#define TRAIN_IMAGES_PATH "/home/fringant2/lisa/data/mnist/train-images-idx3-ubyte"
+#define TRAIN_LABELS_PATH "/home/fringant2/lisa/data/mnist/train-labels-idx1-ubyte"
+#define FIELDS_PATH "/home/fringant2/lisa/data/mnist/fields_float_1522x28x28.bin"
+#define TANGVEC_PATH "/home/fringant2/lisa/data/mnist/tangVec_float_60000x28x28.bin"
+
+#include <plearn/vmat/RowBufferedVMatrix.h>
+#include <kernel-invariant.h>
+
+namespace PLearn {
+
+/**
+ * VMatrix that uses the code from "Training Invariant Support Vector Machines 
+ * using Selective Sampling" by Loosli, Canu and Bottou (JMLR 2007), to generate 
+ * "infinite" stream (i.e. INT_MAX sized set) of samples from the MNIST dataset. The samples
+ * are obtained by applying some class-invariante transformations on the original MNIST
+ * dataset.
+ */
+class InfiniteMNISTVMatrix : public RowBufferedVMatrix
+{
+    typedef RowBufferedVMatrix inherited;
+
+public:
+    //#####  Public Build Options  ############################################
+
+    //! Indication that the test examples from the MNIST dataset should be included.
+    //! This option is false by default. If true, these examples will be the first 10000
+    //! of this VMatrix.
+    bool include_test_examples;
+
+    //! Indication that the validation set examples (the last 10000 examples from the
+    //! training set) should be included in this VMatrix.
+    bool include_validation_examples;
+    
+    //! Value that the inputs should be divided by.
+    real input_divisor;
+
+    // Files required for loading infinite MNIST dataset
+    //! File path of MNIST test images.
+    string test_images;
+    //! File path of MNIST test labels.
+    string test_labels;
+    //! File path of MNIST train images.
+    string train_images;
+    //! File path of MNIST train labels.
+    string train_labels;
+    //! File path of MNIST fields information.
+    string fields;
+    //! File path of MNIST transformation tangent vectors.
+    string tangent_vectors;
+
+public:
+    //#####  Public Member Functions  #########################################
+
+    //! Default constructor
+    // ### Make sure the implementation in the .cc
+    // ### initializes all fields to reasonable default values.
+    InfiniteMNISTVMatrix();
+
+    ~InfiniteMNISTVMatrix();
+
+    //#####  PLearn::Object Protocol  #########################################
+
+    // Declares other standard object methods.
+    // ### If your class is not instantiatable (it has pure virtual methods)
+    // ### you should replace this by PLEARN_DECLARE_ABSTRACT_OBJECT_METHODS
+    PLEARN_DECLARE_OBJECT(InfiniteMNISTVMatrix);
+
+    // Simply calls inherited::build() then build_()
+    virtual void build();
+
+    //! Transforms a shallow copy into a deep copy
+    // (PLEASE IMPLEMENT IN .cc)
+    virtual void makeDeepCopyFromShallowCopy(CopiesMap& copies);
+
+protected:
+    //#####  Protected Options  ###############################################
+
+    static mnistproblem_t *dataset;
+    static int n_pointers_to_dataset;
+
+protected:
+    //#####  Protected Member Functions  ######################################
+
+    //! Declares the class options.
+    // (PLEASE IMPLEMENT IN .cc)
+    static void declareOptions(OptionList& ol);
+
+    //! Fill the vector 'v' with the content of the i-th row.
+    //! 'v' is assumed to be the right size.
+    // (PLEASE IMPLEMENT IN .cc)
+    virtual void getNewRow(int i, const Vec& v) const;
+
+private:
+    //#####  Private Member Functions  ########################################
+
+    //! This does the actual building.
+    // (PLEASE IMPLEMENT IN .cc)
+    void build_();
+
+private:
+    //#####  Private Data Members  ############################################
+
+    // The rest of the private stuff goes here
+};
+
+// Declares a few other classes and functions related to this class
+DECLARE_OBJECT_PTR(InfiniteMNISTVMatrix);
+
+} // end of namespace PLearn
+
+#endif
+
+
+/*
+  Local Variables:
+  mode:c++
+  c-basic-offset:4
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:79
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=79 :
